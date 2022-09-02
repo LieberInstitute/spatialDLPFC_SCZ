43a1062fd262bdea1a31918c3f4db3b45d211be9
Uma Kaipa <ukaipa@compute-095.cm.cluster>
2022-09-02 14:59:08 -0400

    preprocessing code for Claudin5 channel segmentation

---
+++ code/2_MockPNN/Claudin_segmentation/Claudin_segmentations.py
@@ -0,0 +1,71 @@
+'''
+For Visium-IF
+Channel0 = DAPI, DAPI
+Channel1 = Claudin5 (Alex 488),
+Channel2 = NeuN (Alexa 555),
+Channel3 = WFA (Alexa 647),
+Channel4 = AF (Autofluorescence), sample AF
+Channel5 = Thumbnail
+'''
+
+
+import numpy as np
+import numpy.ma as ma
+import pandas as pd
+import PIL
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance, ImageSequence
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import mpldatacursor
+import glob
+import sys
+import cv2
+import math
+import scipy
+from scipy.spatial.distance import *
+# from scipy import ndimage, distance
+import imageio
+import skimage
+from skimage import *
+from skimage import feature, segmentation, draw, measure, morphology
+from skimage.morphology import (erosion,dilation,opening,closing,white_tophat,black_tophat,skeletonize,convex_hull_image)
+from skimage.draw import polygon_perimeter
+import tifffile as tif
+import imagecodecs
+import itertools
+from itertools import product
+from collections import defaultdict
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+
+img_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles/'
+img_dir_NTC = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining/'
+img_NTC = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining/20220712_VIF_MockPNN_Strong_NTC_Scan1_[11013,50974]_component_data.tif'
+img_SCZ = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/20220712_VIF_MockPNN_Strong_SCZ_C1_Br2039_MLtraining/20220712_VIF_MockPNN_Strong_SCZ_Scan1_[10629,49106]_component_data.tif'
+
+# Read and normalize the image
+img_claudin = Image.open(img_NTC)
+img_claudin.seek(1) # channel 1 = Claudin 5
+claudin = cv2.normalize(np.array(img_claudin, dtype = 'float32'), np.zeros(np.array(img_claudin, dtype = 'float32').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
+
+
+# Increasing the contrast
+claudin[claudin <= claudin.mean()] = 0.0
+claudin[claudin >= claudin.mean()] = 1.0
+
+# Plot the normalized/pre-processed image
+fig,ax = plt.subplots(figsize = (20,20))
+ax.imshow(claudin)
+fig.show()
+
+
+
+
+
+
+
+
+
+
