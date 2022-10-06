18a9a95e5e431c0dbc2eea2ad3b3aa8310dce52b
Uma Kaipa <ukaipa@compute-093.cm.cluster>
2022-10-06 14:32:54 -0400

    added relative paths

--- code/2_MockPNN/PNN_Segmentation/01_Read_annotations/Process_manual_annotations.py
+++ code/2_MockPNN/PNN_Segmentation/01_Read_annotations/Process_manual_annotations.py
@@ -16,8 +16,8 @@ import tensorflow as tf
 import keras
 
 # import one of the manual annotation csvs--pref which has lesser rows
-img_test = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles/20220712_VIF_MockPNN_Strong_Scan1_[12864,50280]_component_data_17.tif'
-csv_test = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/2_MockPNN/Training_tiles/Manual_annotations/Annotations/20220712_VIF_MockPNN_Strong_Scan1_[12864,50280]_component_data_17.csv'
+img_test = pyhere.here('raw-data', 'images', '2_MockPNN', '20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining', '20220712_VIF_MockPNN_Strong_Scan1_[12864,50280]_component_data_17.tif')
+csv_test = pyhere.here('processed-data', '2_MockPNN', 'Training_tiles', 'Manual_annotations', 'Annotations', '20220712_VIF_MockPNN_Strong_Scan1_[12864,50280]_component_data_17.csv')
 
 # function to extract all the coordinates from the csv
 def extracting_coords(csv_path, object_name):
@@ -48,14 +48,15 @@ example = tf.train.Example(features=tf.train.Features(feature={'img_name': tf.tr
                                                                'label': tf.train.Feature(bytes_list = tf.train.BytesList(value = [obj_name])),
                                                                'x1':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([x for x in file17['x1']])))),
                                                                'y1':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['y1']])))),
-                                                               'x2':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([x for x in file17['x2']])))),
-                                                               'y2':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['y2']])))),
-                                                               'x3':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([x for x in file17['x3']])))),
-                                                               'y3':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['y3']])))),
-                                                               'x4':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([x for x in file17['x4']])))),
-                                                               'y4':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['y4']]))))}))
+                                                               'w':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['Width']])))),
+                                                               'h':tf.train.Feature(int64_list = tf.train.Int64List(value = np.int0(np.ceil([y for y in file17['Height']]))))}))
 
-
-# create a function for one file
-# call the function for all the csvs
-# check if this works
+# for loop to create the format of label,x,y,w,h
+for x,y,w,h in zip(file17['x1'], file17['y1'], file17['w'], file17['h']):
+    print(x,y,w,h)
+# import the df for blood vessels
+# create the feature objects for blood vessels
+# then create 1 example object for 1 tile
+# loop through the rest of the manual annotations slides
+# sanity check
+# convert to Pascal-VOC
