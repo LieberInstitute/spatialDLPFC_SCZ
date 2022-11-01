d798b46a4ebd1991cca11051f1bd203cd001fa6b
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2022-11-01 09:40:13 -0400

    changed the read function to include the contrast preprocessing of claudin and wfa channel

--- code/2_MockPNN/helper_functions.py
+++ code/2_MockPNN/helper_functions.py
@@ -32,8 +32,16 @@
 def read_norm(filepath, ch_num):
     img = Image.open(filepath)
     img.seek(ch_num)
-    img = cv2.normalize(np.array(img, dtype = 'float32'), np.zeros(np.array(img, dtype = 'float32').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
-    return img
+    if ch_num == 1: # if claudin,
+        img_claudin = cv2.normalize(np.array(img, dtype = 'float32'), np.zeros(np.array(img, dtype = 'float32').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
+        img_claudin[img_claudin <= img_claudin.mean()] = 0.0
+        img_claudin[img_claudin >= img_claudin.mean()] = 1.0
+    else:
+        img_arr = np.array(img, dtype = 'float32')
+        img_arr[img_arr <= img_arr.mean()] = 0.0
+        img_arr[img_arr >= 1.0] = img_arr.max()
+        img_wfa = cv2.normalize(img_arr, np.zeros(img_arr.shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
+    return img_claudin, img_wfa
 
 
 # detect contours in the normalised_img
