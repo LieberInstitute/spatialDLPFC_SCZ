6b375283419ae461d45a3fac5e9c42e7540a6ec6
Uma Kaipa <ukaipa@compute-095.cm.cluster>
2022-11-01 16:22:36 -0400

    runtime error with contours

--- code/2_MockPNN/03_DL_training_data/Segmentation_test_file.py
+++ code/2_MockPNN/03_DL_training_data/Segmentation_test_file.py
@@ -10,6 +10,7 @@ Channel5 = Thumbnail
 
 
 import numpy as np
+import helper_functions
 import pyhere
 from pathlib import Path
 import pandas as pd
@@ -34,7 +35,7 @@ from collections import defaultdict
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-# cd /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code
+
 # img_dir = pyhere.here('raw-data', 'images', '2_MockPNN', 'Training_tiles')
 img_dir_NTC = pyhere.here('raw-data', 'images', '2_MockPNN', '20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining')
 img_NTC = pyhere.here('raw-data', 'images', '2_MockPNN', '20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining', '20220712_VIF_MockPNN_Strong_NTC_Scan1_[11013,50974]_component_data.tif')
@@ -45,17 +46,22 @@ img_test = pyhere.here('raw-data', 'images', '2_MockPNN', 'Training_tiles', '202
 wfa = read_norm(img_test, 3)
 claudin = read_norm(img_test, 1)
 
-
 # Plot the normalized/pre-processed image
 fig,ax = plt.subplots(nrows = 1, ncols = 2,figsize = (20,20))
-ax[0].imshow(claudin)
+ax[0].imshow(wfa_contours)
 ax[1].imshow(wfa)
 fig.show()
 
-# detect contours
-contour_list = detect_contours(claudin)
-# draw contours
-x,y,w,h,area,claudin_contours = draw_contours(contour_list, claudin, (255,0,0), 2)
+# detect contours for claudin
+contour_list_claudin = detect_contours(claudin)
+
+# draw contours on wfa
+x,y,w,h,area,claudin_contours = draw_contours(contour_list_claudin, wfa, (0,0,0), -1) # -1 to fill the BB
 claudin_df = create_df(x,y,w,h,area,img_test, 'blood_vessels')
+
+# draw PNN contours on wfa
+contour_list_wfa = detect_contours(wfa) # something wrong with detecting contours (the blood vessel contours get added up to wfa ones)
+x,y,w,h,area,wfa_contours = draw_contours(contour_list_wfa, wfa, (255,0,0), 3) # -1 to fill the BB
+wfa_df = create_df(x,y,w,h,area,img_test, 'PNNs')
 
 
