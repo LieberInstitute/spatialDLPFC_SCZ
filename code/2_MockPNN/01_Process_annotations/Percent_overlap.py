007f94a9d67118de3099aef334e7e122aef4289c
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2022-11-14 14:31:12 -0500

    relative file paths to directories

--- code/2_MockPNN/01_Process_annotations/Percent_overlap.py
+++ code/2_MockPNN/01_Process_annotations/Percent_overlap.py
@@ -35,3 +35,11 @@ from itertools import product
 from collections import defaultdict
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+
+
+img_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles/'
+img_dir = pyhere.here('raw_data', 'images', '2_Mock_PNN', 'Training_tiles')
+csv_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/2_MockPNN/Training_tiles/Manual_annotations/Annotations/'
+csv_dir = pyhere.here('processed-data', '2_Mock_PNN', 'Training_tiles', 'Manual_annotations', 'Annotations')
+img_test = pyhere.here('raw-data', 'images', '2_MockPNN', 'Training_tiles', '20220712_VIF_MockPNN_Strong_Scan1_[6384,53057]_component_data_11.tif')
+csv_test = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/2_MockPNN/Training_tiles/Manual_annotations/Annotations/20220712_VIF_MockPNN_Strong_Scan1_[6384,53057]_component_data_11.csv'
