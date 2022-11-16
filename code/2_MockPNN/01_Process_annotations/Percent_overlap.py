947e548bd50e3085a9debe7dff5c02ea14e78a75
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2022-11-16 14:38:49 -0500

    find the percent overlap between DAPI and NeuN with WFA

--- code/2_MockPNN/01_Process_annotations/Percent_overlap.py
+++ code/2_MockPNN/01_Process_annotations/Percent_overlap.py
@@ -33,7 +33,7 @@ from skimage.draw import polygon_perimeter
 import itertools
 import collections
 from itertools import product
-from collections import defaultdict
+from collections import defaultdict, Counter
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
@@ -54,7 +54,7 @@ for img_name in os.listdir(img_dir):
             print(img_name, csv_name)
             images = Image.open(os.path.join(img_dir, img_name))
             csv = manual_annot(os.path.join(csv_dir, csv_name))
-            print(len(csv))
+            print("Manual annotated PNNs:", len(csv))
             images.seek(1) # seek the frame of interest #1=Claudin
             cla = cv2.normalize(np.array(images, dtype = 'float32'), np.zeros(np.array(images, dtype = 'float32').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX) # normalisation using local min_max
             img_cla = Image.fromarray(cla) # reconstruct the image
@@ -65,7 +65,7 @@ for img_name in os.listdir(img_dir):
             wfac = skimage.color.gray2rgb(wfa)
             hierachy, img_threshold = cv2.threshold(np.array(cla * 255, dtype = np.uint8), 10, 255, cv2.THRESH_BINARY)
             contours,_ = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            print(len(contours))
+            print("Claudin contours:",len(contours))
             for cnt in contours:
                 x,y,w,h = cv2.boundingRect(cnt)
                 # print("CLAUDIN CONTOURS", x,y,w,h)
@@ -91,23 +91,78 @@ for img_name in os.listdir(img_dir):
                     w.append(w1)
                     h.append(h1)
                     area.append(area1)
-                    out_img1 = cv2.rectangle(out_img_clr, (x1-10,y1-10), (x1+w1+10, y1+h1+10), (0,255,0), 2) # change the color to black (0,0,0) if bb is not needed
+                    out_img1 = cv2.rectangle(out_img_clr, (x1-10,y1-10), (x1+w1+10, y1+h1+10), (255,0,0), 2) # change the color to black (0,0,0) if bb is not needed
                     rect = cv2.minAreaRect(cnt)
                     box = cv2.boxPoints(rect)
                     box = np.int0(box)
                     out_img1 = cv2.drawContours(out_img1,[box],0,(0,0,0),1) # comment out if contour box is not needed
-            draw_rect(csv, out_img1)
+            draw_rect(csv, out_img1, (0,255,0)) # draw manual annotations BB on PNN segmented image GREEN
             df_wfa_ml = create_df(x,y,w,h, area, os.path.join(img_dir, img_name), 'PNN')
-            for i in range(len(df_wfa_ml)): # PNN
-                for k in range(len(csv)):
-                    for j in range(len(img_info_dapi)): # DAPI
-                        xmin1, xmax1, xmin2, xmax2 = df_wfa_ml['x1'][i], df_wfa_ml['x4'][i], csv['x1'][k], csv['x4'][k]
-                        ymin1, ymax1, ymin2, ymax2 = df_wfa_ml['y1'][i], df_wfa_ml['y4'][i], csv['y1'][k], csv['y4'][k]
-                        # xmin1, xmax1, xmin2, xmax2 = df_wfa_ml['x1'][i], df_wfa_ml['x4'][i], img_info_dapi['x1'][k], img_info_dapi['x4'][k]
-                        # ymin1, ymax1, ymin2, ymax2 = df_wfa_ml['y1'][i], df_wfa_ml['y4'][i], img_info_dapi['y1'][k], img_info_dapi['y4'][k]
-                        if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
-                            print(xmin1, xmax1, xmin2, xmax2, i, k)
+            overlap_ml_ann = []
+            for pnn in range(len(df_wfa_ml)):
+                for ann in range(len(csv)):
+                    xmin1, xmin2, xmax1, xmax2, ymin1, ymin2, ymax1, ymax2 = df_wfa_ml['x1'][pnn],csv['x1'][ann],df_wfa_ml['x4'][pnn],csv['x4'][ann],df_wfa_ml['y1'][pnn],csv['y1'][ann],df_wfa_ml['y4'][pnn],csv['y4'][ann]
+                    if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
+                        overlap_ml_ann.append(ann)
+            print("Number of manual and segmented box overlap",len(Counter(overlap_ml_ann).keys()))
+            # fig,ax = plt.subplots(figsize = (20,20))
+            # ax.imshow(out_img1)
+            # fig.show()
+            # DAPI SEGMENTATION
+            dapi, dapi_clr = read_norm(os.path.join(img_dir, img_name), 0)
+            shifted, thresh, gray = morph_transform(dapi_clr)
+            labels = find_labels(thresh)
+            dpx, dpy, dpw, dph, area, segmented_dapi = draw_rect_dapi(labels, gray, dapi_clr)
+            img_info_dapi = create_df(dpx, dpy, dpw, dph, area, os.path.join(img_dir, img_name), 'DAPI')
+            draw_rect(img_info_dapi, out_img1, (0,0,255)) # draw dapi BB on the PNN segmented image
+            overlap_ml_dapi = []
+            for pnn1 in range(len(df_wfa_ml)):
+                for dap in range(len(img_info_dapi)):
+                    xmin1, xmin2, xmax1, xmax2, ymin1, ymin2, ymax1, ymax2 = df_wfa_ml['x1'][pnn1], img_info_dapi['x1'][dap], df_wfa_ml['x4'][pnn1], img_info_dapi['x4'][dap], df_wfa_ml['y1'][pnn1], img_info_dapi['y1'][dap],df_wfa_ml['y4'][pnn1], img_info_dapi['y4'][dap]
+                    if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
+                        overlap_ml_dapi.append(pnn1)
+            print("Number of DAPI and segmented box overlap",len(Counter(overlap_ml_dapi).keys()))
+            # NEUN SEGMENTATION
+            neun= read_norm(os.path.join(img_dir, img_name), 2)
+            neun_contours = detect_contours(neun)
+            nx,ny,nw,nh,narea, neun_segmented = draw_contours(neun_contours, neun, (0,255,0), 2)
+            df_ml_neun = create_df(nx,ny,nw,nh, narea,os.path.join(img_dir, img_name) , 'NeuN')
+            draw_rect(df_ml_neun, out_img1, (255,215,0)) # draw neun boxes on the same PNN and dapi segmented image
+            overlap_ml_neun = []
+            for pnn2 in range(len(df_wfa_ml)):
+                for nn in range(len(df_ml_neun)):
+                    xmin1, xmin2, xmax1, xmax2, ymin1, ymin2, ymax1, ymax2 = df_wfa_ml['x1'][pnn2], df_ml_neun['x1'][nn], df_wfa_ml['x4'][pnn2], df_ml_neun['x4'][nn], df_wfa_ml['y1'][pnn2], df_ml_neun['y1'][nn],df_wfa_ml['y4'][pnn2], df_ml_neun['y4'][nn]
+                    if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
+                        overlap_ml_neun.append(pnn2)
+            print("Number of NeuN and segmented box overlap",len(Counter(overlap_ml_neun).keys()))
             fig,ax = plt.subplots(figsize = (20,20))
             ax.imshow(out_img1)
             fig.show()
 
+
+            # for i in range(len(df_wfa_ml)): # PNN
+            #     for k in range(len(csv)):
+            #         for j in range(len(img_info_dapi)): # DAPI
+            #             xmin1, xmax1, xmin2, xmax2 = df_wfa_ml['x1'][i], df_wfa_ml['x4'][i], csv['x1'][k], csv['x4'][k]
+            #             ymin1, ymax1, ymin2, ymax2 = df_wfa_ml['y1'][i], df_wfa_ml['y4'][i], csv['y1'][k], csv['y4'][k]
+            #             # xmin1, xmax1, xmin2, xmax2 = df_wfa_ml['x1'][i], df_wfa_ml['x4'][i], img_info_dapi['x1'][k], img_info_dapi['x4'][k]
+            #             # ymin1, ymax1, ymin2, ymax2 = df_wfa_ml['y1'][i], df_wfa_ml['y4'][i], img_info_dapi['y1'][k], img_info_dapi['y4'][k]
+            #             if xmax1 >= xmin2 and xmax2 >= xmin1 and ymax1 >= ymin2 and ymax2 >= ymin1:
+            #                 print(xmin1, xmax1, xmin2, xmax2, i, k)
+            fig,ax = plt.subplots(figsize = (20,20))
+            ax.imshow(out_img1)
+            fig.show()
+
+
+# i = manual annotations
+# j = dapi
+# k = NeuN
+# l = segmentations
+
+for i in range(len(csv)):
+    for j in range(len(df_wfa_ml)):
+        for k in range(len(img_info_dapi)):
+            xmin1, xmin2, xmin3 = csv['x1'][i], df_wfa_ml['x1'][j], img_info_dapi['x1'][k]
+            ymin1, ymin2, ymin3 = csv['y1'][i], df_wfa_ml['y1'][j], img_info_dapi['y1'][k]
+            xmax1, xmax2, xmax3 = csv['x4'][i], df_wfa_ml['x4'][j], img_info_dapi['x4'][k]
+            ymax1, ymax2, ymax3 = csv['y4'][i], df_wfa_ml['y4'][j], img_info_dapi['y4'][k]
