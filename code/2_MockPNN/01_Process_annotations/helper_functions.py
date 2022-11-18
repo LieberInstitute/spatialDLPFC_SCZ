9be311efe4e45d5c32f3cf9897a440fa210d0c85
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2022-11-18 10:58:04 -0500

    create a dataframe with all of the BB information

--- code/2_MockPNN/01_Process_annotations/helper_functions.py
+++ code/2_MockPNN/01_Process_annotations/helper_functions.py
@@ -89,6 +89,19 @@ def draw_single_rect(df_manual_test, contour_img):
     cv2.rectangle(contour_img, (df_manual_test['x1'], df_manual_test['y1']), (df_manual_test['x4'], df_manual_test['y4']), (255,211,155), 2)
     # return contour_img
 
+# populate a dataframe with the coordinates info
+def create_df(x,y,w,h, area, img_test, label):
+    print("Segmented {0} {1}".format(len(x), label))
+    col_names = ['img_file_name','type_of_object_str', 'x1', 'y1', 'Width', 'Height', 'area']
+    file_name = os.path.basename(img_test) # image file name
+    dict = {col_names[0]: file_name, col_names[1]: label, col_names[2]: x, col_names[3]: y, col_names[4]: w, col_names[5]: h, col_names[6]: area}
+    img_info_df = pd.DataFrame(dict, columns = col_names)
+    img_info_df['x2'] = img_info_df['x1'] + img_info_df['Width']
+    img_info_df['y2'], img_info_df['x3'] = img_info_df['y1'], img_info_df['x1']
+    img_info_df['y3'] = img_info_df['y1'] + img_info_df['Height']
+    img_info_df['x4'], img_info_df['y4'] = img_info_df['x2'], img_info_df['y3']
+    img_info_df = img_info_df[['img_file_name', 'type_of_object_str', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'Width', 'Height', 'area']]
+    return(img_info_df)
 
 
 def plot_img(original_img, segmented_img):
