2139a6595b25fe39071f9ec2b7207695a92a23d2
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-23 12:01:04 -0500

    added a function to find all the pixels and pixel intensities --error with this function

--- code/2_MockPNN/Postprocessing_functions.py
+++ code/2_MockPNN/Postprocessing_functions.py
@@ -25,3 +25,38 @@ def create_df(x,y,w,h, area, img_test, label):
     img_info_df = img_info_df[['img_file_name', 'type_of_object_str', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'xc', 'yc', 'Width', 'Height', 'area']]
     return img_info_df
 
+# draw a white rectangle filled using the coordinates from the csv
+from collections import Counter
+pix_list, locs_list, mean_pix_int_list  = [], [], []
+def all_pix_pnns(img_info_df, contour_img, original_img):
+    for box in range(len(img_info_df['x1'])):
+        print(box)
+        gray_image = np.full((1396,1860), 0, dtype=np.uint8) # new blank image so the original pix intensities are retained
+        rect = cv2.rectangle(gray_image, (img_info_df['x1'][box], img_info_df['y1'][box]), (img_info_df['x4'][box], img_info_df['y4'][box]), (255,255,255), -1) # draw white filled rect on the copy of the image
+        cv2.putText(contour_img, ('%d'%box), (img_info_df['x1'][box],img_info_df['y1'][box]), cv2.FONT_HERSHEY_SIMPLEX, 2, (125, 246, 55), 3)
+        # gray_seg_wfa = skimage.color.rgb2gray(contour_img)
+        # plot_img(gray_image, contour_img)
+        locs = np.argwhere(gray_image == 255)
+        print(locs.shape)
+        for i in range(locs.shape[0]):
+            for j in range(locs.shape[1] -1):
+                if gray_image[locs[i,j],locs[i,j+1]] == 255: # gray image has white filled boxes
+                    # print(original_img[locs[i,j],locs[i,j+1]])
+                    pix_list.append(original_img[locs[i,j],locs[i,j+1]]) # append all pix intensities of coordinates inside the PNN box
+        print("pix mean:", (np.array(pix_list)).mean()) # convert list to array and find the mean pix intensities
+        locs_list.append(locs) # append the all pixels of all PNNs detected
+        mean_pix_int_list.append((np.array(pix_list)).mean()) # and their mean intensities
+    print("Number of PNNs segmented:", len(locs_list))
+    print("lengths", len(locs_list), len(mean_pix_int_list))
+    img_info_df['pixels'] = locs_list
+    img_info_df['mean_pixel_int'] = mean_pix_int_list
+    img_info_df = img_info_df[['img_file_name', 'type_of_object_str', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'xc', 'yc', 'Width', 'Height', 'area', 'mean_pixel_int', 'pixels']]
+    fig = plt.figure(figsize = (5, 5))
+    plt.bar(list(range(len(img_info_df))), img_info_df['mean_pixel_int'], color = 'blue', width = 0.2)
+    plt.xticks(np.arange(0,len(img_info_df), 1), labels = list(range(len(img_info_df))))
+    plt.xlabel("Number of segmented PNNs")
+    plt.ylabel("Mean pixel intensities")
+    plt.title("Mean pixel intensities plot for segmented PNNs")
+    plt.show()
+    return contour_img, img_info_df, mean_pix_int_list # this returns a color image with PNN contours marked along with numbers
+
