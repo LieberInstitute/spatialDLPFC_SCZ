cfd05f4f20416fde1f70e70a630bf16f6c5db705
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-23 12:01:58 -0500

    shape detector function-- needs a little more changes

--- code/2_MockPNN/Postprocessing_functions.py
+++ code/2_MockPNN/Postprocessing_functions.py
@@ -60,3 +60,35 @@ def all_pix_pnns(img_info_df, contour_img, original_img):
     plt.show()
     return contour_img, img_info_df, mean_pix_int_list # this returns a color image with PNN contours marked along with numbers
 
+
+def detect_shape_pnns(contour_img, img_info_df, contours):
+    # color_img = skimage.color.gray2rgb(normalised_img)
+    shape = "unidentified"
+    for cnts in contours:
+        peri = cv2.arcLength(cnts, True) # c is the contour
+        approx = cv2.approxPolyDP(cnts, 0.04 * peri, True)# gray_seg_wfa = skimage.color.rgb2gray(contour_img)
+        c = max(cnts, key=cv2.contourArea) # get the area
+        x,y,w,h = cv2.boundingRect(c)
+        area_ = cv2.contourArea(cnts)
+        # print("peri, approx", peri, approx)
+        if 90 <= area_ <= 2500:
+            if len(approx) == 3:
+                shape = "triangle"
+                cv2.putText(contour_img, shape, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (125, 246, 55), 3)
+            elif len(approx) == 4:
+                (x, y, w, h) = cv2.boundingRect(approx)
+                ar = w / float(h)
+                shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
+                cv2.putText(contour_img, shape, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (125, 246, 55), 3)
+            elif len(approx) == 5:
+                shape = "pentagon"
+                cv2.putText(contour_img, shape, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (125, 246, 55), 3)
+            else:
+                shape = "circle"
+                cv2.putText(contour_img, shape, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (125, 246, 55), 3)
+        print("shape", shape)
+    # rect = cv2.rectangle(contour_img, (img_info_df['x1'][box], img_info_df['y1'][box]), (img_info_df['x4'][box], img_info_df['y4'][box]), (255,255,255), 3) # draw white filled rect on the copy of the image
+    # cv2.putText(contour_img, shape, (img_info_df['x1'][box], img_info_df['y1'][box]), cv2.FONT_HERSHEY_SIMPLEX, 3, (125, 246, 55), 3)
+    return approx, contours, shape, contour_img
+
+
