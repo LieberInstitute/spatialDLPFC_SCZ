33be34a31b02f1500b4c07c7b95e9b8c0acfc312
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-23 12:14:06 -0500

    function to convert manual annotations to python dfs to estimate the accurracy of the segmentations

--- code/2_MockPNN/Preprocessing_functions.py
+++ code/2_MockPNN/Preprocessing_functions.py
@@ -81,27 +81,27 @@ def histo(img, bins = 30, range = [0,1]):
 
 
 
-# img_arr = Image.open(img_test)
-# img_arr.seek(3)
-# img = np.array(img_arr, dtype = 'float32')
-# range = [img.min(),img.max()]
-# n, bins, patches = plt.hist(img.ravel(), bins = 30, range = range, facecolor='gray', align='mid') # (y, x, _)
-# order = np.argsort(n)[::-1]
-# print(order)
-# # print(" highest bins:", n[order][:10])
-# print("  their ranges:", [ (bins[i+1])   for i in order[:]]) #bins[i],
-# for i in order:
-#     if i >=0 and i<3:
-#         print("1",i, bins[i+1], img[img <= ([(bins[i+1])])])
-#         img[img <= ([(bins[i+1])])] = 0.0
-#         print("after 1",img[img <= ([(bins[i+1])])])
-#     elif i>=8:
-#         print("3",i, bins[i+1], img[img >= ([(bins[i+1])])])
-#         img[img >= ([(bins[i+1])])] = 0.0
-#         print("after 3", img[img >= ([(bins[i+1])])])
-#     elif i>=3 and i<8:
-#         print("2",i, bins[i+1], img[img >= ([(bins[i+1])])])
-#         # img[img >= ([(bins[i+1])])] = 1.0
+img_arr = Image.open(img_test)
+img_arr.seek(3)
+img = np.array(img_arr, dtype = 'float32')
+range = [img.min(),img.max()]
+n, bins, patches = plt.hist(img.ravel(), bins = 30, range = range, facecolor='gray', align='mid') # (y, x, _)
+order = np.argsort(n)[::-1]
+print(order)
+# print(" highest bins:", n[order][:10])
+print("  their ranges:", [ (bins[i+1])   for i in order[:]]) #bins[i],
+for i in order:
+    if i >=0 and i<3:
+        print("1",i, bins[i+1], img[img <= ([(bins[i+1])])])
+        img[img <= ([(bins[i+1])])] = 0.0
+        print("after 1",img[img <= ([(bins[i+1])])])
+    elif i>=8:
+        print("3",i, bins[i+1], img[img >= ([(bins[i+1])])])
+        img[img >= ([(bins[i+1])])] = 0.0
+        print("after 3", img[img >= ([(bins[i+1])])])
+    elif i>=3 and i<8:
+        print("2",i, bins[i+1], img[img >= ([(bins[i+1])])])
+        # img[img >= ([(bins[i+1])])] = 1.0
 #
 # fig,ax = plt.subplots(figsize = (20,20))
 # ax.imshow(img, cmap = 'gray')
@@ -127,3 +127,18 @@ def histo(img, bins = 30, range = [0,1]):
 #     xticks_labels = [ "{:.1f}\nto\n{:.1f}".format(value, bins[idx+1]) for idx, value in enumerate(bins[:-1])]
 #     # plt.xticks(xticks, labels = xticks_labels)
 #     plt.xlabel('Pixel intensities', fontsize=14)
+
+
+def manual_annot(filepath):
+    conv_factor = 2.0112375738 # the fiji annotations are measured in microns which need to be translated to pixels (1860/924.81)
+    df_manual_test = pd.read_csv(filepath) # read the manual annotations csv into dataframe
+    df_manual_test = df_manual_test.rename(columns = {'X': 'xc', 'Y': 'yc', 'BX': 'x1', 'BY': 'y1', 'Perim.': 'Perimeter'}) # xc,yc are the centroids of the BB
+    df_manual_test.loc[:,['xc']], df_manual_test.loc[:,['yc']], df_manual_test.loc[:,['x1']], df_manual_test.loc[:,['y1']], df_manual_test['Width'], df_manual_test['Height'] = df_manual_test['xc']*conv_factor, df_manual_test['yc']*conv_factor, df_manual_test['x1']*conv_factor, df_manual_test['y1']*conv_factor, df_manual_test['Width']*conv_factor, df_manual_test['Height']*conv_factor
+    df_manual_test['x2'] = (df_manual_test['x1'] + df_manual_test['Width'])
+    df_manual_test['y2'], df_manual_test['x3'] = df_manual_test['y1'], df_manual_test['x1']
+    df_manual_test['y3'] = (df_manual_test['y1'] + df_manual_test['Height'])
+    df_manual_test['x4'], df_manual_test['y4']  = df_manual_test['x2'], df_manual_test['y3'] # Calculating all 4 coordinates of the BB
+    df_manual_test['xc'], df_manual_test['yc'], df_manual_test['x1'], df_manual_test['y1'] = np.int0(np.ceil(df_manual_test['xc'])), np.int0(np.ceil(df_manual_test['yc'])), np.int0(np.ceil(df_manual_test['x1'])), np.int0(np.ceil(df_manual_test['y1'])) # convert x,y,bx,by from floating point to integers (doing it after, reduces round off errors)
+    df_manual_test['x2'], df_manual_test['y2'], df_manual_test['x3'], df_manual_test['y3'], df_manual_test['x4'], df_manual_test['y4'] = np.int0(np.ceil(df_manual_test['x2'])), np.int0(np.ceil(df_manual_test['y2'])), np.int0(np.ceil(df_manual_test['x3'])), np.int0(np.ceil(df_manual_test['y3'])), np.int0(np.ceil(df_manual_test['x4'])), np.int0(np.ceil(df_manual_test['y4']))
+    df_manual_test = df_manual_test[['Area', 'Perimeter', 'Mean', 'Min', 'Max', 'xc', 'yc', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3' , 'x4' , 'y4', 'Width', 'Height', 'Ch']] # rearranging the columns
+    return df_manual_test
