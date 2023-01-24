8f8f59e8ecd1c6cafe32a54d40e02207c891aba8
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-24 15:45:06 -0500

    modified the fucntions to read, preprocess and detect contours in the stitched image

--- code/RealPNN/Segmentations/Functions_stitched.py
+++ code/RealPNN/Segmentations/Functions_stitched.py
@@ -11,15 +11,15 @@ Channel4 = WFA
 def read_norm(filepath, ch_num):
     img = Image.open(filepath)
     img.seek(ch_num)
-    if ch_num == 2: # DAPI
-        dapi = cv2.normalize(np.array(img, dtype = 'uint8'), np.zeros(np.array(img, dtype = 'uint8').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
-        dapi_clr = skimage.color.gray2rgb((np.array((dapi * 255), dtype = np.uint8))) # convert to color to draw colored bb
-        return dapi, dapi_clr
     if ch_num == 1: # claudin
         img_claudin = cv2.normalize(np.array(img, dtype = 'uint8'), np.zeros(np.array(img, dtype = 'uint8').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
         img_claudin[img_claudin <= img_claudin.mean()] = 0.0
         img_claudin[img_claudin >= img_claudin.mean()] = 1.0
         return img_claudin
+    if ch_num == 2: # DAPI
+        dapi_clr = skimage.color.gray2rgb((np.array(img, dtype = np.uint8))) # convert to color to draw colored bb
+        dapi = cv2.normalize(np.array(img, dtype = 'uint8'), np.zeros(np.array(img, dtype = 'uint8').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
+        return dapi, dapi_clr
     if ch_num == 3: #NeuN
         img_neun = cv2.normalize(np.array(img, dtype = 'uint8'), np.zeros(np.array(img, dtype = 'uint8').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
         # neun_gray = skimage.color.rgb2gray(img_neun) # convert to gray to find contours
@@ -29,3 +29,11 @@ def read_norm(filepath, ch_num):
         # histo(img_arr,range = [img_arr.min(),img_arr.max()])
         img_wfa = cv2.normalize(img_arr, np.zeros(img_arr.shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
         return img_arr, img_wfa
+
+
+# detect contours in the normalised_img
+def detect_contours(normalised_img): ### create a separate function for shape detection and run it through this loop for contour detection
+    hierachy, img_threshold = cv2.threshold((np.array((normalised_img * 255), dtype = np.uint8)), 100, 255, cv2.THRESH_BINARY)
+    contours,_ = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
