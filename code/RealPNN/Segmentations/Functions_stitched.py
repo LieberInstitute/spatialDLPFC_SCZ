562ff5ef6c3dea95c5f9199a497656d876e74247
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-25 15:00:32 -0500

    modified the draw contours function for neun

--- code/RealPNN/Segmentations/Functions_stitched.py
+++ code/RealPNN/Segmentations/Functions_stitched.py
@@ -88,6 +88,83 @@ def draw_rect_dapi(labels, gray, dapi):
     return dpx, dpy, dpw, dph, area, ws_img_bb
 
 
+# draw the extracted contours onto the image
+from __future__ import print_function
+from skimage.feature import peak_local_max
+from skimage.segmentation import find_boundaries, watershed
+from scipy import ndimage
+import imutils
+def draw_contours(normalised_img, ch_num, contours = None,  color = None, thickness = None, dapi_clr = None):
+    if ch_num == 0:
+        shifted = cv2.pyrMeanShiftFiltering(dapi_clr, 21, 51) #dapi_clr
+        gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        D = ndimage.distance_transform_edt(thresh) # Euclidean distance from binary to nearest 0-pixel
+        localMax = peak_local_max(D, indices=False, min_distance=5, labels=thresh) # find the local maxima for all the individual objects
+        markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0] # 8-connectivity connected component analysis
+        labels = watershed(-D, markers, mask=thresh)
+        dpx, dpy, dpw, dph, area = [], [], [], [], []
+        for label in np.unique(labels):
+            if label == 0: # label marked 0 are background
+                continue
+            mask = np.zeros(gray.shape, dtype="uint8") # create masks that only have the detected labels as foreground and 0 as background
+            mask[labels == label] = 255
+            # detect contours in the mask and grab the largest one
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # detect the watershed contours
+            cnts = imutils.grab_contours(cnts) # extract only the contours
+            c = max(cnts, key=cv2.contourArea) # get the area
+            x,y,w,h = cv2.boundingRect(c) # BB coordinates
+            area1 = cv2.contourArea(c)
+            if area1 <= 500:
+                dpx.append(x)
+                dpy.append(y)
+                dpw.append(w)
+                dph.append(h)
+                area.append(cv2.contourArea(c))
+                ws_img_bb = cv2.rectangle(dapi_clr, (x,y), (x+w, y+h), (0,255,0), 1) # if a colored BB is not required then, change color to (0,0,0) and thickness to 1
+        return dpx, dpy, dpw, dph, area, ws_img_bb
+    elif ch_num == 3:
+        color_img = skimage.color.gray2rgb((np.array((normalised_img * 255), dtype = np.uint8)))
+        x, y, w, h, area = [],[],[],[],[]
+        for cnt in contours:
+            x_, y_, w_, h_ = cv2.boundingRect(cnt)
+            area_ = cv2.contourArea(cnt)
+            if area_ >= 1000:
+                contour_img = cv2.rectangle(color_img, (x_-10,y_-10), (x_+w_+10, y_+h_+10), (0,0,0), -1) # eliminating all the big objects
+            elif 90 <= area_ < 2500: # size threshold
+                # print(ax,ay,aw,ah)
+                x.append(x_)
+                y.append(y_)
+                w.append(w_)
+                h.append(h_)
+                area.append(area_)
+                bb_img = cv2.rectangle(color_img, (x_,y_), (x_+w_+10, y_+h_+10), color, thickness) #(255,0,0), 2-- to draw colored boxes
+                box = np.int0(cv2.boxPoints(cv2.minAreaRect(cnt)))
+                contour_img = cv2.drawContours(bb_img,[box],0,(0,0,0),1) # change the color and thickness here if contours need to be visible
+                # cv2.putText(contour_img, label, (x_,y_), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (125, 246, 55), 3)
+        return x, y, w, h, area, contour_img
+    else:
+        color_img = skimage.color.gray2rgb((np.array((normalised_img * 255), dtype = np.uint8)))
+        x, y, w, h, area = [],[],[],[],[]
+        for cnt in contours:
+            x_, y_, w_, h_ = cv2.boundingRect(cnt)
+            area_ = cv2.contourArea(cnt)
+            if area_ >= 100:
+                # area_ = cv2.contourArea(cnt)
+                # print(ax,ay,aw,ah)
+                x.append(x_)
+                y.append(y_)
+                w.append(w_)
+                h.append(h_)
+                area.append(area_)
+                bb_img = cv2.rectangle(color_img, (x_,y_), (x_+w_+10, y_+h_+10), color, thickness) #(255,0,0), 2-- to draw colored boxes
+                box = np.int0(cv2.boxPoints(cv2.minAreaRect(cnt)))
+                contour_img = cv2.drawContours(bb_img,[box],0,(0,0,0),1) # change the color and thickness here if contours need to be visible
+        return x, y, w, h, area, contour_img
+
+
+
+
 # plot the segmented and original image
 def plot_img(original_img, segmented_img):
     fig,ax = plt.subplots(nrows = 1, ncols = 2, figsize = (20,20))
