033e6de95076a9175c8f906a7717d5e319623113
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-01-31 11:39:51 -0500

    re-organizing folders to save segmentations of stitched images and bash logs

---
+++ code/RealPNN/Segmentations/Code/Segment_DAPI.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -cwd;
+#$ -pe local 10
+#$ -l caracol,mem_free=120G,h_vmem=120G,h_fsize=100G
+#$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/RealPNN/Segmentations/logs/Output_DAPI_seg.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/RealPNN/Segmentations/logs/Error__DAPI_seg.txt
+#$ -m be
+#$ -M uma.kaipa@libd.org
+
+
+ 
+echo "**** Job starts ****"
+date
+ 
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+
+### load the python environment
+eval "$(conda shell.bash hook)"
+conda activate venv_uma
+
+### python filename
+python 
+
+
+echo "**** Job ends ****"
+date
+
