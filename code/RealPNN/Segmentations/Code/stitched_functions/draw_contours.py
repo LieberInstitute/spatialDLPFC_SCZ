d9c6019601a8cc481f1f9c636f3a7ef736910ca7
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-02-02 11:14:17 -0500

    draw contour BBs around the segmented objects

--- code/RealPNN/Segmentations/Code/stitched_functions/draw_contours.py
+++ code/RealPNN/Segmentations/Code/stitched_functions/draw_contours.py
@@ -41,38 +41,6 @@ from collections import defaultdict
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-# extract the watershed algorithm labels
-def draw_rect_dapi(labels, gray, dapi):
-    dpx, dpy, dpw, dph, area = [], [], [], [], []
-    # print("1) entering the label loop")
-    for counter, label in enumerate(np.unique(labels)):
-        if label == 0: # label marked 0 are background
-            continue
-        mask = np.zeros(gray.shape, dtype="uint8") # create masks that only have the detected labels as foreground and 0 as background
-        mask[labels == label] = 255
-        # print("2) found the masks", counter)
-        # detect contours in the mask and grab the largest one
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # detect the watershed contours
-        # print("3) found contours", counter)
-        cnts = imutils.grab_contours(cnts) # extract only the contours
-        # print("4) grabbed contours",counter)
-        c = max(cnts, key=cv2.contourArea) # get the area
-        x,y,w,h = cv2.boundingRect(c) # BB coordinates
-        area1 = cv2.contourArea(c)
-        # print("5) found BB coordinates and area", counter)
-        if 2 >= area1 <= 500: # adding shape filter to filter out the smaller contours and the biggest cluter contours
-            # print("6) appending BB coordinates", counter)
-            dpx.append(x)
-            dpy.append(y)
-            dpw.append(w)
-            dph.append(h)
-            area.append(cv2.contourArea(c))
-            # print("7) appended")
-            ws_img_bb = cv2.rectangle(skimage.color.gray2rgb(dapi), (x,y), (x+w, y+h), (0,255,0), 1) # if a colored BB is not required then, change color to (0,0,0) and thickness to 1
-            # print("8) drawing rectangles")
-            print("drawing rectangle number",counter, "with area", area1)
-    return dpx, dpy, dpw, dph, area, ws_img_bb
-
 # draw the extracted contours onto the image
 def draw_contours(normalised_img, ch_num, contours = None,  color = None, thickness = None, dapi_clr = None):
     if ch_num == 1: #Claudin
