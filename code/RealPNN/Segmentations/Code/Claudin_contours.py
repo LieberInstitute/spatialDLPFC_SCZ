d760d0138f74bdbed71ae73e726c67d011bd0199
Uma Kaipa <ukaipa@compute-093.cm.cluster>
2023-02-08 11:32:43 -0500

    added code to segment all the images in the directory

--- code/RealPNN/Segmentations/Code/Claudin_contours.py
+++ code/RealPNN/Segmentations/Code/Claudin_contours.py
@@ -52,12 +52,13 @@ im_claudin = read_img.read_and_preprocess(img_C1, 1)
 plot_im(im_claudin)
 cla_contours = detect_contours.return_contours(im_claudin)
 clx,cly,clw,clh, cl_area, seg_cla = draw_contours.draw_detected_contours(im_claudin, 1, cla_contours , (255,0,0), 2)
-img_info_claudin = save_coordinates.create_df(clx,cly,clw,clh, cl_area, im_claudin, 'claudin')
+claudin_df = save_coordinates.create_df(clx,cly,clw,clh, cl_area, im_claudin, 'claudin')
 
 # claudin segmentations by detecting contours for all images in the directory
 for img_path in os.listdir(img_dir):
     if img_path.endswith(".tif"):
         im_claudin = read_img.read_and_preprocess(img_path, 1)
+        print("read", os.path.basename(img_path))
         # plot_im(im_claudin)
         cla_contours = detect_contours.return_contours(im_claudin)
         clx,cly,clw,clh, cl_area, seg_cla = draw_contours.draw_detected_contours(im_claudin, 1, cla_contours , (255,0,0), 2)
