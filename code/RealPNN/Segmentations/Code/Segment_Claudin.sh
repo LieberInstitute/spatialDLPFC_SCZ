2a290469fcfb66d5a5af8130fb23f02b72ee5e10
Uma Kaipa <ukaipa@jhpce01.cm.cluster>
2023-02-10 15:55:54 -0500

    submitted scripts for segmentations of all channels

--- code/RealPNN/Segmentations/Code/Segment_Claudin.sh
+++ code/RealPNN/Segmentations/Code/Segment_Claudin.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#$ -pe local 10
-#$ -l caracol,mem_free=120G,h_vmem=120G,h_fsize=100G
+#$ -l caracol,mem_free=120G,h_vmem=120G,h_fsize=10G
 #$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/RealPNN/Segmentations/logs/Output_Claudin_seg.txt
 #$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/RealPNN/Segmentations/logs/Error__Claudin_seg.txt
-#$ -m bea
+#$ -m be
 #$ -M uma.kaipa@libd.org
 
 
@@ -24,4 +23,4 @@ eval "$(conda shell.bash hook)"
 conda activate venv_segmentations
 
 ### python filename
-python Claudin.py
+python Claudin_contours.py
