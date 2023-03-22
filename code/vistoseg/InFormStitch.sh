34b6cf19fea51f4a0144e0b05c6876f2c16b2906
Uma Kaipa <ukaipa@compute-126.cm.cluster>
2023-03-22 12:57:35 -0400

    added bash file to re-run InformStitch on the NTC sample for troubleshooting the channel order mismatch

---
+++ code/vistoseg/InFormStitch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -pe local 5
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/Informstitch_output.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/Informstitch_error.txt
+#$ -m be
+#$ -M uma.kaipa.94@gmail.com
+
+
+ 
+echo "**** Job starts ****"
+date
+ 
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+
+module load matlab/R2019a
+
+toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib/'
+
+path1 = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/round1/20220814_VIF_PNN_S1_NTC/20220814_VIF_PNN_S1_NTC_Scan1_*_component_data.tif'
+O1 = extractMD(fname)
+filename1  = 'V12F14-053_test'
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), InFormStitch('$path1',O1,7,'$filename1')"
+
+echo "**** Job ends ****"
+date
+
