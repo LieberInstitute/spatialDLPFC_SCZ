fad13e81d5a28340c9016cc1f4c64a08e4186ed2
Uma Kaipa <ukaipa@compute-126.cm.cluster>
2023-03-23 15:48:26 -0400

    working on resolving the error with writing the multi-channel tiff with channels in the depth axis

--- code/RealPNN/Segmentations/Code/stitched_functions/multi_channel.py
+++ code/RealPNN/Segmentations/Code/stitched_functions/multi_channel.py
@@ -53,12 +53,12 @@ image_WFA = np.array(Image.open('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD41
 image_AF = np.array(Image.open('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/AF/AF_segmented_binary/V12F14-053_A1_af_contours_segmented.tif'))
 
 # Combine the images to create a multi-channel image
-multi_channel_image = np.stack((image_DAPI, image_claudin, image_NeuN, image_WFA, image_AF), axis = -1) # image_NeuN, image_WFA], axis=0
-
-# multi_channel_image = np.concatenate((image_DAPI[..., np.newaxis],
-#                                        image_claudin[..., np.newaxis],
-#                                        image_NeuN[..., np.newaxis],
-#                                        image_WFA[..., np.newaxis]), axis=-1)
+# multi_channel_image = np.stack((image_DAPI, image_claudin, image_NeuN, image_WFA, image_AF), axis = 2) # image_NeuN, image_WFA], axis=0
+multi_channel_image = np.concatenate((image_DAPI[..., np.newaxis],
+                                       image_claudin[..., np.newaxis],
+                                       image_NeuN[..., np.newaxis],
+                                       image_WFA[..., np.newaxis],
+                                      image_AF[..., np.newaxis]), axis=-1)
 
 
 # Set metadata for the channels
@@ -67,5 +67,6 @@ channel_names = ['DAPI', 'Claudin-5', 'NeuN', 'WFA', 'AF']
 metadata = {'axes': 'ZXY', 'channel_names': channel_names} #, 'channel_units': channel_units}
 
 # Save the multi-channel image with metadata
-tifffile.imwrite('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/multi_channel_image_A1_with_AF.tif', multi_channel_image, metadata=metadata) #,
+tifffile.imwrite('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/multi_channel_image_A1_with_AF.tif', multi_channel_image, imagej = True) #, metadata=metadata
+
 
