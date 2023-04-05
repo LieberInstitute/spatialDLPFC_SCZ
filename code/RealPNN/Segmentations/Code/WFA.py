c2fcbd222cbd379e7a836b8467c58a9eb058da1d
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-04-05 17:37:32 -0400

    changed the binarized image range

--- code/RealPNN/Segmentations/Code/WFA.py
+++ code/RealPNN/Segmentations/Code/WFA.py
@@ -124,7 +124,6 @@ fig.show()
 
 gray_segmented_wfa = cv2.cvtColor(wfa_cnt,cv2.COLOR_RGB2GRAY)
 thresh_segmented_wfa = cv2.threshold(gray_segmented_wfa, np.mean(gray_segmented_wfa), 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] #_INV
-# binary_segmented_wfa = cv2.normalize(np.array(thresh_segmented_wfa, dtype = 'uint8'), np.zeros(np.array(thresh_segmented_wfa, dtype = 'uint8').shape, np.double), 1.0, 0.0, cv2.NORM_MINMAX)
 # fig,ax = plt.subplots(figsize = (20,20))
 # ax.imshow(thresh_segmented_wfa, cmap = 'gray')
 # fig.show()
