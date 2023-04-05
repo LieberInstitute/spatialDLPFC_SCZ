a803e109946cf244ecb9b30e4286091d6fb0ca2e
Uma Kaipa <ukaipa@compute-098.cm.cluster>
2023-04-05 11:09:15 -0400

    submitted countnuclei for V12F14-053_A1 multi-channel tiff

--- code/vistoseg/countNuclei_V12F14-053_A1.sh
+++ code/vistoseg/countNuclei_V12F14-053_A1.sh
@@ -2,7 +2,7 @@
 #$ -l caracol,mem_free=100G,h_vmem=100G,h_fsize=10G
 #$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_output_V12F053_A1.txt
 #$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_error_V12F053_A1.txt
-#$ -m e
+#$ -m be
 #$ -M uma.kaipa.94@gmail.com
 
 
@@ -16,12 +16,12 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image.mat"
+echo "Sample id: /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image_Maddy.mat"
 echo "****"
 
 ## List current modules for reproducibility
 module matlab/R2019a
-mask='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image.mat'
+mask='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image_Maddy.mat'
 jsonname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/Br2719_A1/outs/spatial/scalefactors_json.json'
 posname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/Br2719_A1/outs/spatial/tissue_positions.csv'
 
