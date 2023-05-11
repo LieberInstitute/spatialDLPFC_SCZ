7b16a3b72392e06db028dcd51975d7e9051c04ad
Uma Kaipa <uma.kaipa@libd.org>
2023-05-11 11:51:06 -0400

    shell script to run countnuclei

--- code/vistoseg/countNuclei_V12F14-053_A1.sh
+++ code/vistoseg/countNuclei_V12F14-053_A1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l bluejay,mem_free=100G,h_vmem=100G,h_fsize=10G
+#$ -l mem_free=100G,h_vmem=100G,h_fsize=10G
 #$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_output_V12F053_A1.txt
 #$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_error_V12F053_A1.txt
 #$ -m be
@@ -16,16 +16,16 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image_Maddy.mat"
+echo "Sample id: /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_A1.mat"
 echo "****"
 
 ## List current modules for reproducibility
 module matlab/R2019a
-mask='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/A1_new_image_Maddy.mat'
-jsonname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/Br2719_A1/outs/spatial/scalefactors_json.json'
-posname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/Br2719_A1/outs/spatial/tissue_positions.csv'
+mask='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_A1.mat'
+jsonname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/V12F14-053_A1/outs/spatial/scalefactors_json.json'
+posname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/V12F14-053_A1/outs/spatial/tissue_positions.csv'
 
-toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_centroid/'
+toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_pixel_counts/'
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$mask','$jsonname','$posname')" 
 
 echo "**** Job ends ****"
