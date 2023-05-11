4d5dbed50a1a1ec6cdf5cba42058614b28e18d5d
Uma Kaipa <uma.kaipa@libd.org>
2023-05-11 11:51:53 -0400

    converted all 8 segmented multi-channel tiffs to mat files to run countnuclei on the samples

--- code/vistoseg/lib_centroid/tif_to_mat.m
+++ code/vistoseg/lib_centroid/tif_to_mat.m
@@ -1,7 +1,7 @@
-fname = 'path and name to your tif file';
+fname = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_D1.tif';
 DAPI = imread(fname,1);
 Claudin5 = imread(fname,2);
 NeuN = imread(fname,3);
 WFA = imread(fname,4);
 AF = imread(fname,5);
-save(fullfile('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/capture_area_segmentations/all_channels_segemented/Test/', 'V12F14-053_A1_thresholded.mat'), 'DAPI', 'Claudin5','NeuN','WFA','AF', '-v7.3')
+save(fullfile('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/', 'V12F14-053_D1.mat'), 'DAPI', 'Claudin5','NeuN','WFA','AF', '-v7.3')
