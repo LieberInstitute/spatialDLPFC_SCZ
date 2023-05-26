955e327f69429e16df421e490b02615f1285c99b
Boyi Guo <boyi.guo.work@gmail.com>
2023-05-26 17:23:33 -0400

    make UMAP colored by dx

--- code/visium_qc/DR_plots.R
+++ code/visium_qc/DR_plots.R
@@ -3,31 +3,36 @@ library(here)
 library(SpatialExperiment)
 library(scater)
 # library(pryr)                 # Check spe size
-library(spatialLIBD)
+# library(spatialLIBD)
 library(tidyverse)
 
 
-path_raw_spe <- here("processed-data/rds/spe",
-                     "01_build_spe/", "spe_raw.rds")
+path_spe_after_spot_qc <- here::here("processed-data", "rds", "spe", "spe_after_spot_qc.rds")
 
-raw_spe <- readRDS(
-  path_raw_spe
+
+spe <- readRDS(
+  path_spe_after_spot_qc
 )
 
-spe <- raw_spe
-# TODO: remove this
-spe <- logNormCounts(spe)
+# Create logcounts
 spe <- logNormCounts(spe)
 
-# TODO: how many proportion of the spots have missing
+spe$slide_num <- str_remove(spe$sample_id, "_[A-D][1]")
 
-
+spe$dx <- metadata(spe)$dx[match(spe$sample_id, metadata(spe)$sample_name)]
 
 
 # TODO: Create pca
 spe <- runPCA(spe) # TODO: edit this
 plotPCA(spe, color_by = "sample_id")
 
+plotPCA(spe, color_by = "slide_num")
+
+
+
 spe <- runUMAP(spe) # TODO: edit this
-plotUMAP(spe, color_by = "sample_id") +
-  scale_alpha()
+
+
+plotUMAP(spe, color_by = "sample_id") 
+plotUMAP(spe, color_by = "slide_num")
+plotUMAP(spe, color_by = "dx")
