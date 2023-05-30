5e4981ffc41eb9d290669116cbcf280be544580b
Boyi Guo <boyi.guo.work@gmail.com>
2023-05-29 20:16:31 -0400

    add vistoseg parameter and log files

---
+++ code/vistoseg/countNuclei_V12F14-053_C1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -l mem_free=100G,h_vmem=100G,h_fsize=10G
+#$ -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_output_V12F14-053.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/logs/countSpots_error_V12F14-053.txt
+#$ -m be
+#$ -M uma.kaipa.94@gmail.com
+
+
+echo "**** Job starts ****"
+date
+ 
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+echo "Sample id: /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_C1.mat"
+echo "****"
+
+## List current modules for reproducibility
+module matlab/R2019a
+mask='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_C1.mat'
+jsonname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/V12F14-053_C1/outs/spatial/scalefactors_json.json'
+posname='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/V12F14-053_C1/outs/spatial/tissue_positions.csv'
+
+toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_pixel_counts/'
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$mask','$jsonname','$posname')" 
+
+echo "**** Job ends ****"
+date
