2e25a4ed07fb8f219408472445b86bc7f61e9c6c
Uma Kaipa <uma.kaipa@libd.org>
2023-05-31 11:47:10 -0400

    removed the gene expression data for now

--- code/RealPNN/Samui/01_image_data.py
+++ code/RealPNN/Samui/01_image_data.py
@@ -19,3 +19,15 @@ img_channels = [
     'segmented_Claudin', 'segmented_NeuN', 'segmented_WFA'
 ]
 default_channels = {'blue': 'DAPI', 'red': 'WFA'}
+
+# default_gene = 'SNAP25' # we don't have one yet
+
+#   Names of continuous features expected to be columns in the observation
+#   data (colData) of the AnnData
+# spe_cont_features = ['PpTau', 'PAbeta']
+
+#   Diagnosis by brain number (not included in the sample_info sheet)
+# sample_dx = {
+#     'Br3854': 'AD', 'Br3873': 'AD', 'Br3880': 'AD', 'Br3874': 'control'
+# }
+
