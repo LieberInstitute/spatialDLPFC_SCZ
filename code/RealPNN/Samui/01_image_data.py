a1bfeb9a75e14308dfe1a1fc9b86f9d4f3898b65
Uma Kaipa <uma.kaipa@libd.org>
2023-05-31 11:48:36 -0400

    added paths to the raw image, json file and the output file for sample V12F14-053_A1

--- code/RealPNN/Samui/01_image_data.py
+++ code/RealPNN/Samui/01_image_data.py
@@ -31,3 +31,20 @@ default_channels = {'blue': 'DAPI', 'red': 'WFA'}
 #     'Br3854': 'AD', 'Br3873': 'AD', 'Br3880': 'AD', 'Br3874': 'control'
 # }
 
+sample_info_path = here(
+    'raw-data', 'experiment_info', 'VisiumSPG_PNN_Master.xlsx'
+) # /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/experiment_info/VisiumSPG_PNN_Master.xlsx
+
+# spe_path = here('processed-data', '16_samui', 'spe.h5ad')
+# notes_path = str(Path(here('code', '16_samui', 'feature_notes.md')).resolve())
+
+
+
+img_path = here('processed-data', 'RealPNN', 'all_channels_segemented', 'Test2', 'V12F14-053_A1.tif') #/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/RealPNN/all_channels_segemented/Test2/V12F14-053_A1.tif
+json_path = here(
+    'processed-data', 'spaceranger', 'V12F14-053_A1', 'outs', 'spatial',
+    'scalefactors_json.json'
+) # /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/spaceranger/V12F14-053_A1/outs/spatial/scalefactors_json.json
+
+out_dir = here('processed-data', 'Samui', 'V12F14-053_A1') # /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/Samui/V12F14-053_A1/
+
