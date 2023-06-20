20e1d80432b506e7f03b16aae09e32b649fdfe45
Boyi Guo <boyi.guo.work@gmail.com>
2023-06-20 11:28:53 -0400

    Set up code for Harmony batch correction

--- code/visium_qc/05-batch correction.R
+++ code/visium_qc/05-batch correction.R
@@ -1,3 +1,5 @@
+# Require more than 10 GB, running with 80GB
+
 # Load Packages -----------------------------------------------------------
 library(here)
 library(SpatialExperiment)
@@ -6,7 +8,7 @@ library(tidyverse)
 library(scran)
 library(scater)
 library(BiocParallel)
-# library(PCAtools)
+library(PCAtools)
 library(harmony)
 
 
@@ -28,13 +30,13 @@ spe <- logNormCounts(spe)
 ## From
 ## http://bioconductor.org/packages/release/bioc/vignettes/scran/inst/doc/scran.html#4_variance_modelling
 dec <- modelGeneVar(spe,
-                    block = spe$sample_id,
-                    BPPARAM = MulticoreParam(4)
+                    block = spe$sample_id#,
+                    # BPPARAM = MulticoreParam(4)
 )
 
 pdf(
   # TODO: change path
-  here::here("plots", "01_build_spe", "scran_modelGeneVar_final.pdf"),
+  here::here("plots", "01_build_spe", "test_scran_modelGeneVar_final.pdf"),
   useDingbats = FALSE
 )
 mapply(function(block, blockname) {
@@ -64,22 +66,20 @@ top.hvgs.fdr1 <- getTopHVGs(dec, fdr.threshold = 0.01)
 length(top.hvgs.fdr1)
 # [1] 5834 instead of 17830 for spatialDLPFC
 
-save(top.hvgs,
-     top.hvgs.fdr5,
-     top.hvgs.fdr1,
-     # TODO: change path
-     file = here::here("processed-data", "rdata", "spe", "01_build_spe", "top.hvgs_all.Rdata")
-)
+# save(top.hvgs,
+#      top.hvgs.fdr5,
+#      top.hvgs.fdr1,
+#      # TODO: change path
+#      file = here::here("processed-data", "rdata", "spe", "01_build_spe", "top.hvgs_all.Rdata")
+# )
 
 set.seed(020122)
 Sys.time()
-spe <- runPCA(spe, subset_row = top.hvgs, ncomponents = 50)
+spe <- runPCA(spe, subset_row = top.hvgs.fdr5 , ncomponents = 50)
 Sys.time()
 
 dim(reducedDim(spe, "PCA"))
-# 30243    50
-# Instead of DLPFC
-# 118793     50
+
 
 # make elbow plot to determine PCs to use
 percent.var <- attr(reducedDim(spe, "PCA"), "percentVar")
@@ -91,7 +91,7 @@ chosen.elbow
 
 pdf(
   # TODO: edit path
-  here::here("plots", "01_build_spe", "pca_elbow_final.pdf"),
+  here::here("plots", "01_build_spe", "test_pca_elbow_final.pdf"),
   useDingbats = FALSE
 )
 plot(percent.var, xlab = "PC", ylab = "Variance explained (%)")
@@ -100,8 +100,6 @@ dev.off()
 
 
 summary(apply(reducedDim(spe, "PCA"), 2, sd))
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
-# 0.9184  0.9343  0.9509  1.1872  1.0561  4.1944
 
 # RunUMAP
 set.seed(030122)
@@ -111,17 +109,15 @@ colnames(reducedDim(spe, "UMAP")) <- c("UMAP1", "UMAP2")
 Sys.time()
 
 # Run TSNE
-set.seed(030122)
-Sys.time()
-spe <-
-  runTSNE(spe,
-          dimred = "PCA",
-          name = "TSNE_perplexity80",
-          perplexity = 80
-  )
-Sys.time()
-# [1] "2021-12-15 15:30:48 EST"
-# [1] "2021-12-15 16:49:28 EST"
+# set.seed(030122)
+# Sys.time()
+# spe <-
+#   runTSNE(spe,
+#           dimred = "PCA",
+#           name = "TSNE_perplexity80",
+#           perplexity = 80
+#   )
+# Sys.time()
 
 
 # make plots of UMAP
@@ -135,25 +131,26 @@ Sys.time()
 #   labs(color = "Subject") +
 #   theme_bw()
 # dev.off()
-# 
-# pdf(file = here::here("plots", "01_build_spe", "UMAP_sample_id.pdf"))
-# ggplot(
-#   data.frame(reducedDim(spe, "UMAP")),
-#   aes(x = UMAP1, y = UMAP2, color = factor(spe$sample_id))
-# ) +
-#   geom_point() +
-#   labs(color = "sample_id") +
-#   theme_bw()
-# dev.off()
+
+pdf(file = here::here("plots", "01_build_spe", "test_UMAP_sample_id.pdf"))
+ggplot(
+  data.frame(reducedDim(spe, "UMAP")),
+  aes(x = UMAP1, y = UMAP2, color = factor(spe$sample_id))
+) +
+  geom_point() +
+  labs(color = "sample_id") +
+  theme_bw()
+dev.off()
 
 
 ### harmony batch correction
 spe <- RunHarmony(spe, "sample_id", verbose = F)
+set.seed(1)
 spe <- runUMAP(spe, dimred = "HARMONY", name = "UMAP.HARMONY")
 colnames(reducedDim(spe, "UMAP.HARMONY")) <- c("UMAP1", "UMAP2")
 
 # TODO: change path
-pdf(file = here::here("plots", "01_build_spe", "UMAP_harmony_sample_id.pdf"))
+pdf(file = here::here("plots", "01_build_spe", "test_UMAP_harmony_sample_id.pdf"))
 # Oh, my god, perfect match?
 # TODO: check the presentation for a subset of samples that shares
 #      different composition of morphology.
@@ -170,18 +167,23 @@ dev.off()
 ########### for bayesSpace
 
 ## do offset so we can run BayesSpace
-# auto_offset_row <- as.numeric(factor(unique(spe$sample_id))) * 100
-# names(auto_offset_row) <- unique(spe$sample_id)
-# spe$row <- colData(spe)$array_row + auto_offset_row[spe$sample_id]
-# spe$col <- colData(spe)$array_col
+auto_offset_row <- as.numeric(factor(unique(spe$sample_id))) * 100
+names(auto_offset_row) <- unique(spe$sample_id)
+# TODO: rename the row and col to show it is concatenated.
+spe$row <- colData(spe)$array_row + auto_offset_row[spe$sample_id]
+spe$col <- colData(spe)$array_col
 
 ## Set the BayesSpace metadata using code from
 ## https://github.com/edward130603/BayesSpace/blob/master/R/spatialPreprocess.R#L43-L46
-# metadata(spe)$BayesSpace.data <- list(platform = "Visium", is.enhanced = FALSE)
 
-# message("Running spatialCluster()")
+# TODO: to remove this after generating new data. doesn't needs this because dx_df becomes a element
+metadata(spe) <- list()
+metadata(spe)$BayesSpace.data <- list(platform = "Visium", is.enhanced = FALSE)
+
+message("Running spatialCluster()")
 
 # Sys.time()
 #TODO: change path
-save(spe, file = here::here("processed-data", "rdata", "spe", "01_build_spe", "spe_filtered_final.Rdata"))
+save(spe, file = here::here("processed-data", "rds", "spe",
+                            "01_build_spe", "test_spe_filtered_final.rds"))
 # Sys.time()
