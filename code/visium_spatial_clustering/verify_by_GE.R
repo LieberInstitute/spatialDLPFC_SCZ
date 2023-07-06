1beac6a043f47b36eb37b587b216615c7fcdfa3c
Boyi Guo <boyi.guo.work@gmail.com>
2023-07-05 20:48:30 -0400

    Use escheR to verify SpD

---
+++ code/visium_spatial_clustering/verify_by_GE.R
@@ -0,0 +1,35 @@
+library(SpatialExperiment)
+library(tidyverse)
+library(escheR)
+
+spe <- readRDS(
+  here("processed-data", "rds", "spe", "test_spe_bayesSpace_harmony.rds")
+)
+
+
+# Choose the SpD ----------------------------------------------------------
+
+spe$MBP <- logcounts(spe)["ENSG00000197971",]
+spe$spd <- factor(spe$bayesSpace_harmony_9 %in% c(2,3) , levels = c(TRUE))
+
+unique(spe$sample_id) |> 
+  map(
+    .f = function(id){
+      browser()
+      uni_spe <- spe[, spe$sample_id == id]
+      stopifnot(ncol(uni_spe)!=0)
+      
+      # uni_spe$bayesSpace_harmony_9 <- factor(uni_spe$bayesSpace_harmony_9)
+      # This is logcount
+      # make_escheR(uni_spe) |> 
+      #   add_fill(var = "bayesSpace_harmony_9")
+      
+      make_escheR(uni_spe) |> 
+        add_fill(var = "MBP") |> 
+        # TODO: check 
+        add_ground(var = "spd")
+    }
+   
+  )
+
+
