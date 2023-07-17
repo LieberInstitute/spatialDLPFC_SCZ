12ca4ef6397b11678214b8b2155c998e4c13018f
uma.kaipa <uma.kaipa@libd.org>
2023-07-17 14:11:10 -0400

    changed the code to convert mat to tif

--- code/vistoseg/lib_updated/tif2mat.m
+++ code/vistoseg/lib_updated/tif2mat.m
@@ -1,12 +1,8 @@
 function tif2mat(fname) %function to convert segmented tif file to mat format
 %fname is the path and name of the segmented tif file
-imgInfo = imfinfo(fname);
-N = size(imgInfo,1);
-
-for i=1:N, eval(sprintf('channel%d = imread(fname,i);', i)); end
-save([fname(1:end-4), '.mat'],'channel1','-v7.3')
-
-if N>2
-for i=2:N, save([fname(1:end-4), '.mat'],sprintf('channel%d',i),"-append"); end
-%output mat file is saved at the same location as the input tif file
+img = load(fname)
+O = fieldnames(img);
+%imwrite(mat2gray(img.(O{1})),[fname(1:end-4),'.tif'])
+for i = 2:numel(O)
+imwrite(mat2gray(img.(O{i})),[fname(1:end-4),'.tif'],'writemode', 'append')
 end
