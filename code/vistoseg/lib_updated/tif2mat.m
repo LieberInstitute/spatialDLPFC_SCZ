e1ab95d672533b4479b79af2a679d4f483c62396
uma.kaipa <uma.kaipa@libd.org>
2023-07-17 14:13:35 -0400

    changed the code to include the AF channel as well

--- code/vistoseg/lib_updated/tif2mat.m
+++ code/vistoseg/lib_updated/tif2mat.m
@@ -3,6 +3,6 @@ function tif2mat(fname) %function to convert segmented tif file to mat format
 img = load(fname)
 O = fieldnames(img);
 %imwrite(mat2gray(img.(O{1})),[fname(1:end-4),'.tif'])
-for i = 2:numel(O)
+for i = 1:numel(O)
 imwrite(mat2gray(img.(O{i})),[fname(1:end-4),'.tif'],'writemode', 'append')
 end
