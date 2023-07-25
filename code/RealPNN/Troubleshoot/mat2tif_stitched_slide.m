d0d6b49ee25dd3678b6215222eec313aaefce738
uma.kaipa <uma.kaipa@libd.org>
2023-07-25 09:42:05 -0400

    troubleshooting splitslide issues-converting the stitched mat file to tiff in matlab

--- code/RealPNN/Troubleshoot/mat2tif_stitched_slide.m
+++ code/RealPNN/Troubleshoot/mat2tif_stitched_slide.m
@@ -6,8 +6,8 @@ fname = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealP
 img = load(fname);
 O = fieldnames(img);
 
-% Assuming you want to save the first channel, change the index (3rd dimension) to 1
-channelToSave = 1;
+% Save the first channel, change the index (3rd dimension) to 1
+channelToSave = 1; % saving just the AF channel
 
 % Get the image data for the specified channel
 imageData = img.(O{1})(:,:,channelToSave);
