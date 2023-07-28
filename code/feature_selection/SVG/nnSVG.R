20a6ffd64c0f1f3c6aee577bc9c72d1ade7224d3
Boyi Guo <boyi.guo.work@gmail.com>
2023-07-28 15:31:55 -0400

    Set up nnSVG code

---
+++ code/feature_selection/SVG/nnSVG.R
@@ -0,0 +1,60 @@
+suppressPackageStartupMessages({
+  library(SpatialExperiment)
+  library(spatialLIBD)
+  library(here)
+  library(scater)
+  library(scran)
+  library(nnSVG)
+  library(dplyr)
+  library(tidyr)
+  library(ggplot2)
+  library(sessioninfo)
+})
+
+#TODO: replace this path
+spe <- readRDS(here::here(
+  "processed-data/rds/spe/01_build_spe/spe_raw.rds")
+)
+
+# Create vector of samples for nnSVG on whole tissue
+brains <- as.character(unique(spe$brnum))
+samples <- unique(spe$sample_id)
+# Run nnSVG once per sample whole tissue and store lists of top SVGs
+res_list <- as.list(rep(NA, length(samples)))
+
+names(res_list) <- samples
+
+for (s in seq_along(samples)) {
+  
+  # select sample_id
+  # ix <- spe$brnum == brains[s]
+  ix <- spe$sample_id == samples[s]
+  spe_sub <- spe[, ix]
+  
+  # run nnSVG filtering for mitochondrial gene and low-expressed genes
+  spe_sub <- filter_genes(spe_sub)
+  
+  # re-calculate logcounts after filtering
+  spe_sub <- logNormCounts(spe_sub)
+  
+  # run nnSVG
+  set.seed(12345)
+  message('running nnSVG')
+  spe_sub <- nnSVG(spe_sub, n_threads = 10)
+  
+  # store whole tissue results
+  message('saving data')
+  res_list[[s]] <- rowData(spe_sub)
+  temp = rowData(spe_sub)
+  save(temp, file = here::here("processed-data","07_Feature_selection", "nnSVG", paste0(samples[s], ".Rdata")))
+}
+
+# save whole tissue nnSVG results
+save(res_list, file = here::here("processed-data","07_Feature_selection", "nnSVG", "nnSVG.Rdata"))
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
