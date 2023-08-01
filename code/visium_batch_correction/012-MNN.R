91e744a44fa5e15caad4eefb2c09a4b5c740bdb3
Boyi Guo <boyi.guo.work@gmail.com>
2023-08-01 17:42:19 -0400

    set up the MNN run for batch correction.

--- code/visium_batch_correction/012-MNN.R
+++ code/visium_batch_correction/012-MNN.R
@@ -1,17 +1,32 @@
 library(batchelor)
 library(scuttle)
+library(here)
+library(SpatialExperiment)
+library(tidyverse)
+library(scran)
+library(scater)
+
+
+# Read in spe
+spe <- readRDS(here::here("processed-data", "rds", "spe",
+                          "spe_after_spot_qc.rds"))
+
 
 # Create a list of sample-specific SPEs
-spe_lst <- map()
+spe_lst <- unique(spe$sample_id) |> 
+  set_names() |> 
+  map(~spe[,spe$sample_id == .x])
 
 # Feed the list to multiBatchNorm
-multiBatchNorm()
+tmp <- multiBatchNorm(spe_lst)
+
+## What is the diff between `multiBatchNorm` and lognorm
+# TODO
 
 
 # Create modelGeneVar for each item of the list
 
-HVG_lst <- spe_lst |> map( .f = modelGeneVar) |> 
-  #TODO: flatten
+HVG_lst <- spe_lst |> map(.f = modelGeneVar)
 
 # Use combineVar to combine these variables
 combined.dec <- combineVar(HVG_lst)
@@ -21,7 +36,7 @@ chosen.hvgs <- getTopHVGs(combined.dec, n=5000)
 combined <- correctExperiments(spe_lst,
                                PARAM=NoCorrectParam()) # Note not corrected 
 
-library(scater)
+
 set.seed(100)
 combined <- runPCA(combined, subset_row=chosen.hvgs)
 combined <- runTSNE(combined, dimred="PCA")
@@ -30,7 +45,7 @@ plotTSNE(combined, colour_by="batch")
 
 # Fast MNN
 set.seed(101)
-f.out2 <- fastMNN(combined, batch=combined$batch, subset.row=chosen.hvgs)
+f.out <- fastMNN(combined, batch=combined$batch, subset.row=chosen.hvgs)
 
 
 set.seed(103)
