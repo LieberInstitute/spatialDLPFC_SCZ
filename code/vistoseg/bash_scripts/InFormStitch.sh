7eb397a1833ecece7455f67488633bda835da31a
uma.kaipa <uma.kaipa@libd.org>
2023-08-04 11:18:11 -0400

    ran informstitch for slide4 -- memory errors

--- code/vistoseg/bash_scripts/InFormStitch.sh
+++ code/vistoseg/bash_scripts/InFormStitch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -pe local 5
 #$ -l mem_free=80G,h_vmem=80G,h_fsize=20G
-#$ -o //dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_V12D07-334_output.txt
-#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_V12D07-334_error.txt
+#$ -o //dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_V13M06-279_output.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_V13M06-279_error.txt
 #$ -m be
 #$ -M uma.kaipa.94@gmail.com
 
@@ -19,7 +19,7 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 # echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id:  V12D07-334"
+echo "Sample id:  V13M06-279"
 echo "****"
 
 ## load MATLAB
@@ -30,9 +30,10 @@ toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_upd
 
 
 ## Read parameters
-path1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/round2/20230531_VSPG_PNN_Round2_Scan1_[5402,27279]_component_data.tif'
-fname='Channelorder_test_NTC'
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O{1} = 'DAPI'; O{2} = 'Claudin5'; O{3} = 'NeuN'; O{4} = 'WFA'; O{5} = 'AF'; InFormStitch('$filename',O,7,'$fname')"
+path1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/5_Real_PNN_Slide4/20230531_VSPG_PNN_Round2_Scan1_[5979,27982]_component_data.tif'
+
+fname1='V13M06-279'
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O{1} = 'DAPI'; O{2} = 'Claudin5'; O{3} = 'NeuN'; O{4} = 'WFA'; O{5} = 'AF'; InFormStitch('$path1',O,6,'$fname1')"
 
 echo "**** Job ends ****"
 date
