0306465571eb83ca003f01fe2c369263f7d73138
Boyi Guo <boyi.guo.work@gmail.com>
2023-08-07 21:03:14 -0400

    Pipeline for calculating SVG

--- code/feature_selection/SVG/nnSVG.R
+++ code/feature_selection/SVG/nnSVG.R
@@ -4,7 +4,9 @@ suppressPackageStartupMessages({
   library(here)
   library(scater)
   library(scran)
+  library(scuttle)
   library(nnSVG)
+  library(tidyverse)
   library(dplyr)
   library(tidyr)
   library(ggplot2)
@@ -13,50 +15,62 @@ suppressPackageStartupMessages({
 
 #TODO: replace this path
 spe <- readRDS(here::here(
-  "processed-data/rds/spe/01_build_spe/spe_raw.rds")
+  "processed-data/rds/spe/spe_after_spot_qc.rds")
 )
 
 # Create vector of samples for nnSVG on whole tissue
-samples <- unique(spe$sample_id)
-# Run nnSVG once per sample whole tissue and store lists of top SVGs
-res_list <- as.list(rep(NA, length(samples)))
-
-names(res_list) <- samples
+samples <- unique(spe$sample_id) 
 
 
+set.seed(12345)
+nnsvg_df <- unique(spe$sample_id) |> 
+    set_names() |> 
+  map_dfc(
+    .f = function(sample){
+      # browser()
+      # TODO: remove 1:5
+      spe_sub <- spe[, spe$sample_id == sample]
+      
+      # TODO: optional
+      spe_sub <- filter_genes(spe_sub)
+      # re-calculate logcounts after filtering
+      spe_sub <- logNormCounts(spe_sub)
+      
+      # TODO: edit number of thread
+      spe_sub <- nnSVG(spe_sub, n_threads = 1)
+      
+      nnsvg_df <- rowData(spe_sub) |> data.frame() |> 
+        select(gene_id, sigma.sq:padj) |> 
+        rename_all(~paste0(., "_", sample))
+      
+      tmp <- rowData(spe) |> data.frame() |> 
+        select(gene_id) |> rename_all(~paste0(., "_", sample)) |> 
+        left_join(
+          nnsvg_df, 
+          by = c(paste0("gene_id", "_", sample ))
+        )
+    }
+  )
 
-for (s in samples) {
-  
-  # select sample_id
-  ix <- spe$sample_id == s
-  spe_sub <- spe[, ix]
-  
-  # run nnSVG filtering for mitochondrial gene and low-expressed genes
-  spe_sub <- filter_genes(spe_sub)
-  
-  # re-calculate logcounts after filtering
-  spe_sub <- logNormCounts(spe_sub)
-  
-  # run nnSVG
-  set.seed(12345)
-  message('running nnSVG')
-  spe_sub <- nnSVG(spe_sub, n_threads = 10)
-  
-  # store whole tissue results
-  message('saving data')
-  res_list[[s]] <- rowData(spe_sub)
-  # temp = rowData(spe_sub)
-  # save(temp, file = here::here("processed-data","07_Feature_selection", "nnSVG", paste0(samples[s], ".Rdata")))
-}
-
-dir.create(
-  here("processed-data","feature_selection", "nnSVG"),
-  recursive = T, showWarnings = FALSE
-)
+# Check if merge is correct
+stopifnot(all(nnsvg_df$"gene_id_V12F14-053_A1" == nnsvg_df$"gene_id_V12F14-053_B1"))
 
 
 # save whole tissue nnSVG results
-saveRDS(res_list, file = here::here("processed-data","feature_selection", "nnSVG", "nnSVG.rdata"))
+nnsvg_fld <- here::here("processed-data","feature_selection",
+                        "nnSVG")
+
+dir.create(nnsvg_fld, showWarnings = FALSE,
+           recursive = TRUE)
+
+# Save a data frame that contains only the rowData from nnSVG
+# generated spe object
+saveRDS(
+  nnsvg_df, 
+  file = file.path(nnsvg_fld,
+                   # TODO: remove test
+                  "test_nnSVG.rds")
+)
 
 ## Reproducibility information
 print("Reproducibility information:")
