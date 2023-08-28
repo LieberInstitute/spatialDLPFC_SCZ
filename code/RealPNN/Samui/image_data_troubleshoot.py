0f620e130a344d4b69455559dbc444e4866bd1a7
uma.kaipa <uma.kaipa@libd.org>
2023-08-28 10:55:24 -0400

    not adding spaceranger or spot counts files for the raw image troubleshoot

--- code/RealPNN/Samui/image_data_troubleshoot.py
+++ code/RealPNN/Samui/image_data_troubleshoot.py
@@ -18,3 +18,13 @@ img_channels = ['Lipofuscin', 'Claudin5', 'DAPI', 'NeuN', 'WFA']
                 # 'segmented_DAPI',
     # 'segmented_Claudin', 'segmented_NeuN', 'segmented_WFA', 'segmented_Lipofuscin']
 default_channels = {'blue': 'DAPI', 'red': 'WFA'}
+
+master_excel_path = here('raw-data', 'experiment_info', 'VisiumSPG_PNN_Master.xlsx')
+img_path = here('processed-data', 'Samui', 'section_053_A1V12F14-053_A1.tif')
+json_path = here('processed-data', 'spaceranger', 'V12F14-053_A1', 'outs', 'spatial', 'scalefactors_json.json')
+tissue_positions_path = here('processed-data', 'spaceranger', 'V12F14-053_A1', 'outs', 'spatial', 'tissue_positions.csv')
+spot_counts_path = here('processed-data', 'spaceranger', 'V12F14-053_A1', 'outs', 'spatial', 'tissue_spot_counts_correct_counts.csv')
+
+
+# out_dir = here('processed-data', 'Samui', 'section_053_A1')
+out_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/Samui/section_053_A1_/'
