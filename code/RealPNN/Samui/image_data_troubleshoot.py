d07750eb2e429d2563fad308af0ccb887ab32574
uma.kaipa <uma.kaipa@libd.org>
2023-08-29 15:30:29 -0400

    overlaying spot info leads to error without the rest of the spaceranger data -- just adding the raw image into samui

--- code/RealPNN/Samui/image_data_troubleshoot.py
+++ code/RealPNN/Samui/image_data_troubleshoot.py
@@ -55,7 +55,7 @@ test_sample = Sample(name = 'V13M06-279_A1', path = out_dir)
 m_per_px = 4.211664472772355e-07
 
 # add the IF image for test sample
-test_sample.add_image(tiff = img_path, channels = img_channels, scale = m_per_px,
+test_sample.add_image(tiff = img_path, channels = img_channels, # scale = m_per_px,
     defaultChannels = default_channels)
 
 # write to the output directory
