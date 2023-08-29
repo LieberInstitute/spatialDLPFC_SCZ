47287044ffed251d05c352e4e0b117f0348b93d0
Boyi Guo <boyi.guo.work@gmail.com>
2023-08-29 15:58:33 -0400

    Update the script that automates the softlinks set up.
    It is now robust against naming convention changes of FASTQ files across different Visium experiments: 1v-shk or PNN-20v-shk

--- code/raw_data_process/01-set_fastq_softlink.R
+++ code/raw_data_process/01-set_fastq_softlink.R
@@ -1,19 +1,19 @@
-# Helper Function ---------------------------------------------------------
-check_fastq_files_exist <- function(
-    fastq_name_start,    # The start of fastq file name
-    fastq_fldr_path      # The sample-specific fastq folder
-) {
-  tmp_fastq_files <- grep( 
-    pattern = paste0(fastq_name_start,
-                     "[^/]*",
-                     "\\.fastq\\.gz$"),
-    x = list.files(fastq_fldr_path),
-    value = TRUE,
-    ignore.case = TRUE # 2 samples have SHK instead of shk
-  )
-  
-  return(length(tmp_fastq_files)==2)
-}
+# Helper Function (deprecated) ---------------------------------------------------------
+# check_fastq_files_exist <- function(
+#     fastq_name_start,    # The start of fastq file name
+#     fastq_fldr_path      # The sample-specific fastq folder
+# ) {
+#   tmp_fastq_files <- grep( 
+#     pattern = paste0(fastq_name_start,
+#                      "[^/]*",
+#                      "\\.fastq\\.gz$"),
+#     x = list.files(fastq_fldr_path),
+#     value = TRUE,
+#     ignore.case = TRUE # 2 samples have SHK instead of shk
+#   )
+#   
+#   return(length(tmp_fastq_files)==2)
+# }
 
 
 # Create Sample Specific Folder -------------------------------------------
@@ -29,30 +29,56 @@ stopifnot(dir.exists(processed_fastq_fldr))
 
 
 # Create A Softlink -------------------------------------------------------
-raw_all_fastq_files <- list.files(input_data_wh_folder, full.names = TRUE)
+# TODO: add new path to input_data_wh_folder in "file_pahts.R" when
+#       new data comes from seq core
+raw_all_fastq_files <- list.files(
+  input_data_wh_folder,
+  full.names = TRUE, recursive = TRUE)
+
 
 expr_meta |> 
-  pwalk(.f = function(fastq_fldr_path,
-                      fastq_name_start, ...){
-    
+  pwalk(.f = function(
+    fastq_fldr_path,
+    # fastq_name_start,
+    Experimenter,
+    sample_name,
+    `Sample #`,
+    ...){
     # Create Sample Directory
     mkdir_if_not_exist(fastq_fldr_path)
     
-    # Check if the soft link is already set up
-    if(check_fastq_files_exist(fastq_name_start, fastq_fldr_path))
-      return() # Do nothing
+    # Skip samples with softlink set up
+    if(length(list.files(fastq_fldr_path)) != 0){
+      return()
+    }
     
     raw_fast_paths <- grep(
-      pattern = paste0("/", fastq_name_start,
-                       "[^/]*",
-                       "\\.fastq\\.gz$"),
+      # TODO: ask someone to clean up this regex
+      pattern = paste0(
+        "[^/]*", # File path prefix
+        "/", `Sample #`, # Starts with /xxv or /PNN-xxv
+        # "(/|`/PNN-`)", `Sample #`, # Starts with /xxv or /PNN-xxv
+        "[-]", Experimenter, #e.g. `-shk` or `-SHK`
+        "[^/]*",
+        "\\.fastq\\.gz$"),
       x = raw_all_fastq_files,
-      value = TRUE,
+      ignore.case = TRUE,
+      value = TRUE
     )
     
+    
     # Error Prevention
-    # TODO: verify if it is true, there are 2 files
-    stopifnot(length(raw_fast_paths)==2)
+    
+    # Exceptions: shk for dACC project
+    # SHK did Visium experiment for dACC project too.
+    if(any(str_detect(raw_fast_paths, "dACC"))){
+      stop("Detected dACC sample in FASTQ files")
+    }
+    
+    if(length(raw_fast_paths)==0){
+      stop("No matched FASTQ file for Sample ", sample_name, 
+           " with Experiment # ", `Sample #`)
+    }
     
     # Create the command
     soft_copy_command <-
@@ -65,11 +91,11 @@ expr_meta |>
     system(soft_copy_command)
     
     # Validation step    
-    stopifnot(
-      check_fastq_files_exist(fastq_name_start, fastq_fldr_path)
-    )
+    # if there's at least 1 FASTQ in the folder
+    if(length(list.files(fastq_fldr_path)) == 0){
+      stop("Failed to set up soft link for Sample ", sample_name)
+    }
   })
-
 
 
 # Update folder permission
@@ -81,6 +107,8 @@ system(
         processed_fastq_fldr,
         sep = " ")
 )
+
+
 
 
 
