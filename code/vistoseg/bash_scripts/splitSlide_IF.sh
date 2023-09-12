bdcde1940743e855a01b6141460a445871bb531a
uma.kaipa <uma.kaipa@libd.org>
2023-09-12 15:20:59 -0400

    created a new bash file to run splitslide on slides 6-9

---
+++ code/vistoseg/bash_scripts/splitSlide_IF.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -pe local 10
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -o //dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide6_7_8_9_output.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide6_7_8_9_error.txt
+#$ -m be
+#$ -M uma.kaipa.94@gmail.com
+
+
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+# echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+echo "Sample id:  V13M06-281, V13M06-282, V13F27-293, V13F27-294"
+echo "****"
+
+## load MATLAB
+module load matlab/R2019a
+
+## Load toolbox for VistoSeg
+toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/'
+
+
+
+
