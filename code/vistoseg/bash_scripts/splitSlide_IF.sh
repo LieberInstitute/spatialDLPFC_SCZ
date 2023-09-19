38f85b8d8940d70dc0662b95699e812aa20ee431
uma.kaipa <uma.kaipa@libd.org>
2023-09-19 15:02:54 -0400

    submitted InFormStitch for slides 10-13

--- code/vistoseg/bash_scripts/splitSlide_IF.sh
+++ code/vistoseg/bash_scripts/splitSlide_IF.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -pe local 10
 #$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
-#$ -o //dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide6_7_8_9_output.txt
-#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide6_7_8_9_error.txt
+#$ -o //dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide_10_11_12_13_output.txt
+#$ -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slide_10_11_12_13_error.txt
 #$ -m be
 #$ -M uma.kaipa.94@gmail.com
 
@@ -19,7 +19,7 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 # echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id:  V13M06-281, V13M06-282, V13F27-293, V13F27-294"
+echo "Sample id:  V13F27-295 V13F27-296 V13M06-340 V13M06-342"
 echo "****"
 
 ## load MATLAB
@@ -29,25 +29,25 @@ module load matlab/R2019a
 toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/'
 
 
-## slide 6
-fname1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/7_Real_PNN_Slide6/V13M06-281.mat'
+## slide 10
+fname1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/11_Real_PNN_Slide10/V13F27-295.mat'
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname1')" 
-mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/7_Real_PNN_Slide6/V13M06-281*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
+mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/11_Real_PNN_Slide10/V13F27-295*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
 
 
-## slide 7
-fname2='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/8_Real_PNN_Slide7/V13M06-282.mat'
+## slide 11
+fname2='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/12_Real_PNN_Slide11/V13F27-296.mat'
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname2')" 
-mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/8_Real_PNN_Slide7/V13M06-282*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
+mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/12_Real_PNN_Slide11/V13F27-296*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
 
 
-## slide 8
-fname3='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/9_Real_PNN_Slide8/V13F27-293.mat'
+## slide 12
+fname3='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/13_Real_PNN_Slide12/V13M06-340.mat'
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname3')" 
-mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/9_Real_PNN_Slide8/V13F27-293*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
+mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/13_Real_PNN_Slide12/V13M06-340*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
 
-## slide 9
-fname4='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/10_Real_PNN_Slide9/V13F27-294.mat'
+## slide 13
+fname4='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/14_Real_PNN_Slide13/V13M06-342.mat'
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname4')" 
-mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/10_Real_PNN_Slide9/V13F27-294*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
+mv /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/14_Real_PNN_Slide13/V13M06-342*_* /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/
 
