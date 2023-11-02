8bdcf18f9bb7d2400d2234fecf4553ed77a8f688
Boyi Guo <boyi.guo.work@gmail.com>
2023-11-02 14:48:33 -0400

    Add code to create supp tab 1/2 about donor demographics

--- code/visium_qc/02_dx_descriptive.R
+++ code/visium_qc/02_dx_descriptive.R
@@ -1,14 +1,29 @@
 # Descriptive Table -------------------------------------------------------
 # TODO: move to an analysis
 library(gtsummary)
+library(SpatialExperiment)
 
 # expr_meta <- read_csv()
+# list.files(here::here("processed-data", "rds", 
+#                       "spe", "01_build_spe"))
 
 # TODO: readin spe object
 spe <- readRDS(here::here("processed-data", "rds", 
-                          "spe", "01_build_spe", "spe_raw.rds"))
+                          "spe", "01_build_spe", "test_spe_raw_36.rds"))
 
 demo_df <- metadata(spe)$dx_df
+
+# Error Prevention
+if("Br3942" %in% demo_df$brain_num){
+  warning("Br3942 is still in the dataset")
+  demo_df <- demo_df[which(demo_df$brain_num != "Br3942"), ]
+}
+stopifnot(nrow(demo_df) == 63) # Should remove Br3942
+
+# TODO: save the demo_df to an excel file
+
+# TODO: change variable names
+
 
 # Create Demo Table
 demo_df |> 
@@ -20,11 +35,12 @@ demo_df |>
   gtsummary::as_tibble() # |>
   # gt::gtsave(here("code", "visium_qc", "demo_tab.pdf"))
 
+# TODO: save Supplementary Table 2
 
-# # A tibble: 4 × 3
-# `**Characteristic**` `**ntc**, N = 4` `**scz**, N = 4`
-# <chr>                <chr>            <chr>           
-# 1 age                  39.4 (7.0)       42.0 (3.9)      
-# 2 sex                  NA               NA              
-# 3 F                    2 (50%)          1 (25%)         
-# 4 M                    2 (50%)          3 (75%)  
+
+# `**Characteristic**` `**ntc**, N = 17` `**scz**, N = 18`
+# <chr>                <chr>             <chr>            
+# 1 age                  46 (9)            47 (7)           
+# 2 sex                  NA                NA               
+# 3 F                    9 (53%)           8 (44%)          
+# 4 M                    8 (47%)           10 (56%)    
