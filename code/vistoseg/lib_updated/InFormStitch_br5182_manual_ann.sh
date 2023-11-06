d62a7fa6befabf72da38935e1019ac0c092eb8f8
uma.kaipa <uma.kaipa@libd.org>
2023-11-06 15:38:08 -0500

    updated the script and the bash file, submitted the file and got the stitched file, converted both stitched files to tif

--- code/vistoseg/lib_updated/InFormStitch_br5182_manual_ann.sh
+++ code/vistoseg/lib_updated/InFormStitch_br5182_manual_ann.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --mem=20G
 #SBATCH --job-name=InFormStitch_br5182_manual_annoataion_single_section_stitch
-#SBATCH -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_br5182.log
-#SBATCH -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_br5182.log
+#SBATCH -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_br5182_ouput.log
+#SBATCH -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/IS_br5182_error.log
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=uma.kaipa.94@gmail.com
 #SBATCH -c 5
@@ -25,7 +25,7 @@ echo "Job id: ${job_id}"
 echo "Job name: ${job_name}"
 echo "Node name: ${node_name}"
 echo "****"
-echo "Sample id:  br5182 br2039"
+echo "Sample id:  br2039"
 echo "****"
 
 ## load MATLAB
@@ -38,9 +38,9 @@ toolbox='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_upd
 ## Read parameters
 
 ## Br 5182
-path1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining/20220712_VIF_MockPNN_Strong_NTC_Scan1_*_component_data.tif'
-fname1='V12F14-053'
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O{1} = 'DAPI'; O{2} = 'Claudin5'; O{3} = 'NeuN'; O{4} = 'WFA'; O{5} = 'AF'; InFormStitch('$path1',O,7,'$fname1')"
+## path1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/20220712_VIF_MockPNN_Strong_NTC_C1_Br5182_MLtraining/20220712_VIF_MockPNN_Strong_NTC_Scan1_*_component_data.tif'
+## fname1='V12F14-053'
+## matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), O{1} = 'DAPI'; O{2} = 'Claudin5'; O{3} = 'NeuN'; O{4} = 'WFA'; O{5} = 'AF'; InFormStitch('$path1',O,7,'$fname1')"
 
 
 ## Br 2039
