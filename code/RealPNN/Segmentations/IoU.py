4680fea7dfca0f15f54f884bd955220de9cd0c49
uma.kaipa <uma.kaipa@libd.org>
2023-11-07 15:33:46 -0500

    save all the segmentation data

--- code/RealPNN/Segmentations/IoU.py
+++ code/RealPNN/Segmentations/IoU.py
@@ -31,6 +31,7 @@ img_test = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_
 Image.MAX_IMAGE_PIXELS = None
 source_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles_raw_no_annotations/' # folder where all the selected tiles exist
 dst_dir = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles_segmented_CV_IoU_test/' # where all the segmented tiles are located
+dst_dir_csv = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/2_MockPNN/Training_tiles_cv_segmentation_csv_files/' # where all the segmented tiles info is located
 
 # segment all the individual tiles
 for img_path in os.listdir(source_dir):
@@ -57,6 +58,7 @@ for img_path in os.listdir(source_dir):
         # wfa_cnt = cv2.drawContours(img_th_c, wfa_contours, -1, (0, 0, 0), 1) # yellow all contours
         area_ = []
         count_outside_range = 0
+        wfx, wfy, wfw, wfh, pnn_area = [], [], [], [], []
         for cnt in wfa_contours:
             x,y,w,h = cv2.boundingRect(cnt)
             area = cv2.contourArea(cnt)
@@ -65,15 +67,33 @@ for img_path in os.listdir(source_dir):
             # max, avg = max(area_), (sum(area_)/len(area_)).astype('uint8')
             if area<500: # ; area>=3000 and area>=3000
                 # print(f"Area {area} satisfies condition 1")
-                wfa_cnt = cv2.rectangle(img_th_c, (x,y), (x+w, y+h), (0,0,0), -1) # rectangle
+                wfa_cnt = cv2.rectangle(img_th_c, (x,y), (x+w, y+h), (0,0,0), -1) # black rectangle
             if area>=3000:
-                wfa_cnt = cv2.rectangle(img_th_c, (x,y), (x+w, y+h), (0,0,0), -1) # rectangle
+                wfa_cnt = cv2.rectangle(img_th_c, (x,y), (x+w, y+h), (0,0,0), -1) # black rectangle
             elif 300 <= area <= 3000:
                 # print(f"Area {area} satisfies condition 2")
                 wfa_cnt = cv2.rectangle(img_th_c, (x,y), (x+w, y+h), (0,0,255), 2) # red rectangle
+                wfx.append(x)
+                wfy.append(y)
+                wfw.append(w)
+                wfh.append(h)
+                pnn_area.append(area)
         # gray_segmented_wfa = cv2.cvtColor(img_th_c,cv2.COLOR_RGB2GRAY)
         # thresh_segmented_wfa = cv2.threshold(gray_segmented_wfa, 80, 255, cv2.THRESH_BINARY)[1] #_INV # | cv2.THRESH_OTSU
         cv2.imwrite(dst_dir + img_path.split('.')[0] + '_wfa__seg.tif', wfa_cnt)
+        # Populate the data in the dataframe
+        col_names = ['img_file_name','type_of_object', 'x1', 'y1', 'Width', 'Height', 'Area', 'total_number_pnns']
+        object_name = 'PNN' # name of the objects stored in the dataframe
+        file_name = os.path.basename(img_test) # image file name
+        dict = {col_names[0]: file_name, col_names[1]: object_name, col_names[2]: wfx, col_names[3]: wfy, col_names[4]: wfw, col_names[5]: wfh, col_names[6]: pnn_area, col_names[7]: len(wfx)}
+        df_wfa_cv = pd.DataFrame(dict, columns = col_names) 
+        df_wfa_cv['x2'] = df_wfa_cv['x1'] + df_wfa_cv['Width']
+        df_wfa_cv['y2'], df_wfa_cv['x3'] = df_wfa_cv['y1'], df_wfa_cv['x1']
+        df_wfa_cv['y3'] = df_wfa_cv['y1'] + df_wfa_cv['Height']
+        df_wfa_cv['x4'], df_wfa_cv['y4'] = df_wfa_cv['x2'], df_wfa_cv['y3']
+        df_wfa_cv = df_wfa_cv[['img_file_name', 'type_of_object', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'Width', 'Height', 'Area', 'total_number_pnns']]
+        df_wfa_cv.to_csv(dst_dir_csv + img_path.split('.')[0] + 'wfa_seg.csv')
+
 
 # match the tile numbers from annotation images to segmented images and overlay the boxes
 # draw a rectangle from the manual annotations csv on the contour detected image --> works to overlay annotations over segmentations
@@ -137,6 +157,47 @@ for part, (file1, file2) in matching_files.items():
 #4 - this gives figure a,b,c
 #5 - for the math,
 #6 - find the number of overlaps in all 13 ntc and 12 scz tiles
+def calculate_iou(box1, box2):
+    # Calculate the coordinates of the intersection rectangle
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+
+    # Calculate the area of the intersection rectangle
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+
+    # Calculate the areas of both bounding boxes
+    area_box1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area_box2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    # Calculate the IoU
+    iou = intersection_area / float(area_box1 + area_box2 - intersection_area)
+
+    return iou
+
+# Define the list of ground truth bounding boxes and predicted bounding boxes
+ground_truth_boxes = [(x1, y1, x2, y2), (x1, y1, x2, y2), ...]  # Replace with your actual values
+predicted_boxes = [(x1, y1, x2, y2), (x1, y1, x2, y2), ...]  # Replace with your actual values
+
+# Initialize a matrix to store the IoU values
+iou_matrix = np.zeros((len(ground_truth_boxes), len(predicted_boxes))
+
+# Calculate IoU for all pairs of ground truth and predicted bounding boxes
+for i, gt_box in enumerate(ground_truth_boxes):
+    for j, pred_box in enumerate(predicted_boxes):
+        iou_matrix[i, j] = calculate_iou(gt_box, pred_box)
+
+# Count how many ground truth boxes overlap with each predicted box
+gt_matches = np.sum(iou_matrix > threshold, axis=0)
+
+# Count how many predicted boxes overlap with each ground truth box
+pred_matches = np.sum(iou_matrix > threshold, axis=1)
+
+# You can then access the counts of matches for each predicted and ground truth box.
+# gt_matches[i] contains the number of ground truth boxes that overlap with predicted box i.
+# pred_matches[j] contains the number of predicted boxes that overlap with ground truth box j.
+
 #7 - find the number of overlaps in the zero PNN tiles as well
 #8 - find the IoUs for all of the overlaps
 #9 - find the avg of IoUs 
