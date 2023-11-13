fbd32cefb4a5ea080a3cef12d420f50c22bfe82e
uma.kaipa <uma.kaipa@libd.org>
2023-11-13 11:30:07 -0500

    running splitlside for slide 15

--- code/vistoseg/lib_updated/Splitslide_IF_slurm.sh
+++ code/vistoseg/lib_updated/Splitslide_IF_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --mem=50G
-#SBATCH --job-name=SplitSlide_slides_14_15_16
-#SBATCH -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slides_14_15_16_output_$SLURM_ARRAY_TASK_ID.log
-#SBATCH -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slides_14_15_16_error_$SLURM_ARRAY_TASK_ID.log
+#SBATCH --job-name=SplitSlide_slides_15
+#SBATCH -o /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slides_15_output.txt
+#SBATCH -e /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/code/vistoseg/lib_updated/logs/SS_slides_15_error.txt
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=uma.kaipa.94@gmail.com
 #SBATCH -c 5
@@ -25,7 +25,7 @@ echo "Job id: ${job_id}"
 echo "Job name: ${job_name}"
 echo "Node name: ${node_name}"
 echo "****"
-echo "Sample id:  V13M06-343 V13M06-344 V13F27-336"
+echo "Sample id:  V13M06-344"
 echo "****"
 
 ## load MATLAB
@@ -33,22 +33,22 @@ module load matlab
 
 
 ## slide 14
-fname1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/15_Real_PNN_Slide14/V13M06-343.mat'
+## fname1='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/15_Real_PNN_Slide14/V13M06-343.mat'
 
 
 ## slide 15
 fname2='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/16_Real_PNN_Slide15/V13M06-344.mat'
 
 ## slide 16
-fname3='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/17_Real_PNN_Slide16/V13F27-336.mat'
+## fname3='/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/raw-data/images/RealPNN/17_Real_PNN_Slide16/V13F27-336.mat'
 
 
-echo "splitting V13M06-343"
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname1')"
+## echo "splitting V13M06-343"
+## matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname1')"
 echo "splitting V13M06-344"
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname2')"
-echo "splitting V13M06-336"
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname3')"
+## echo "splitting V13M06-336"
+## matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide_IF('$fname3')"
 
 
 echo "**** Job ends ****"
