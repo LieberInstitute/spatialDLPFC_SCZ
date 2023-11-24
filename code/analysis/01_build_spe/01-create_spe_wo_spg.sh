3c2d9a31e929ebb86b885baa3b24461dc0718c39
Boyi Guo <boyi.guo.work@gmail.com>
2023-11-23 21:08:09 -0500

    SLURM bash file to run create spe

---
+++ code/analysis/01_build_spe/01-create_spe_wo_spg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name= 01-create_spe_wo_spg
+#SBATCH --mem=120G
+#SBATCH -n 1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bguo6@jhu.edu
+#SBATCH --output=logs/%x.txt
+#SBATCH --error=logs/%x.txt    # file to collect standard output
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_CLUSTER_NAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load Modules
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Run code
+Rscript 01-create_spe_wo_spg.R
+
+echo "**** Job ends ****"
+date
+
