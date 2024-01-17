93512e4094d4158ff8e11910444d3e5851054858
Boyi Guo <boyi.guo.work@gmail.com>
2024-01-17 17:32:21 -0500

    archive example code.

--- code/analysis/visium_spatial_clustering/archived/eg_cluster_annotation.R
+++ code/analysis/visium_spatial_clustering/archived/eg_cluster_annotation.R
@@ -12,8 +12,16 @@ spe <- fetch_data(type = "spatialDLPFC_Visium")
 
 colData(spe) |> names()
 
+# Error
+# spe$BayesSpace_harmony_09 <- factor(spe$BayesSpace_harmony_09)
+spe$BayesSpace_harmony_09 <- factor(
+  spe$BayesSpace_harmony_09,
+  labels = paste0("k09d", levels(spe$BayesSpace_harmony_09))
+)
+levels(spe$BayesSpace_harmony_09 )
+
 set.seed(1)
-scr_idx <- sample(unique(spe$sample_id), 15)
+scr_idx <- sample(unique(spe$sample_id), 5)
 tar_idx <- setdiff(unique(spe$sample_id), scr_idx)
 
 src_spe <- spe[,spe$sample_id %in% scr_idx]
@@ -22,7 +30,7 @@ tar_spe <- spe[,spe$sample_id %in% tar_idx]
 # Spatial Registration ----------------------------------------------------
 
 ## Get enrichment statistics from source ---------------------------------
-sce_modeling_results <- registration_wrapper(
+src_modeling_results <- registration_wrapper(
   sce = src_spe,
   # sce = src_spe,
   var_registration = "BayesSpace_harmony_09",
@@ -30,6 +38,16 @@ sce_modeling_results <- registration_wrapper(
   gene_ensembl = "gene_id",
   gene_name = "gene_name"
 )
+
+tar_modeling_results <- registration_wrapper(
+  sce = tar_spe,
+  # sce = src_spe,
+  var_registration = "BayesSpace_harmony_09",
+  var_sample_id = "sample_id",
+  gene_ensembl = "gene_id",
+  gene_name = "gene_name"
+)
+
 
 # Error in combn(colnames(registration_model)[regis_cols], 2) : n < m
 
