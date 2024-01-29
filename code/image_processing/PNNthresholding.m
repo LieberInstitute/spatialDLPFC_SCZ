912392f5b61a6ac62c476be1799db5619684fb09
madhavitippani <madhavitippani28@gmail.com>
2024-01-29 11:40:01 -0500

    try t-point thresholding in matlab

--- code/image_processing/PNNthresholding.m
+++ code/image_processing/PNNthresholding.m
@@ -1,2 +1,14 @@
- net = denoisingNetwork('DnCNN'); %Load the pretrained denoising convolutional neural network, 'DnCNN'.
- dWFA = denoiseImage(WFA,net); %denoise the image
+% net = denoisingNetwork('DnCNN'); %Load the pretrained denoising convolutional neural network, 'DnCNN'.
+% dWFA = denoiseImage(WFA,net); %denoise the image
+
+dt = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/';
+myfiles = dir([dt,'*1.mat']);
+
+for i = 1:length(myfiles)
+    % Read the image
+    load(fullfile(dt,myfiles(i).name),'WFA');
+
+[lehisto, x]=imhist(WFA);
+level=triangle_th(lehisto,x);
+
+end
