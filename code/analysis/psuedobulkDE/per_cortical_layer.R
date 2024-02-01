2707ab6cd002bb7494e6949ad8cf356e3bb12029
Boyi Guo <boyi.guo.work@gmail.com>
2024-02-01 15:10:15 -0500

    Fix the issue plotting individual genes.

--- code/analysis/psuedobulkDE/per_cortical_layer.R
+++ code/analysis/psuedobulkDE/per_cortical_layer.R
@@ -178,13 +178,12 @@ col_names <- colData(spe) |> names()
 spd_var<- grep("^PRECAST", col_names, value = TRUE)
 
 for(.spd_var in spd_var){
-  # .spd_var <- spd_var[[1]]
+  # .spd_var <- spd_var[[3]]
   
   
   
-  spe$spd <- spe[[.spd_var]]
-  spe$spd <- spe$spd |> 
-    factor(labels = paste0("SpD_", unique(spe$spd)))
+  spe$spd <- paste0("SpD_", spe[[.spd_var]]) |> 
+    factor()
   
   spd_list <- spe$spd |>
     levels() |> 
@@ -205,7 +204,9 @@ for(.spd_var in spd_var){
   spd_list |> 
     walk(
       .f = function(spe_sub){
-        # spe_sub <- spd_list[[1]]
+        browser()
+        
+        # spe_sub <- spd_list[[3]]
         # THis is taking the sum. The logcounts is CPM scale.
         sce_pseudo <-
           registration_pseudobulk(
@@ -217,8 +218,9 @@ for(.spd_var in spd_var){
         
         covars <- c("age", "sex")
         registration_mod <-
-          registration_model(sce_pseudo,
-                             covars = covars)
+          registration_model(
+            sce_pseudo,
+            covars = covars)
         
         block_cor <-
           registration_block_cor(
@@ -256,42 +258,81 @@ for(.spd_var in spd_var){
           results_enrichment, 
           aes(x = logFC_scz, y = -log10(fdr_scz))
         ) +
-          geom_point() +
-          geom_label_repel(
-            data = impl_gene_df, # Add labels last to appear as the top layer  
-            aes(label = gene),
-            force = 2,
-            nudge_y = 0.1) +
-          labs(
-            title = paste0(
-              "PB-", .spd_var,
-              "-", 
-              spe_sub$spd |> unique() |> as.character()
-            )
-          ) +
-          theme_minimal()) |> 
+            geom_point() +
+            geom_label_repel(
+              data = impl_gene_df, # Add labels last to appear as the top layer  
+              aes(label = gene),
+              force = 2,
+              nudge_y = 0.1) +
+            labs(
+              title = paste0(
+                "PB-", .spd_var,
+                "-", 
+                spe_sub$spd |> unique() |> as.character()
+              )
+            ) +
+            theme_minimal()) |> 
           print()
         
-        (data.frame(
-          dx = sce_pseudo$dx,
-          PVALB_log = logcounts(sce_pseudo)["ENSG00000100362",],
-          NOS1_log = logcounts(sce_pseudo)["ENSG00000089250",],
-          SST_log = logcounts(sce_pseudo)["ENSG00000157005",]#,
-          # CHODL_log = logcounts(sce_pseudo)["ENSG00000154645",]
-        ) |> 
-          pivot_longer(
-            cols = ends_with("_log")
-          ) |> 
-          ggplot() +
-          geom_violin(aes(x = dx, y = value)) +
-          facet_wrap(vars(name), scales = "free") +
-          labs(
-            title = paste0(
-              "PB-", .spd_var,
-              "-", 
-              spe_sub$spd |> unique() |> as.character()
-            )
-          )) |> print()
+        remain_gene_ids <- intersect(
+          c(
+            "ENSG00000100362",
+            "ENSG00000089250",
+            "ENSG00000157005",
+            "ENSG00000154645"
+          ), 
+          rowData(sce_pseudo)$gene_id
+        )
+        
+        ge_df <- remain_gene_ids |> 
+          map_dfc(~logcounts(sce_pseudo)[.x,])
+        
+        colnames(ge_df) <- paste0(
+          rowData(sce_pseudo)[remain_gene_ids, "gene_name"],
+          "_log"
+        ) 
+        ge_df <- cbind(
+          ge_df,
+          dx = sce_pseudo$dx
+        )
+        # 
+        # (data.frame(
+        #   dx = sce_pseudo$dx,
+        #   PVALB_log = logcounts(sce_pseudo)["ENSG00000100362",],
+        #   NOS1_log = logcounts(sce_pseudo)["ENSG00000089250",],
+        #   SST_log = logcounts(sce_pseudo)["ENSG00000157005",]#,
+        #   # CHODL_log = logcounts(sce_pseudo)["ENSG00000154645",]
+        # ) |> 
+        
+        if(ncol(ge_df) <=1){
+          (
+            ggplot() + 
+              labs(
+                title = paste0(
+                  "PB-", .spd_var,
+                  "-", 
+                  spe_sub$spd |> unique() |> as.character()
+                )
+              )
+          )|> print()
+        } else {
+          (
+            ge_df |> 
+              pivot_longer(
+                cols = ends_with("_log")
+              ) |> 
+              ggplot() +
+              geom_violin(aes(x = dx, y = value)) +
+              facet_wrap(vars(name), scales = "free") +
+              labs(
+                title = paste0(
+                  "PB-", .spd_var,
+                  "-", 
+                  spe_sub$spd |> unique() |> as.character()
+                )
+              )
+          ) |> print()
+        }
       }
     )
   
