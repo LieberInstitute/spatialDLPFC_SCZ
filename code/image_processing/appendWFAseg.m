4be550125ad28b99e39c951196c6e76645900c6d
madhavitippani <madhavitippani28@gmail.com>
2024-02-21 13:12:52 -0500

    initiate directories

--- code/image_processing/appendWFAseg.m
+++ code/image_processing/appendWFAseg.m
@@ -1,1 +1,10 @@
-copyfile /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/*1.tif /dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/image_processing/samui/
+ot = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/image_processing/samui/';
+dt = '/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/image_processing/WFAseg/';
+
+copyfile('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/processed-data/VistoSeg/captureAreas/*1.tif', ot)
+
+myfiles = dir([dt,'*WFAseg.mat']);
+for i = 1:numel(myfiles)
+    fname=fullfile(myfiles(i).folder, myfiles(i).name);
+    load(fname)
+    imwrite(BW,[fname1(1:end-4),'.tif'],'writemode', 'append')
