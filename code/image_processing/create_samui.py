660aa23bed0ac4722e37b109e7445ac3292aad1b
madhavitippani <madhavitippani28@gmail.com>
2024-02-22 13:46:43 -0500

    updated create_samui.py

--- code/image_processing/create_samui.py
+++ code/image_processing/create_samui.py
@@ -1,27 +1,26 @@
 from pathlib import Path
 import os
-os.chdir('/dcs04/lieber/lcolladotor/spatialHPC_LIBD4035/spatial_hpc/')
+os.chdir('/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/')
 from pyhere import here
 import json
-
-import scanpy as sc
-
-import numpy as np
-import pandas as pd
-from rasterio import Affine
 
 from loopy.sample import Sample
 from loopy.utils.utils import remove_dupes, Url
 
+import glob
+files = glob.glob(os.path.join('processed-data', 'image_processing', 'samui', '*.tif'))
 
-spot_diameter_m = 55e-6 # 5-micrometer diameter for Visium spot
-img_channels = ['DAPI', 'Alexa_488', 'Alexa_555', 'Alexa_594', 'Alexa_647', 'Autofluorescence', 'binaryWFA']
-default_channels = {'blue': 'DAPI', 'green': 'Alexa_488', 'yellow': 'Alexa_555', 'red': 'Alexa_594', 'magenta': 'Alexa647', 'cyan': 'Autofluorescence', 'white': 'binaryWFA'}
-
-sample_id = ''
-IMG_path = here('processed-data', 'image_processing', 'samui_test', sample_id+'.tif')
-
-this_sample = Sample(name = sample_id, path = out_dir)
-this_sample.add_image( tiff = img_path, channels = img_channels, scale = m_per_px, defaultChannels = default_channels)
-
-this_sample.write()
+#spot_diameter_m = 55e-6 # 5-micrometer diameter for Visium spot
+#m_per_px = 4.971263040387764e-07
+    
+for file in files[5:]:    
+    sample_id = os.path.basename(file)[:-4]
+    samui_dir = Path(here('processed-data', 'image_processing', 'samui', sample_id))
+    samui_dir.mkdir(parents = True, exist_ok = True)
+    img_channels = ['DAPI', 'NeuN', 'Claudin5', 'WFA', 'Autofluorescence', 'binaryWFA']
+    default_channels = {'blue': 'DAPI', 'yellow': 'NeuN', 'green': 'Claudin5', 'red': 'WFA', 'cyan': 'Autofluorescence', 'white': 'binaryWFA'}
+    img_path = here('processed-data', 'image_processing', 'samui', sample_id+'.tif')
+    this_sample = Sample(name = sample_id, path = samui_dir)
+    this_sample.add_image(tiff = img_path, channels = img_channels, scale = m_per_px, defaultChannels = default_channels)
+    this_sample.write()
+    print(sample_id)
