ff38ee82a787186986e15757c0fc6a9e63571180
Boyi Guo <boyi.guo.work@gmail.com>
2024-02-21 20:30:26 -0500

    Add code sections to only where the tears is at and outliers identified by spotSweeper.

--- code/analysis/visium_qc/11_spotSweeper.R
+++ code/analysis/visium_qc/11_spotSweeper.R
@@ -7,7 +7,7 @@ library(escheR)
 library(here)
 # library(scater)
 # library(spatialLIBD)
-# library(tidyverse)
+library(tidyverse)
 
 # File Paths --------------------------------------------------------------
 path_raw_spe <- here(
@@ -34,41 +34,91 @@ sample_info <- readxl::read_xlsx(
 
 artf_samples <- sample_info |> 
   select(SlideSerial, CaptureArea, hangnail, tear) |> 
-  filter(hangnail=="yes" | tear=="yes") |> 
+  filter(tear=="yes") |> 
   transmute(sample_id = paste0(SlideSerial, "_", CaptureArea)) |>
   pull(sample_id)
 
+artf_spots_key <- c()
 
 pdf(
-  here("plots/02_visium_qc/spotSweeper", "artifacts_hangneil+tears.pdf"),
+  here("plots/02_visium_qc/spotSweeper", "artifacts_tears.pdf"),
     width = 5.9, height = 3.9
   )
 for(.sample in artf_samples){
-  # Samples with tears
-  # .sample <- artf_samples[3]
-  # .sample <- "V13M06-282_B1"
+  # .sample <- artf_samples[[1]]
   sub_spe <- spe[, spe$sample_id==.sample]
-  
   sub_spe <- sub_spe[, sub_spe$in_tissue == 1]
-  # features <- c('sum_umi' ,'sum_gene', 'm')
-  # sub_spe<- localOutliers(
-  #   sub_spe, 
-  #   features=features,
-  #   n_neighbors=18, 
-  #   data_output=TRUE,
-  #   method="multivariate"
-  # )
-  # 
-  # make_escheR(sub_spe) |> 
-  #   add_fill(var = "sum_umi_log2", point_size=1.25) |>
-  #   add_ground(var = "local_outliers",point_size=1.25, stroke = 1) +
-  #   scale_color_manual(
-  #     name = "", # turn off legend name for ground_truth
-  #     values = c(
-  #       "TRUE" = "red",
-  #       "FALSE" = "transparent")
-  #   ) +
-  #   scale_fill_gradient(low ="white",high =  "darkgreen")
+  
+  sub_spe <- findArtifacts(
+    sub_spe,
+    mito_percent="expr_chrM_ratio",
+    mito_sum="expr_chrM",
+    n_rings=5,
+    name="artifact"
+  )
+  
+  print(
+    make_escheR(sub_spe) |> 
+    add_fill(var = "expr_chrM_ratio", point_size = 1.25) |> 
+    add_ground(var = "artifact", point_size=1.25, stroke = 0.3) +
+    scale_fill_gradient(low = "white", high = "black") +
+    scale_color_manual(
+      name = "Affected area", # turn off legend name for ground_truth
+      values = c(
+        "TRUE" = "red",
+        "FALSE" = "transparent")
+    ) +
+    labs(title = .sample)
+  )
+  # Warning message:
+  #   In .check_reddim_names(x, value, withDimnames) :
+  #   non-NULL 'rownames(value)' should be the same as 'colnames(x)' for
+  # 'reducedDim<-'. This will be an error in the next release of Bioconductor.
+  
+  artf_spots_key <- c(artf_spots_key, sub_spe$key[sub_spe$artifact == TRUE])
+  
+}
+dev.off()
+
+
+
+spe_clean_artifacts <- spe[, !(spe$key %in% artf_spots_key)]
+spe_clean_artifacts <- spe_clean_artifacts[, spe_clean_artifacts$in_tissue == TRUE]
+
+
+pdf(
+  here("plots/02_visium_qc/spotSweeper", "outliers.pdf"),
+  width = 5.9, height = 3.9
+)
+for(.sample in unique(spe_clean_artifacts$sample_id)){
+  # .sample <- unique(spe_clean_artifacts$sample_id)[1]
+  sub_spe <- spe_clean_artifacts[, spe_clean_artifacts$sample_id==.sample]
+  # sub_spe <- sub_spe[, sub_spe$in_tissue == 1]
+  
+  
+  features <- c('sum_umi' ,'sum_gene')
+  sub_spe<- localOutliers(
+    sub_spe,
+    features=features,
+    n_neighbors=18,
+    data_output=TRUE,
+    method="multivariate"
+  )
+
+  print(
+    make_escheR(sub_spe) |>
+    add_fill(var = "sum_umi_log2", point_size=1.25) |>
+    add_ground(var = "local_outliers",point_size=1.25, stroke = 1) +
+    scale_color_manual(
+      name = "", # turn off legend name for ground_truth
+      values = c(
+        "TRUE" = "red",
+        "FALSE" = "transparent")
+    ) +
+    scale_fill_gradient(low ="white",high =  "darkgreen")
+  )
+  
+  
   # 
   # make_escheR(sub_spe) |> 
   #   add_fill(var = "sum_gene_log2", point_size=1.25) |>
@@ -92,46 +142,21 @@ for(.sample in artf_samples){
   #   # ) +
   #   scale_fill_gradient(low ="white",high =  "darkgreen")
   
-  
-  sub_spe <- findArtifacts(
-    sub_spe,
-    mito_percent="expr_chrM_ratio",
-    mito_sum="expr_chrM",
-    n_rings=5,
-    name="artifact"
-  )
-  
-  print(
-    make_escheR(sub_spe) |> 
-    add_fill(var = "expr_chrM_ratio", point_size = 1.25) |> 
-    add_ground(var = "artifact", point_size=1.25, stroke = 0.3) +
-    scale_fill_gradient(low = "white", high = "black") +
-    scale_color_manual(
-      name = "Affected area", # turn off legend name for ground_truth
-      values = c(
-        "TRUE" = "red",
-        "FALSE" = "transparent")
-    ) +
-    labs(title = .sample)
-  
-  )
-  # Warning message:
-  #   In .check_reddim_names(x, value, withDimnames) :
-  #   non-NULL 'rownames(value)' should be the same as 'colnames(x)' for
-  # 'reducedDim<-'. This will be an error in the next release of Bioconductor.
-  
-  
 }
 dev.off()
 
 
 
-library(rbenchmark)
-benchmark(
-  `colData` = {colData(spe)[["sample_id"]]},
-  `dollar` = spe$sample_id,
-  replications = 100
-)
+
+
+
+
+# library(rbenchmark)
+# benchmark(
+#   `colData` = {colData(spe)[["sample_id"]]},
+#   `dollar` = spe$sample_id,
+#   replications = 100
+# )
 
 
 
