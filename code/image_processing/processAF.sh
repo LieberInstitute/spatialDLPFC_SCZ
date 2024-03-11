c026c74696c801e30843de3bc47a75079d657822
madhavitippani <madhavitippani28@gmail.com>
2024-03-11 14:53:47 -0400

    code to segment AF

---
+++ code/image_processing/processAF.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem=20G
+#SBATCH --job-name=AFseg
+#SBATCH -o logs/AFseg.txt 
+#SBATCH -e logs/AFseg.txt 
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"s
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## load MATLAB
+module load matlab/R2023a
+
+## Run WFAseg version 2 function
+matlab -nodesktop -nosplash -r "run('processAF.m'); exit" 
+
+echo "**** Job ends ****"
+date
+
