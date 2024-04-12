674963a5fe33872620af4d0f8c069386ab10188a
Boyi Guo <boyi.guo.work@gmail.com>
2024-04-12 15:09:05 -0400

    format

--- code/analysis/visium_spatial_clustering/post_PRECAST_spe_int.R
+++ code/analysis/visium_spatial_clustering/post_PRECAST_spe_int.R
@@ -1,4 +1,4 @@
-# Load Packages -----------------------------------------------------------
+# Load Packages ----
 suppressPackageStartupMessages({
   library(here)
   library(tidyverse)
@@ -9,26 +9,25 @@ suppressPackageStartupMessages({
 
 
 # Path --------------------------------------------------------------------
-fld_data_spatialcluster <- here(
-  "processed-data",
-  "rds", "spatial_cluster")
-
-path_precast_output <- file.path(
-  fld_data_spatialcluster, "PRECAST",
-  paste0("test_PRECASTObj_semi_inform",".rds")
-)
-
 path_spe <- file.path(
   here::here(
     "processed-data/rds/",
-    #TODO: replace this path
-    "test_spe_after_spot_qc_63.rds")
+    # TODO: replace this path
+    "test_spe_after_spot_qc_63.rds"
+  )
 )
 
-path_PRECAST_int_spe <- file.path(
-  fld_data_spatialcluster, "PRECAST",
-  paste0("test_spe_semi_inform",".rds")
+path_precast_output <- file.path(
+  here(
+    "processed-data", "rds", "spatial_cluster", "PRECAST",
+    paste0("test_PRECASTObj_semi_inform", ".rds")
+  )
 )
+
+# path_PRECAST_int_spe <- file.path(
+#   fld_data_spatialcluster, "PRECAST",
+#   paste0("test_spe_semi_inform", ".rds")
+# )
 
 
 # Load PRECAST Data -------------------------------------------------------
@@ -44,7 +43,7 @@ k_clus <- attr(PRECASTObj@resList, "para_settings")$K
 precast_resList <- map2(
   PRECASTObj@seulist,
   PRECASTObj@resList,
-  .f = function(seu_obj, res_obj){
+  .f = function(seu_obj, res_obj) {
     browser()
     names(seu_obj)
   }
@@ -53,45 +52,44 @@ precast_resList <- map2(
 sample_names <- names(PRECASTObj@seulist)
 sample_sizes <- sapply(PRECASTObj@seulist, FUN = ncol)
 
-spot_keys <- map(PRECASTObj@seulist, ~.x@meta.data$key)
+spot_keys <- map(PRECASTObj@seulist, ~ .x@meta.data$key)
 
 
 PRECAST_df <- map2(
   .x = PRECASTObj@resList,
   .y = k_clus,
   .f = function(
-    res,      # RECAST result for each k, as a list
-    k,         # Number of Clusters,
-    spot_keys
-  ){
-
+      res, # RECAST result for each k, as a list
+      k, # Number of Clusters,
+      spot_keys) {
     # Error Prevention: if sample size matches
-    if(
+    if (
       !all(
         sapply(res$cluster, length) == sapply(spot_keys, length)
       )
-    ){
+    ) {
       stop("Unconformatble dimensions")
     }
-      
+
     # browser()
 
-  # TODO: store the cluster results as a string with SpD_xx instead of a number
-  ret_vec <- map2(
-    .x = res$cluster,
-    .y = spot_keys,
-    .f = function(vec_cluster, vec_keys){
-      # browser()
-      vec_cluster <- vec_cluster |> as.vector() |> factor()
-      names(vec_cluster) <- vec_keys
-      return(vec_cluster)
-    }
-  ) |> list_c()
-  # browser()
-  # ret_vec <- paste("Clus", ret_vec) |> factor()
-  
-    ret_vec
+    # TODO: store the cluster results as a string with SpD_xx instead of a number
+    ret_vec <- map2(
+      .x = res$cluster,
+      .y = spot_keys,
+      .f = function(vec_cluster, vec_keys) {
+        # browser()
+        vec_cluster <- vec_cluster |>
+          as.vector() |>
+          factor()
+        names(vec_cluster) <- vec_keys
+        return(vec_cluster)
+      }
+    ) |> list_c()
+    # browser()
+    # ret_vec <- paste("Clus", ret_vec) |> factor()
 
+    ret_vec
   },
   spot_keys = spot_keys
 )
@@ -108,9 +106,9 @@ PRECAST_df_final2 <- PRECAST_df_final |> as.data.frame()
 spe <- readRDS(path_spe)
 
 col_data_df <- PRECAST_df_final2 |>
-   rownames_to_column(var = "key") |> 
-   right_join(
-     colData(spe) |> data.frame(),
+  rownames_to_column(var = "key") |>
+  right_join(
+    colData(spe) |> data.frame(),
     by = c("key"),
     relationship = "one-to-one"
   )
@@ -128,4 +126,3 @@ saveRDS(
 
 # SessionInfo -------------------------------------------------------------
 sessioninfo::session_info()
-
