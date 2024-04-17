4bde542b5df4350b111bbc6abc3d5c49c32dd479
Boyi Guo <boyi.guo.work@gmail.com>
2024-04-17 16:41:38 -0400

    rename + refactor + job file

---
+++ code/analysis/visium_spatial_clustering/plot_PRECAST_res.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=plot_PRECAST_res
+#SBATCH --mem=40G
+#SBATCH --time=7:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1            # Number of cores requested
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bguo6@jhu.edu
+#SBATCH --output=logs/%x.txt
+#SBATCH --error=logs/%x.txt    # file to collect standard output
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_CLUSTER_NAME}"
+
+## Load Modules
+module load conda_R/devel
+
+## List current modules for reproducibility
+module list
+
+## Run code
+Rscript plot_PRECAST_res.R
+
+
+## Estimate JHPCE Memory
+sstat -a -o MaxVMSize,AveVMSize,MaxRSS,AveRSS -j ${SLURM_JOB_ID}
+
+
+echo "**** Job ends ****"
+date
