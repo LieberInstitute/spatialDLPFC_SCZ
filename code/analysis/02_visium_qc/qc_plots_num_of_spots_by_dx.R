844a9dce66fa6673faaaf3f8d21ece8ea04e217d
Boyi Guo <boyi.guo.work@gmail.com>
2024-05-07 16:40:02 -0400

    create violin plots of QC metrics per sample

--- code/analysis/02_visium_qc/qc_plots_num_of_spots_by_dx.R
+++ code/analysis/02_visium_qc/qc_plots_num_of_spots_by_dx.R
@@ -9,141 +9,202 @@ suppressPackageStartupMessages({
 
 # Load Data ----
 ## Load Spe ----
-# Load Data ----
 spe <- readRDS(
   here::here(
     "processed-data", "rds", "01_build_spe",
-    "test_raw_spe_w_spg_N63_no_img.rds"
+    "raw_spe_wo_SPG_N63.rds"
   )
 )
 
+spe$dx <- metadata(spe)$dx_df$dx[
+  match(
+    spe$sample_id,
+    metadata(spe)$dx_df$sample_id
+  )
+]
+
+spe$brnum <- metadata(spe)$dx_df$subject[
+  match(
+    spe$sample_id,
+    metadata(spe)$dx_df$sample_id
+  )
+]
+
+spe$sample_id <- paste0(
+  spe$brnum
+)
+
+
 ## Load outlier keys ----
-outlier_spots_key <- readRDS(here("processed-data/visium_qc/outlier_key_ngb_36.rds"))
+
+tot_outlier_df <- readRDS(
+  here(
+    "processed-data/rds/02_visium_qc",
+    "combined_outlier_df.rds"
+  )
+)
 
 ## Merge data together ----
-# TODO: edit this section to make it more compatible with
-spe$outlier <- FALSE
-spe$outlier[spe$key %in% outlier_spots_key] <- TRUE
+spe$all_outlier <- tot_outlier_df[spe$key, "all_outlier"]
+spe$discard <- tot_outlier_df[spe$key, "remove"]
 
 
-# Remove out_tissue_spots ----
-spe <- spe[, spe$in_tissue==TRUE]
+## Remove out_tissue_spots ----
+spe <- spe[, spe$in_tissue == TRUE]
 
 # Plots ----
-## Raw data spots ----
-raw_col_df <- colData(spe) |> data.frame()
 
-in_tissue_spots_df <- raw_col_df |>
-  group_by(sample_id) |>
-  summarize(n_in_tissue = sum(in_tissue)) |>
-  left_join(
-    metadata(spe)$dx_df,
-    by = "sample_id"
-  )
-
-in_tissue_spots_df |>
-  ggplot(aes(x = dx, y = n_in_tissue, color = dx)) +
-  geom_violin() +
-  geom_boxplot() #+
-#  geom_jitter()
-
-
-## Outlier spots ----
-outlier_spots_df <- raw_col_df |>
-  filter(
-    in_tissue == TRUE,
-    outlier == TRUE
-  ) |>
-  group_by(sample_id) |>
-  summarize(n = n()) |>
-  left_join(
-    metadata(spe)$dx_df,
-    by = "sample_id"
-  )
-
-outlier_spots_df |>
-  ggplot(aes(x = dx, y = n, color = dx)) +
-  geom_violin() +
-  geom_boxplot() +
-  labs(
-    title = "# of Ouliters"
-  )
-
-
-## Post-qc spots ----
-post_qc_spots_df <- raw_col_df |>
-  filter(
-    in_tissue == TRUE,
-    outlier == FALSE
-  ) |>
-  group_by(sample_id) |>
-  summarize(n = n()) |>
-  left_join(
-    metadata(spe)$dx_df,
-    by = "sample_id"
-  )
-
-post_qc_spots_df |>
-  ggplot(aes(x = dx, y = n, color = dx)) +
-  geom_violin() +
-  geom_boxplot() +
-  labs(
-    title = "# of Kept Spots"
-  )
 
 # Per-sample distribution
-sample_id_by_dx <- metadata(spe)$dx_df |>
-  arrange(dx, sample_id)
+# sample_id_by_dx <- metadata(spe)$dx_df |>
+#   arrange(dx, sample_id)
 
 
 
 png(
   here("plots/02_visium_qc/post_qc_metric.png"),
-  width = 816, height = 352, units = "px"
-  # width = 8.5, height = 11/3, units = "in"
+  width = 816, height = 1056, units = "px"
+  # width = 7.5, height = 9, units = "in"
 )
-gridExtra::grid.arrange(
+# gridExtra::grid.arrange(
+ggpubr::ggarrange(
+  # cowplot::plot_grid(
   ## sum_umi
   plotColData(spe,
     x = "sample_id", y = "sum_umi",
-    color = "outlier",
+    color = "discard",
     point_size = 0.5
   ) +
     scale_y_log10() +
-    scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    facet_wrap(~ spe$dx, scales = "free_x") +
+    # scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    labs(
+      title = "Total UMIs",
+      y = "library size",
+      x = "sample"
+    ) +
     theme(
       axis.title.x = element_blank(),
-      axis.text.x = element_blank()
+      axis.text.x = element_blank(),
+      text = element_text(size = 20),
+      axis.text = element_text(size = 20),
+      legend.text = element_text(size = 20),
+      legend.justification = "center"
     ),
   # theme(axis.text.x = element_text(angle = 90, hjust = 1)),
 
-  ## sum_gene
+  # ## sum_gene
   plotColData(spe,
     x = "sample_id", y = "sum_gene",
-    color = "outlier",
+    color = "discard",
     point_size = 0.5
   ) +
-    scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    facet_wrap(~ spe$dx, scales = "free_x") +
     scale_y_log10() +
-    scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    # scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    labs(
+      title = "Detected genes",
+      y = "detected genes",
+      x = "sample"
+    ) +
     theme(
       axis.title.x = element_blank(),
-      axis.text.x = element_blank()
+      axis.text.x = element_blank(),
+      text = element_text(size = 20),
+      axis.text = element_text(size = 20)
     ),
-  # theme(axis.text.x = element_text(angle = 90, hjust = 1)),
+  # # theme(axis.text.x = element_text(angle = 90, hjust = 1)),
 
-  ## Mito_ratio
+  # ## Mito_ratio
   plotColData(spe,
     x = "sample_id", y = "expr_chrM_ratio",
-    color = "outlier",
+    color = "discard",
     point_size = 0.5
   ) +
-    scale_x_discrete(limits = sample_id_by_dx$sample_id) +
+    facet_wrap(~ spe$dx, scales = "free_x") +
+    # scale_x_discrete(limits = sample_id_by_dx$sample_id) +
     # scale_y_log10() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)),
-  ncol = 1
+    labs(
+      title = "Mitochondrial gene expression percentage",
+      y = "mito gene prop.",
+      x = "sample"
+    ) +
+    theme(
+      axis.title.x = element_blank(),
+      axis.text = element_text(size = 20),
+      text = element_text(size = 20),
+      axis.text.x = element_text(
+        size = 15, angle = 90, hjust = 1
+      )
+    ),
+  ncol = 1,
+  # heights = c(2, 2, 2.3),
+  # widths = c(1, 1, 0.7),
+  align = "v",
+  legend = "bottom",
+  common.legend = TRUE
 )
 dev.off()
+
+
+
+## Deprecated plots ----
+### Raw data spots ----
+# raw_col_df <- colData(spe) |> data.frame()
+
+# in_tissue_spots_df <- raw_col_df |>
+#   group_by(sample_id) |>
+#   summarize(n_in_tissue = sum(in_tissue)) |>
+#   left_join(
+#     metadata(spe)$dx_df,
+#     by = "sample_id"
+#   )
+
+# in_tissue_spots_df |>
+#   ggplot(aes(x = dx, y = n_in_tissue, color = dx)) +
+#   geom_violin() +
+#   geom_boxplot() #+
+# #  geom_jitter()
+### Outlier spots ----
+# outlier_spots_df <- raw_col_df |>
+#   filter(
+#     in_tissue == TRUE,
+#     outlier == TRUE
+#   ) |>
+#   group_by(sample_id) |>
+#   summarize(n = n()) |>
+#   left_join(
+#     metadata(spe)$dx_df,
+#     by = "sample_id"
+#   )
+
+# outlier_spots_df |>
+#   ggplot(aes(x = dx, y = n, color = dx)) +
+#   geom_violin() +
+#   geom_boxplot() +
+#   labs(
+#     title = "# of Ouliters"
+#   )
+### Post-qc spots ----
+# post_qc_spots_df <- raw_col_df |>
+#   filter(
+#     in_tissue == TRUE,
+#     outlier == FALSE
+#   ) |>
+#   group_by(sample_id) |>
+#   summarize(n = n()) |>
+#   left_join(
+#     metadata(spe)$dx_df,
+#     by = "sample_id"
+#   )
+
+# post_qc_spots_df |>
+#   ggplot(aes(x = dx, y = n, color = dx)) +
+#   geom_violin() +
+#   geom_boxplot() +
+#   labs(
+#     title = "# of Kept Spots"
+#   )
 
 # Session Info ----
 sessioninfo::session_info()
