414b55fe8abfa11bff2035b6763a1193db1ed6d5
madhavitippani <madhavitippani28@gmail.com>
2024-05-20 13:30:28 -0400

    create_samui.py

--- code/image_processing/create_samui.py
+++ code/image_processing/create_samui.py
@@ -13,15 +13,19 @@ import pandas as pd
 files = glob.glob(os.path.join('processed-data', 'image_processing', 'samui', '*.tif'))
 
 spot_diameter_m = 55e-6 # 5-micrometer diameter for Visium spot
-m_per_px = 4.971263040387764e-07
+m_per_px = 4.20390369911423e-07
     
-for file in files[1:]:    
+for file in files:    
     sample_id = os.path.basename(file)[:-4]
     samui_dir = Path(here('processed-data', 'image_processing', 'samui', sample_id))
     samui_dir.mkdir(parents = True, exist_ok = True)
-    img_channels = ['DAPI', 'NeuN', 'Claudin5', 'WFA', 'Autofluorescence', 'seg', 'seg1', 'seg2', 'seg3']
+    #img_channels = ['DAPI', 'NeuN', 'Claudin5', 'WFA', 'Autofluorescence', 'seg', 'seg1', 'seg2', 'seg3']
     #default_channels = {'blue': 'DAPI', 'yellow': 'NeuN', 'green': 'Claudin5', 'red': 'WFA', 'cyan': 'Autofluorescence', 'white': 'seg', 'white': 'seg1', 'white': 'seg2'}
-    default_channels = {'blue': 'DAPI', 'yellow': 'NeuN', 'red': 'WFA', 'white': 'seg', 'white': 'seg1', 'white': 'seg2', 'white': 'seg3'}
+    #default_channels = {'blue': 'DAPI', 'yellow': 'NeuN', 'red': 'WFA', 'white': 'seg', 'white': 'seg1', 'white': 'seg2', 'white': 'seg3'}
+    
+    img_channels = ['DAPI', 'NeuN', 'Claudin5', 'WFA', 'Autofluorescence', 'tpoint', 'ostus', 'adapT', 'AFmask', 'filt']
+    default_channels = {'red': 'WFA', 'white': 'tpoint', 'white': 'ostus', 'white': 'adapT' , 'white': 'AFmask', 'white':'filt'}
+    
     img_path = here('processed-data', 'image_processing', 'samui', sample_id+'.tif')
     tissue_positions_path = Path(here("processed-data", "spaceranger", sample_id, "outs", "spatial", "tissue_positions.csv"))
     tissue_positions = pd.read_csv(tissue_positions_path ,index_col = 0).rename({'pxl_row_in_fullres': 'y', 'pxl_col_in_fullres': 'x'},axis = 1)
