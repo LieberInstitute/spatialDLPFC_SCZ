ebeef55e7a7d7a2ee062d26e51cbf5cb0372d284
Boyi Guo <boyi.guo.work@gmail.com>
2024-07-01 08:52:07 -0400

    Making heatmap showing both spd registration and dx_diff magnitute.

---
+++ code/analysis/pseudobulk_dx/plot_heatmap_spd_reg+lanima_effect.r
@@ -0,0 +1,44 @@
+library(here)
+
+
+# Load Spd Registration
+source(
+  here(
+    "code/analysis/pseudobulk_dx",
+    "plot_sig_gene_spd_enrichment_w_laminar_diff.r"
+  )
+)
+
+
+# Load Effect Size plot
+source(
+  here(
+    "code/analysis/pseudobulk_dx/interaction_mdl",
+    "plot_heatmap_effect_size.R"
+  )
+)
+
+
+# Check if the order of the two heatmap is the same.
+stopifnot(
+  all(
+    heatmap_all@row_names_param$labels[heatmap_all |>
+      row_order() |>
+      unlist() |>
+      c()] ==
+      effect_heatmap@row_names_param$labels[effect_heatmap |>
+        row_order() |>
+        unlist()]
+  )
+)
+
+
+pdf(
+  here(
+    "plots/PB_dx_genes",
+    "test_Heatmap_spd_reg_N_effect_size.pdf"
+  ),
+  height = 20
+)
+heatmap_all + effect_heatmap
+dev.off()
