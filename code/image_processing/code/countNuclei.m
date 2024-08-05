9d46b06267134004acc817d9331e2e9a443e5cc8
madhavitippani <madhavitippani28@gmail.com>
2024-08-05 17:48:59 -0400

    update countNuclei.m to count claudin5 segmentation and append to existing spot_counts csv

--- code/image_processing/code/countNuclei.m
+++ code/image_processing/code/countNuclei.m
@@ -1,13 +1,17 @@
-%mask = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/Images/Liebert_Institute_OTS-20-7748_rush_posterior_2_nuclei.mat';
+ %mask = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/Images/Liebert_Institute_OTS-20-7748_rush_posterior_2_nuclei.mat';
 %jsonname = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/outputs/NextSeq/DLPFC_Br3942_post_manual_alignment/outs/spatial/scalefactors_json.json';
 %posname = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/outputs/NextSeq/DLPFC_Br3942_post_manual_alignment/outs/spatial/tissue_positions_list.csv';
 
 function [count,prop,countC,inten] = countNuclei(mask,img,jsonname,posname,~) 
 
 disp('loading data')
- img = load(img, 'DAPI', 'NeuN', 'WFA');
- BW = load(mask, 'DAPI', 'NeuN', 'WFA');
- O = fieldnames(BW);
+% img = load(img, 'DAPI', 'NeuN', 'WFA');
+% BW = load(mask, 'DAPI', 'NeuN', 'WFA');
+ 
+img = load(img, 'Claudin5');
+BW = load(mask, 'Claudin5');
+
+O = fieldnames(BW);
    
 [posPath,~] = fileparts(posname);
 
@@ -17,17 +21,20 @@ tbl = readtable(posname) ;
 count = [];
 prop = [];
 
-    if size(tbl, 2) > 6
-        b = 7;
-        for C = 1:numel(O)
-        count.(O{C}) = table2array(tbl(:, b));
-        prop.(O{C}) = table2array(tbl(:, b+1));
-        countC.(O{C}) = table2array(tbl(:, b+2));
-        inten.(O{C}) = table2array(tbl(:, b+3));
-        b = b+4;
-        end
+  % if size(tbl, 2) > 6
+  %     b = 7;
+  %     for C = 1:numel(O)
+  %     count.(O{C}) = table2array(tbl(:, b));
+  %     prop.(O{C}) = table2array(tbl(:, b+1));
+  %     countC.(O{C}) = table2array(tbl(:, b+2));
+  %     inten.(O{C}) = table2array(tbl(:, b+3));
+  %     b = b+4;
+  %     end
+	if size(tbl, 2) > 18
+			disp("file error")
     else
-       tbl.Properties.VariableNames = {'barcode','tissue','row','col','imagerow','imagecol'};
+       %tbl.Properties.VariableNames = {'barcode','tissue','row','col','imagerow','imagecol'};
+	   tbl.Properties.VariableNames = {'barcode','tissue','row','col','imagerow','imagecol','NDAPI','PDAPI','IDAPI','CNDAPI','NNeuN','PNeuN','INeuN','CNNeuN','NWFA','PWFA','IWFA','CNWFA'}
        [count,prop,countC,inten] = countSpots_centroid(BW, img, R, tbl, posPath);
         
     end
