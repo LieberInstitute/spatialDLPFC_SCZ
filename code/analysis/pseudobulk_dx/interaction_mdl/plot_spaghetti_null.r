18a476d44dcd1167a31ed711db945a3d683c8c94
Boyi Guo <boyi.guo.work@gmail.com>
2024-08-21 13:49:41 -0400

    Add plots showing House keeping genes an dus house keeping genes as background.

--- code/analysis/pseudobulk_dx/interaction_mdl/plot_spaghetti_null.r
+++ code/analysis/pseudobulk_dx/interaction_mdl/plot_spaghetti_null.r
@@ -7,20 +7,48 @@ suppressPackageStartupMessages({
 })
 
 # Load data ----
-## LogFC mat
-log2fc_mat <- readRDS(
+## Synthetic null
+null_mat <- readRDS(
   here(
     "processed-data/PB_dx_genes/interaction",
     "test_permed_laminar_specific_log2FC.rds"
   )
 ) |> slice_tail(n = 50)
 
+## LogFC mat
+log2fc_mat <- readRDS(
+  here(
+    "processed-data/PB_dx_genes/interaction",
+    "test_laminar_specific_log2FC.rds"
+  )
+)
+
+gene_172_ensembl <- read_csv(
+  here(
+    "processed-data/PB_dx_genes/",
+    "test_PRECAST_07.csv"
+  )
+) |>
+  filter(fdr_scz <= 0.10) |>
+  pull(ensembl)
+
+log2fc_mat_172 <- log2fc_mat |> filter(ensembl %in% gene_172_ensembl)
+
+
+
+
 # Make spaghetti plot -----
-make_spaghetti_plot <- function(
+make_spaghetti_plot_null <- function(
     fc_mat,
+    bg_mat,
     bg_genes,
     genes,
     .center = FALSE) {
+  fc_mat <- rbind(
+    fc_mat,
+    bg_mat
+  )
+
   if (.center) {
     spd_comp <- fc_mat |>
       select(starts_with("spd"))
@@ -52,7 +80,7 @@ make_spaghetti_plot <- function(
     geom_hline(aes(yintercept = 0), linetype = 2, alpha = 0.4) +
     # Create transparent paths for background genes
     geom_line(
-      aes(color = "Background genes"),
+      aes(color = "Synthesized"),
       data = fc_mat_long |> filter(gene %in% bg_genes),
       alpha = 0.4, linewidth = 0.5
     ) +
@@ -72,7 +100,7 @@ make_spaghetti_plot <- function(
     ) +
     scale_color_manual(
       values = c(
-        "Background" = "gray",
+        "Synthesized" = "gray",
         "Sig." = "red"
       )
     ) +
@@ -86,24 +114,240 @@ make_spaghetti_plot <- function(
   return(ret_p)
 }
 
-treg_genes <- c("AKT3", "MALAT1", "ARID1B")
+# treg_genes <- c("AKT3", "MALAT1", "ARID1B")
 
 
 
-make_spaghetti_plot(
+
+pdf(
+  here(
+    "plots/PB_dx_genes/spaghetti_plot",
+    "spatially_divergent_genes_syn_background.pdf"
+  ),
+  height = 5
+)
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat_172,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "HNRNPH3",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat_172,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "ALDH1A1",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat_172,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "SOD2",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat_172,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "CCNI",
+  .center = FALSE
+) |> print()
+dev.off()
+
+
+# Debug why some permuted genes still have very large logFC
+# ENSG00000228543
+
+
+make_spaghetti_plot_null(
   fc_mat = log2fc_mat,
-  bg_genes = log2fc_mat$gene,
-  genes = "",
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "AC003684.1",
   .center = FALSE
 )
 
-make_spaghetti_plot(
+make_spaghetti_plot_null(
   fc_mat = log2fc_mat,
-  bg_genes = log2fc_mat$gene,
-  genes = "",
-  .center = TRUE
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "TOM1L1",
+  .center = FALSE
 )
 
+
+# Housekeeping genes ----
+# Ref: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2950262/
+# Note: "PBDG" are not in the data set
+
+
+pdf(
+  here(
+    "plots/PB_dx_genes/spaghetti_plot",
+    "spatially_divergent_genes_showing House Keeping gene.pdf"
+  ),
+  height = 5
+)
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "GUSB",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "PPIA",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "UBC",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "SDHA",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "ACTB",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "TBP",
+  .center = FALSE
+) |> print()
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "B2M",
+  .center = FALSE
+) |> print()
+
+
+make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = null_mat$gene,
+  genes = "GAPDH",
+  .center = FALSE
+) |> print()
+dev.off()
+
+
+
+
+
+pdf(
+  here(
+    "plots/PB_dx_genes/spaghetti_plot",
+    "spatially_divergent_genes_HK_background.pdf"
+  ),
+  height = 5
+)
+print(
+  make_spaghetti_plot_null(
+    fc_mat = log2fc_mat,
+    bg_mat = null_mat,
+    bg_genes = c("GUSB", "PPIA", "UBC", "SDHA", "ACTB", "TBP", "B2M", "GAPDH"),
+    genes = "HNRNPH3",
+    .center = FALSE
+  ) +
+    scale_color_manual(
+      values = c(
+        "Synthesized" = "gray",
+        "Sig." = "red"
+      ),
+      labels = c(
+        "Synthesized" = "Housekeeper",
+        "Sig." = "Sig."
+      )
+    )
+)
+
+print(
+  make_spaghetti_plot_null(
+    fc_mat = log2fc_mat,
+    bg_mat = null_mat,
+    bg_genes = c("GUSB", "PPIA", "UBC", "SDHA", "ACTB", "TBP", "B2M", "GAPDH"),
+    genes = "ALDH1A1",
+    .center = FALSE
+  ) +
+    scale_color_manual(
+      values = c(
+        "Synthesized" = "gray",
+        "Sig." = "red"
+      ),
+      labels = c(
+        "Synthesized" = "Housekeeper",
+        "Sig." = "Sig."
+      )
+    )
+)
+
+print(
+  make_spaghetti_plot_null(
+    fc_mat = log2fc_mat,
+    bg_mat = null_mat,
+    bg_genes = c("GUSB", "PPIA", "UBC", "SDHA", "ACTB", "TBP", "B2M", "GAPDH"),
+    genes = "SOD2",
+    .center = FALSE
+  ) +
+    scale_color_manual(
+      values = c(
+        "Synthesized" = "gray",
+        "Sig." = "red"
+      ),
+      labels = c(
+        "Synthesized" = "Housekeeper",
+        "Sig." = "Sig."
+      )
+    )
+)
+
+print(make_spaghetti_plot_null(
+  fc_mat = log2fc_mat,
+  bg_mat = null_mat,
+  bg_genes = c("GUSB", "PPIA", "UBC", "SDHA", "ACTB", "TBP", "B2M", "GAPDH"),
+  genes = "CCNI",
+  .center = FALSE
+) +
+  scale_color_manual(
+    values = c(
+      "Synthesized" = "gray",
+      "Sig." = "red"
+    ),
+    labels = c(
+      "Synthesized" = "Housekeeper",
+      "Sig." = "Sig."
+    )
+  ))
+dev.off()
 
 
 # Session Info ----
