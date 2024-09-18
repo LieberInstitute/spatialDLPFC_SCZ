c58c2f07b570f0da50798da8221acc45f2ae77c0
Boyi Guo <boyi.guo.work@gmail.com>
2024-09-18 14:38:19 -0400

    format

--- code/analysis/01_build_spe/fun_import_dx.R
+++ code/analysis/01_build_spe/fun_import_dx.R
@@ -15,25 +15,26 @@ raw_df <- readxl::read_excel(
   range = cellranger::cell_rows(
     c(2, NA)
   ) # Start from the second row
-) |> 
+) |>
   # Only include relevent columns
   select(
     brain_num = `Brain #`,
     SCZ_study = `Study 1`,
-    dx_raw = "Diagnosis", 
+    dx_raw = "Diagnosis",
     age = "AGE",
     sex = "SEX",
     "PMI", "RIN",
     "Dissection Date"
     #  Seems to be notes
     # "opiate dx"
-  ) 
+  )
 # Remove example row based on "Ex" --------------------------------------
 ex_row_id <- grep(
   pattern = "^Ex:.*",
-  x = raw_df$brain_num)
+  x = raw_df$brain_num
+)
 
-clean_df <- raw_df[-ex_row_id,] |> 
+clean_df <- raw_df[-ex_row_id, ] |>
   filter_all(any_vars(!is.na(.))) # Remove empty rows.
 
 # Validation
@@ -41,13 +42,14 @@ clean_df <- raw_df[-ex_row_id,] |>
 stopifnot(
   grep(
     pattern = "^Ex:.*",
-    x = clean_df$dx_raw) |> 
+    x = clean_df$dx_raw
+  ) |>
     length() == 0
 )
 
 # Clean up brain number column--------------------------------------------------
 # Remove all " - xxNotesxx"
-clean_df$brain_num  <- str_sub(
+clean_df$brain_num <- str_sub(
   clean_df$brain_num,
   1, 6
 )
@@ -57,7 +59,7 @@ clean_df$brain_num  <- str_sub(
 # If all brain numbers are 6 characters
 # This validation is not robust to longer Brain nums
 stopifnot(
-  all(str_length(clean_df$brain_num)==6)
+  all(str_length(clean_df$brain_num) == 6)
 )
 
 # Validation
@@ -71,30 +73,30 @@ stopifnot(all(!duplicated(clean_df$brain_num)))
 
 # Helper functions
 # Change all elements starting with "sc" to scz (lower case)
-clean_scz <- function(vec){
+clean_scz <- function(vec) {
   scz_ind <- grep(
     pattern = "^sc.+",
     x = vec
   )
-  
+
   vec[scz_ind] <- "scz"
-  
+
   return(vec)
 }
 
-clean_ntc <- function(vec){
+clean_ntc <- function(vec) {
   browser()
   ntc_ind <- str_detect(
-    string =  vec,
+    string = vec,
     pattern = "control|neurotypical|ntc"
   )
-  
+
   vec[ntc_ind] <- "ntc"
-  
+
   return(vec)
 }
 
-clean_df <- clean_df |> 
+clean_df <- clean_df |>
   mutate(
     dx = case_when(
       str_detect(tolower(dx_raw), "^sc.+") ~ "scz",
@@ -102,10 +104,7 @@ clean_df <- clean_df |>
       # Set other dx to NA
       TRUE ~ NA_character_
     ),
-    race = "CAUC"  # NOTE: "all donors in this study are European ancestry"
-  ) |> 
-  filter(!is.na(dx)) |> 
+    race = "CAUC" # NOTE: "all donors in this study are European ancestry"
+  ) |>
+  filter(!is.na(dx)) |>
   select(-c(SCZ_study, dx_raw))
-
-
-
