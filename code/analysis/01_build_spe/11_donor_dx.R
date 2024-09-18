1a3f40ff09f1eadae01f6d9d1528aba3e8d9b251
Boyi Guo <boyi.guo.work@gmail.com>
2024-09-18 14:45:49 -0400

    format

--- code/analysis/01_build_spe/11_donor_dx.R
+++ code/analysis/01_build_spe/11_donor_dx.R
@@ -8,9 +8,11 @@ library(sessioninfo)
 
 # Load SPE Object ---------------------------------------------------------
 spe <- readRDS(
-  here::here("processed-data",
-             "rds", "01_build_spe", 
-             "raw_spe_wo_SPG_N63.rds")
+  here::here(
+    "processed-data",
+    "rds", "01_build_spe",
+    "raw_spe_wo_SPG_N63.rds"
+  )
 )
 
 
@@ -18,13 +20,13 @@ spe <- readRDS(
 demo_df <- metadata(spe)$dx_df
 
 # Error Prevention
-if("Br3942" %in% demo_df$subject){
+if ("Br3942" %in% demo_df$subject) {
   warning("Non-DLPFC Sample *Br3942* is still in the dataset")
   demo_df <- demo_df[which(demo_df$subject != "Br3942"), ]
 }
 stopifnot(nrow(demo_df) == 63) # Should remove Br3942
 
-demo_df_formated <- demo_df |> 
+demo_df_formated <- demo_df |>
   transmute(
     `Brain Number (ID)` = subject,
     `Date Dxt\'d` = `Dissection Date`,
@@ -42,51 +44,57 @@ demo_df_formated <- demo_df |>
 
 
 #  Save Raw Demo Data as CSV ----------------------------------------------
-demo_df_formated |> 
+demo_df_formated |>
   write_csv(
     file = here::here(
       "manuscript", "tables",
-      "stabl_demo_raw.csv")
+      "stabl_demo_raw.csv"
+    )
   )
 
 
 
 # Create Demo Compare Table
-demo_df_formated |> 
-  select(-c(`Brain Number (ID)`,
-            `Date Dxt\'d`)) |> 
-  tbl_summary(by = `PrimaryDx`,
-              type = list(
-                Age ~ "continuous",
-                RIN ~ "continuous",
-                PMI ~ "continuous"),
-              statistic = list(
-                Age ~ "{mean} ({sd})",
-                RIN ~ "{mean} ({sd})",
-                PMI ~ "{mean} ({sd})"
-              )
-  ) |> 
+demo_df_formated |>
+  select(-c(
+    `Brain Number (ID)`,
+    `Date Dxt\'d`
+  )) |>
+  tbl_summary(
+    by = `PrimaryDx`,
+    type = list(
+      Age ~ "continuous",
+      RIN ~ "continuous",
+      PMI ~ "continuous"
+    ),
+    statistic = list(
+      Age ~ "{mean} ({sd})",
+      RIN ~ "{mean} ({sd})",
+      PMI ~ "{mean} ({sd})"
+    )
+  ) |>
   # Note: View output on JHPCE
   # as_tibble()
   # Note: save as an xlsx file
   as_hux_xlsx(
-    file = here("manuscript", "tables",
-                "stabl_demo_compare.xlsx")
+    file = here(
+      "manuscript", "tables",
+      "stabl_demo_compare.xlsx"
     )
+  )
 
 # A tibble: 8 × 3
 # `**Characteristic**` `**Control**, N = 31` `**Schizophrenia**, N = 32`
-# <chr>                <chr>                 <chr>                      
-#   1 Race                 NA                    NA                         
-# 2 CAUC                 31 (100%)             32 (100%)                  
-# 3 Sex                  NA                    NA                         
-# 4 F                    15 (48%)              14 (44%)                   
-# 5 M                    16 (52%)              18 (56%)                   
-# 6 Age                  47 (10)               47 (7)                     
-# 7 RIN                  7.80 (7.35, 8.55)     7.85 (7.18, 8.50)          
-# 8 PMI                  26 (22, 31)           26 (19, 33)          
+# <chr>                <chr>                 <chr>
+#   1 Race                 NA                    NA
+# 2 CAUC                 31 (100%)             32 (100%)
+# 3 Sex                  NA                    NA
+# 4 F                    15 (48%)              14 (44%)
+# 5 M                    16 (52%)              18 (56%)
+# 6 Age                  47 (10)               47 (7)
+# 7 RIN                  7.80 (7.35, 8.55)     7.85 (7.18, 8.50)
+# 8 PMI                  26 (22, 31)           26 (19, 33)
 
 
 # Session Info ------------------------------------------------------------
 session_info()
-
