d084c6a6c8f23f91d45022b2b796e89d4f43bdd3
Boyi Guo <boyi.guo.work@gmail.com>
2024-10-09 16:21:48 -0400

    Create pipeline to PB DEGs in SCZ of SPG+ at both donor level as well as SpD level

---
+++ code/analysis/SPG_analysis/SPG_DE_analysis.R
@@ -0,0 +1,96 @@
+# Load Packages ----
+suppressPackageStartupMessages({
+  library(spatialLIBD)
+  library(limma)
+  library(sessioninfo)
+  library(here)
+  library(glue)
+})
+
+# Loop over all SPG + analysis level ----
+analysis_type <- c("SPD", "donor")
+spg_names <- c("pnn_pos", "neuropil_pos", "neun_pos", "vasc_pos")
+
+
+analysis_combo <- expand.grid(
+  type = analysis_type,
+  spg = spg_names
+)
+
+pb_files <- analysis_combo |> glue_data("test_{type}_pseudo_{spg}.rds")
+
+pb_files |>
+  walk(.f = function(.pb_file) {
+    # Error prevention
+    # Check if pseudobulk rds file exists
+    stopifnot(
+      file.exists(
+        here(
+          "processed-data", "rds", "PB_dx_spg",
+          .pb_file
+        )
+      )
+    )
+
+    ## Load Data ----
+    sce_pseudo <- readRDS(
+      here(
+        "processed-data", "rds", "PB_dx_spg",
+        .pb_file
+      )
+    )
+    # colData(sce_pseudo) |> str()
+    ## Run pipeline ----
+
+    if (grepl("donor", .pb_file)) {
+      ## At the donor level
+      cat("Donor level DE analysis\n")
+      dx_res <- registration_stats_enrichment(
+        sce_pseudo,
+        block_cor = NaN,
+        covars = c("age", "sex"),
+        var_registration = "dx",
+        gene_ensembl = "gene_id",
+        gene_name = "gene_name"
+      )
+    } else if (grepl("SPD", .pb_file)) {
+      cat("SPD level DE analysis\n")
+      ## Adjust for SPD
+      dx_mod <-
+        registration_model(
+          sce_pseudo,
+          covars = c("age", "sex"),
+          var_registration = "dx"
+        )
+
+      dx_block_cor <- registration_block_cor(
+        sce_pseudo,
+        registration_model = dx_mod,
+        var_sample_id = "sample_id"
+      )
+
+      dx_res <- registration_stats_enrichment(
+        sce_pseudo,
+        block_cor = dx_block_cor,
+        covars = c("age", "sex", "PRECAST_07"),
+        var_registration = "dx",
+        gene_ensembl = "gene_id",
+        gene_name = "gene_name"
+      )
+    } else {
+      stop("No analysis for this type of PB data.")
+    }
+
+
+    ### Save the DE result ----
+    write_csv(
+      dx_res,
+      here(
+        "processed-data/spg_pb_de",
+        .pb_file |> str_replace(".rds", ".csv")
+      )
+    )
+  })
+
+# Session Info ----
+session_info()
