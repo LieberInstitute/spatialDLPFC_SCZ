75dbcefefa8e38cd164c7b57fea1fa007de7be13
Boyi Guo <boyi.guo.work@gmail.com>
2024-12-27 20:03:57 -0500

    refactor code to show diff circula plot and diff heatmap

--- code/analysis/Layer_layer_communication/DEG_only/understanding_pathways.R
+++ code/analysis/Layer_layer_communication/DEG_only/understanding_pathways.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(here)
 library(CellChat)
+library(circlize)
+library(ComplexHeatmap)
 
 # Load data ----
 ## LR pair enriched in CellChatDB ----
@@ -26,7 +28,7 @@ unique(LR_pair_single$pathway_name) |> length()
 cellchat <- readRDS(
   here(
     "processed-data/layer_layer_comm",
-    "merged_cellchat.rds"
+    "merged_cellchat_annotated.rds"
   )
 )
 
@@ -57,60 +59,142 @@ overlap_pathways <- intersect(cellchat_diff_pathways, LR_pair_single$pathway_nam
 
 ntc_cellchat <- readRDS(here(
   "processed-data/layer_layer_comm",
-  "ntc_cellchat.rds"
+  "ntc_cellchat_annotated.rds"
 ))
 scz_cellchat <- readRDS(
   here(
     "processed-data/layer_layer_comm",
-    "scz_cellchat.rds"
+    "scz_cellchat_annotated.rds"
   )
 )
 
 object.list <- list(ntc = ntc_cellchat, scz = scz_cellchat)
 
-# TODO: create pdf file to save
-pdf(
+# Source code to create diff circular plot
+source(here(
+  "code/analysis/SPG_analysis/ccc_neun_claudin",
+  "3_A-Sender_Receiver_Chord.r"
+))
+## Heatmap show diff
+source(
   here(
-    "plots/layer_layer_comm/",
-    "DEG_overlap_pathways_annotated.pdf"
-  ),
-  height = 7, width = 9
+    "code/analysis/Layer_layer_communication/vulnerablility_charact",
+    "fun_extract_pathway_diff_mat.r"
+  )
 )
-par(mfrow = c(1, 2), xpd = TRUE)
-for (.path in overlap_pathways) {
-  pathways.show <- .path
-  weight.max <- getMaxWeight(object.list,
-    slot.name = c("netP"),
-    attribute = pathways.show
-  ) # control the edge weig
-  for (i in 1:length(object.list)) {
-    # browser()
-    netVisual_aggregate(object.list[[i]],
-      signaling = pathways.show, layout = "chord",
-      edge.weight.max = weight.max[1], edge.width.max = 10,
-      signaling.name = paste(pathways.show, names(object.list)[i])
+
+plot_pathway_panel <- function(vec_pathway, plot_path) {
+  # TODO: create pdf file to save
+  pdf(
+    plot_path,
+    height = 7, width = 9
+  )
+  for (.path in vec_pathway) {
+    ## 2 circular plots for NTC and SCZ respectively
+
+    par(mfrow = c(1, 2), xpd = TRUE)
+    pathways.show <- .path
+    weight.max <- getMaxWeight(object.list,
+      slot.name = c("netP"),
+      attribute = pathways.show
+    ) # control the edge weig
+    for (i in 1:length(object.list)) {
+      netVisual_aggregate(object.list[[i]],
+        signaling = pathways.show, layout = "chord",
+        edge.weight.max = weight.max[1], edge.width.max = 10,
+        signaling.name = paste(pathways.show, names(object.list)[i])
+      )
+    }
+    par(mfrow = c(1, 1), xpd = TRUE)
+    ## Circular plots for diff
+    my_netVisual_aggregate(ntc_cellchat, scz_cellchat,
+      signaling = .path,
+      measure = "weight", layout = "chord"
     )
+
+    # Extract the pathway matrix
+    pathway_matrix <- extract_diff_pathway(
+      ntc_cellchat, scz_cellchat,
+      signaling = .path, measure = "weight"
+    ) |>
+      as.matrix()
+
+    # Create row and column annotations
+    row_annotation <- rowAnnotation(
+      hist = anno_barplot(pathway_matrix |> rowSums(), which = "row")
+    )
+    col_annotation <- HeatmapAnnotation(
+      hist = anno_barplot(pathway_matrix |> colSums(), which = "column")
+    )
+
+
+    # Visualize the pathway matrix using a heatmap with annotations
+    Heatmap(
+      pathway_matrix,
+      name = .path,
+      cluster_rows = FALSE,
+      cluster_columns = FALSE,
+      row_title = "Sender",
+      column_title = "Receiver",
+      col = colorRamp2(c(min(pathway_matrix), 0, max(pathway_matrix)), c("blue", "white", "red")),
+      top_annotation = col_annotation,
+      left_annotation = row_annotation
+    ) |> print()
   }
+
+  dev.off()
 }
-par(mfrow = c(1, 1), xpd = TRUE)
-dev.off()
 
+# DEG overlap pathways ----
+plot_pathway_panel(
+  vec_pathway = overlap_pathways,
+  plot_path = here(
+    "plots/layer_layer_comm/",
+    "DEG_overlap_pathways_annotated_w_diff.pdf"
+  )
+)
+# DEG overlap pathways ----
 
-# TODO: create pdf file to save
+# Likeli_pathway Curated by Sang Ho ----
 likeli_pathway <- c(
   "NRXN", "L1CAM", "CNTN", "PTN", "GABA-A", "GABA-B",
   "Glutamate"
 )
 
-pdf(
-  here(
+plot_pathway_panel(
+  vec_pathway = likeli_pathway,
+  plot_path = here(
     "plots/layer_layer_comm/",
-    "SCZ_likely_pathways_annotated.pdf"
-  ),
-  height = 7, width = 9
+    "SCZ_likely_pathways_annotated_w_diff.pdf"
+  )
+# )
+
+# Potential_pathway Curated by Sang Ho ----
+novel_pathway <- c(
+  "APP", "CypA", "SPP1")
+  # , "NT", "SOMATOSTATIN")
+plot_pathway_panel(
+  vec_pathway = novel_pathway,
+  plot_path =
+here(
+    "plots/layer_layer_comm/",
+    "SCZ_pop_novel_pathways_annotated_w_diff.pdf"
+  )
 )
+
+# pdf(
+#   here(
+#     "plots/layer_layer_comm/",
+#     "SCZ_likely_pathways_annotated.pdf"
+#   ),
+#   height = 7, width = 9
+# )
+
+# NT and SOMATOSTATIN Pathway
+# No NT pathway detected
 par(mfrow = c(1, 2), xpd = TRUE)
-for (.path in likeli_pathway) {
+for (.path in c("NT", "SOMATOSTATIN")) {
+  browser()
   pathways.show <- .path
   weight.max <- getMaxWeight(object.list,
     slot.name = c("netP"),
@@ -126,57 +210,51 @@ for (.path in likeli_pathway) {
   }
 }
 par(mfrow = c(1, 1), xpd = TRUE)
-dev.off()
+# dev.off()
 
 # TODO: create pdf file to save
-pdf(
-  here(
-    "plots/layer_layer_comm/",
-    "SCZ_pop_novel_pathways_annotated.pdf"
-  ),
-  height = 7, width = 9
-)
-novel_pathway <- c(
-  "CypA", "SPP1", "APP"
-)
-par(mfrow = c(1, 2), xpd = TRUE)
-for (.path in novel_pathway) {
-  pathways.show <- .path
-  weight.max <- getMaxWeight(object.list,
-    slot.name = c("netP"),
-    attribute = pathways.show
-  ) # control the edge weig
-  for (i in 1:length(object.list)) {
-    # browser()
-    netVisual_aggregate(object.list[[i]],
-      signaling = pathways.show, layout = "chord",
-      edge.weight.max = weight.max[1], edge.width.max = 10,
-      signaling.name = paste(pathways.show, names(object.list)[i])
-    )
-  }
-}
-par(mfrow = c(1, 1), xpd = TRUE)
-dev.off()
+# pdf(
+#   here(
+#     "plots/layer_layer_comm/",
+#     "SCZ_pop_novel_pathways_annotated.pdf"
+#   ),
+#   height = 7, width = 9
+# )
+
+# par(mfrow = c(1, 2), xpd = TRUE)
+# for (.path in novel_pathway) {
+#   pathways.show <- .path
+#   weight.max <- getMaxWeight(object.list,
+#     slot.name = c("netP"),
+#     attribute = pathways.show
+#   ) # control the edge weig
+#   for (i in 1:length(object.list)) {
+#     # browser()
+#     netVisual_aggregate(object.list[[i]],
+#       signaling = pathways.show, layout = "chord",
+#       edge.weight.max = weight.max[1], edge.width.max = 10,
+#       signaling.name = paste(pathways.show, names(object.list)[i])
+#     )
+#   }
+# }
+# par(mfrow = c(1, 1), xpd = TRUE)
+# dev.off()
 
 # TODO: possibly plotting the difference plot
 
 
 # netVisual_diffInteraction
-source(here(
-  "code/analysis/SPG_analysis/ccc_neun_claudin",
-  "3_A-Sender_Receiver_Chord.r"
-))
 
-pdf(
-  here(
-    "plots/layer_layer_comm/",
-    "SCZ_likely_pathways_diff.pdf"
-  )
-)
-for (.path in likeli_pathway) {
-  print(my_netVisual_aggregate(ntc_cellchat, scz_cellchat, signaling = .path, measure = "weight", layout = "chord"))
-}
-dev.off()
+# pdf(
+#   here(
+#     "plots/layer_layer_comm/",
+#     "SCZ_likely_pathways_diff.pdf"
+#   )
+# )
+# for (.path in likeli_pathway) {
+#   print(my_netVisual_aggregate(ntc_cellchat, scz_cellchat, signaling = .path, measure = "weight", layout = "chord"))
+# }
+# dev.off()
 
 
 
