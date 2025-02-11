efe6d99de3654159c4c884a57cbf72f9c4d55eff
Boyi Guo <boyi.guo.work@gmail.com>
2025-02-11 17:21:19 -0500

    Code to explore var in gene expression explained by spatial domains.

--- code/analysis/03_visium_spatial_clustering/plot_var_explained_spd_pb.R
+++ code/analysis/03_visium_spatial_clustering/plot_var_explained_spd_pb.R
@@ -9,12 +9,12 @@ suppressPackageStartupMessages({
   library(sessioninfo)
 })
 
-spd_rds <- list.files(
-  here(
-    "processed-data", "rds", "layer_spd"
-  ),
-  pattern = ".rds"
-)
+# spd_rds <- list.files(
+#   here(
+#     "processed-data", "rds", "layer_spd"
+#   ),
+#   pattern = ".rds"
+# )
 
 # PCA analysis ----
 
@@ -26,43 +26,58 @@ spd_rds <- list.files(
 # )
 
 # for (.file in spd_rds) {
-  # .file <- spd_rds[1]
+# .file <- spd_rds[1]
 
-  .spd <- str_remove(.file, "test_spe_pseudo_") |>
-    str_remove(".rds")
+# .spd <- str_remove(.file, "test_spe_pseudo_") |>
+#   str_remove(".rds")
+.spd <- "PRECAST_07"
 
-  sce_pseudo <- readRDS(
-    here(
-      "processed-data", "rds", "layer_spd",
-      .file
-    )
+sce_pseudo <- readRDS(
+  here(
+    "processed-data", "rds", "layer_spd",
+    paste0("test_spe_pseudo_", .spd, ".rds")
   )
+)
 
 
-  set.seed(20240411)
-  sce_pseudo <- runPCA(sce_pseudo)
+set.seed(20240411)
+sce_pseudo <- runPCA(sce_pseudo)
 
-  pdf(
-    here(
-      "plots/PB_DE", "SpD_PB_PCA",
-      paste0("test_", .spd, ".pdf")
-    )
+pdf(
+  here(
+    "plots/PB_DE", "SpD_PB_PCA",
+    paste0("test_", .spd, ".pdf")
   )
-  sce_pseudo <- runPCA(sce_pseudo)
-  # for (.var in c("age", "sex", "spd", "dx", "slide_id", "lot_num")) {
-  #   plotPCA(
-  #     sce_pseudo,
-  #     colour_by = .var,
-  #     ncomponents = 6,
-  #     point_size = 0.3,
-  #     label_format = c("%s %02i", " (%i%%)")
-  #   ) |> print()
-  # }
+)
+# sce_pseudo <- runPCA(sce_pseudo)
+# for (.var in c("age", "sex", "spd", "dx", "slide_id", "lot_num")) {
+#   plotPCA(
+#     sce_pseudo,
+#     colour_by = .var,
+#     ncomponents = 6,
+#     point_size = 0.3,
+#     label_format = c("%s %02i", " (%i%%)")
+#   ) |> print()
+# }
 
-  plotExplanatoryVariables(sce_pseudo,
-    variables = c("dx", "age", "sex", "slide_id", "spd", "sample_id")
+
+dat_mat <- getVarianceExplained(sce_pseudo,
+  variables = c(
+    "dx", "age", "sex", "slide_id", "PRECAST_07" # , "sample_id")
   )
-  dev.off()
+)
+
+pdf(
+  here("plots/03_visium_spatial_clustering",
+  "den_plot_var_explained_by_spd.pdf"),
+  height = 5
+)
+plotExplanatoryVariables(sce_pseudo,
+  variables = c(
+    "dx", "age", "sex", "slide_id", "PRECAST_07" # , "sample_id")
+  )
+)
+dev.off()
 # }
 
 
