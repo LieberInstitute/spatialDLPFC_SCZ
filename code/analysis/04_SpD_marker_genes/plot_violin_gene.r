756e5605d4e6951e59bdcdf5ee7ed684712dd1cf
Boyi Guo <boyi.guo.work@gmail.com>
2025-02-25 15:52:46 -0500

    Violin plot for gene expression across spd

--- code/analysis/04_SpD_marker_genes/plot_violin_gene.r
+++ code/analysis/04_SpD_marker_genes/plot_violin_gene.r
@@ -19,49 +19,39 @@ sce_pseudo <- readRDS(
 )
 
 # Select gene ----
-gene_name <- "RELN"
-gene_ensembl <- rowData(sce_pseudo)$gene_id[rowData(sce_pseudo)$gene_name == gene_name]
-
-# error prevention
-stopifnot(legnth(gene_ensembl) == 1)
-
-# Plot ----
-## Across all spatial domains -----
-
-plotExpression(
-  sce_pseudo,
-  features = gene_ensembl,
-  x = .spd,
-  color = "dx",
-  show_median = TRUE
-) + scale_x_discrete(
-  limits = sprintf("spd%02d", c(7, 6, 2, 5, 3, 1, 4))
-  # TODO: add the layer annotation here
-) +
-  scale_color_manual(
-    name = "Diagnosis",
-    values = c(
-      "ntc" = "blue", "scz" = "red"
-    ),
-    labels = c("NTC", "SCZ")
-  )
-
-
-
-## Between SpD01 (L6/WM) and SpD04 (WM)
-# TODO: subset to SpD01 and SPD04 to run the lines above
-
-
 ## Oligodentrocytes ----
 # from PEC data
-Oligo_genes <- c("FOLH1", "CD22", "SLC5A11", "MAG", "MYRF", "ANLN", "MOG", "LDB3")
-Oligo_ensembl <- rowData(sce_pseudo)$gene_id[rowData(sce_pseudo)$gene_name %in% Oligo_genes]
+oligo_genes <- c("PLP1", "MBP", "MOBP")
+neuronal_genes <- c("SNAP25", "SLC17A7")
 
-stopifnot(length(Oligo_genes) == length(Oligo_ensembl))
+# stopifnot(length(oligo_genes) == length(oligo_ensembl))
 
-plotExpression(
+# oligo_p <- plotExpression(
+#   sce_pseudo,
+#   features = Oligo_genes,
+#   x = .spd,
+#   color = "dx",
+#   show_median = TRUE,
+#   scales = "free_y",
+#   swap_rownames = "gene_name"
+# ) + scale_x_discrete(
+#   limits = sprintf("spd%02d", c(7, 6, 2, 5, 3, 1, 4))
+# ) +
+#   scale_color_manual(
+#     name = "Diagnosis",
+#     values = c(
+#       "ntc" = "blue", "scz" = "red"
+#     ),
+#     labels = c("NTC", "SCZ")
+#   ) +
+#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+## Neuronal genes ----
+
+
+paneled_p <- plotExpression(
   sce_pseudo,
-  features = Oligo_genes,
+  features = c(neuronal_genes, Oligo_genes),
   x = .spd,
   color = "dx",
   show_median = TRUE,
@@ -77,48 +67,20 @@ plotExpression(
       "ntc" = "blue", "scz" = "red"
     ),
     labels = c("NTC", "SCZ")
-  )
-
-
-## Excit L6 ----
-ex_l6_genes <- c(
-  # "LINC02718",
-  "MCTP2",
-  # "AC006299.1",
-  # "DPP4",
-  # "ADAMTSL1",
-   "FILIP1",
-    "OLFML2B", "PTPRU",
-  # "AC011246.1",
-  "AC024610.2"
-)
-# ex_l6_ensembl <- rowData(sce_pseudo)$gene_id[rowData(sce_pseudo)$gene_name %in% ex_l6_genes]
-
-# stopifnot(length(ex_l6_genes) == length(ex_l6_ensembl))
-
-plotExpression(
-  sce_pseudo,
-  features = ex_l6_genes,
-  x = .spd,
-  color = "dx",
-  show_median = TRUE,
-  scales = "free_y",
-  swap_rownames = "gene_name"
-) + scale_x_discrete(
-  limits = sprintf("spd%02d", c(7, 6, 2, 5, 3, 1, 4))
-  # TODO: add the layer annotation here
-) +
-  scale_color_manual(
-    name = "Diagnosis",
-    values = c(
-      "ntc" = "blue", "scz" = "red"
-    ),
-    labels = c("NTC", "SCZ")
-  )
-
+  ) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), axis.title.x.bottom = element_blank())
 
 
 # Save plot -----
+
+
+ggsave(
+  filename = here("plots/04_SpD_marker_genes", "violin_plot_spd01-04.pdf"),
+  plot = paneled_p,
+  width = 12, height = 6
+)
+
+
 
 
 # Session Info ----
