3c39c4795b4843210611b57bff01463f6924dd60
Boyi Guo <boyi.guo.work@gmail.com>
2025-03-21 09:02:45 -0400

    comparing w. and w/o. adjusting RIN in the model

--- code/analysis/10_dx_deg_adjust_spd/sensitivity_analysis.R
+++ code/analysis/10_dx_deg_adjust_spd/sensitivity_analysis.R
@@ -21,10 +21,10 @@ RIN_adj_res <- read_csv(
     "tmp_rin_adj_PRECAST07.csv"
   )
 )
+# Error prevention
+stopifnot(nrow(prelim_res) == nrow(RIN_adj_res))
 
 ## Merged data sets ----
-nrow(prelim_res) == nrow(RIN_adj_res)
-
 merged_res <- inner_join(
   prelim_res,
   RIN_adj_res,
@@ -32,10 +32,7 @@ merged_res <- inner_join(
   suffix = c("_prelim", "_rin_adj")
 )
 
-
-prelim_res |> filter(gene == "GBP2")
-
-# histogram of p-value
+# histogram of p-value ----
 ## prelim_res ----
 hist(prelim_res$p_value_scz, breaks = 100)
 qqplot(prelim_res$p_value_scz, runif(n = nrow(prelim_res)))
@@ -61,7 +58,7 @@ cor(
   merged_res$logFC_scz_prelim,
   merged_res$logFC_scz_rin_adj
 )
-# [1] 0.6997641
+# [1] 0.9578429
 
 ## Scatter plot of t-statistics ----
 plot(
@@ -77,7 +74,46 @@ cor(
   merged_res$t_stat_scz_prelim,
   merged_res$t_stat_scz_rin_adj
 )
-# [1] 0.7198662
+# [1] 0.9563269
+
+
+sum(merged_res$fdr_scz_rin_adj < 0.10)
+
+
+## What are overlapping sig genes
+merged_res |>
+  mutate(
+    prelim_sig = fdr_scz_prelim < 0.10,
+    rin_sig = fdr_scz_rin_adj < 0.10) |>
+  with(
+    table(prelim_sig, rin_sig)
+  ) # 81 genes
+
+#             rin_sig
+# prelim_sig FALSE  TRUE
+#      FALSE 13763    42
+#      TRUE     69   103
+
+## RIN specific genes
+merged_res |>
+  filter(
+    fdr_scz_prelim > 0.10,
+    fdr_scz_rin_adj < 0.10
+  ) |>
+  pull(
+    gene_rin_adj
+  )
+
+## Preliminary specific genes
+merged_res |>
+  filter(
+    fdr_scz_prelim < 0.10,
+    fdr_scz_rin_adj > 0.10
+  ) |>
+  pull(
+    gene_prelim
+  )
+
 
 # Session Info ----
 sessioninfo::session_info()
