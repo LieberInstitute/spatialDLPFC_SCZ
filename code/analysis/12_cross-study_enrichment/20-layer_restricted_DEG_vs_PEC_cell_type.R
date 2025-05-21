9b8fec0cd6a7c372ceda89f26c048776d0e7930a
Boyi Guo <boyi.guo.work@gmail.com>
2025-05-21 17:26:13 -0400

    Enrich layer-restricted DEGs to PEC cell type_amrkers

--- code/analysis/12_cross-study_enrichment/20-layer_restricted_DEG_vs_PEC_cell_type.R
+++ code/analysis/12_cross-study_enrichment/20-layer_restricted_DEG_vs_PEC_cell_type.R
@@ -3,52 +3,186 @@ suppressPackageStartupMessages({
   library(here)
   library(tidyverse)
   library(spatialLIBD)
+  library(ComplexHeatmap)
+  library(circlize)
   library(sessioninfo)
 })
 
 # Load Data ----
 ## load Layer_specific DEGs ----
-spd_files <- list.files(
-  "processed-data/rds/11_dx_deg_interaction", ,
-  pattern = "layer_specific_logFC_.*\\.csv",
-  full.names = TRUE
-)
-
-names(spd_files) <- str_extract(
-  spd_files,
-  "(?<=layer_specific_logFC_).*?(?=\\.csv)"
-)
-
-spd_deg_list <- map(
-  spd_files,
-  ~ read_csv(.x, col_types = cols()) |>
-    mutate(gene = AnnotationDbi::mapIds(
-      org.Hs.eg.db::org.Hs.eg.db,
-      keys = gene_id,
-      column = "SYMBOL",
-      keytype = "ENSEMBL",
-      multiVals = "first"
-    )) |>
-    filter(
-      P.Value < 0.05
-    )
-)
-
-spd_deg_list |> map(~ .x |> nrow())
-
-## Load Huuki-Myers cell type -markers ----
-load(
+layer_specifc_DEG_df <- read_csv(
   here(
-    "code/analysis/12_deg_integration",
-    "huuki_myers_cell_type_enrichment_cellType_hc.Rdata"
+    "processed-data/rds/11_dx_deg_interaction",
+    "layer_restricted_degs_all_spds.csv"
+  )
+) |>
+  # keep the nominal p-value < 0.05 DEGs
+  filter(P.Value < 0.05)
+
+overall_deg_list <- layer_specifc_DEG_df |>
+  group_split(PRECAST_spd) |>
+  set_names(
+    nm = layer_specifc_DEG_df |>
+      distinct(PRECAST_spd) |>
+      pull(PRECAST_spd)
+  )
+
+up_reg_deg_list <- overall_deg_list |>
+  map(~ .x |> filter(logFC > 0))
+
+down_reg_deg_list <- overall_deg_list |>
+  map(~ .x |> filter(logFC < 0))
+
+
+# spd_deg_list |> map(~ .x |> nrow())
+
+## Load Huuki-Myer et al. (2024) cell-type markers ----
+### (Deprecated) Cell type not annocated ----
+# load(
+#   here(
+#     "code/analysis/12_deg_integration",
+#     "huuki_myers_cell_type_enrichment_cellType_hc.Rdata"
+#   )
+# )
+# cell_type_enrich_df <- results_specificity
+
+### Load Enrichment results with annotated cell types ----
+cell_type_enrich_df <- readRDS(
+  # NOTE: if run on JHPCE
+  # file.path(
+  #   # PEC Study folder
+  #   "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/",
+  #   "processed-data/rdata/spe/14_spatial_registration_PEC",
+  #   "registration_stats_LIBD.rds"
+  # )
+  # NOTE: if run on Boyi's local machine
+  here(
+    "code/analysis/12_cross-study_enrichment",
+    "registration_stats_LIBD.rds"
   )
 )
 
-cell_type_enrich_df <- results_specificity
-
 # Enrichment Test ----
-res <- spatialLIBD::gene_set_enrichment(
-  gene_list = spd_deg_list |> map(~ .x |> pull(gene_id)),
+## Wrapper function for the dot plot ---
+enrichment_dot_plot_heatmap <- function(
+    res,
+    # PThresh = 12, ORcut = 3, enrichOnly = FALSE, cex = 0.5
+    title) {
+  # Prepare data for ComplexHeatmap
+  # browser()
+  mat <- res |>
+    mutate(
+      OR = ifelse(OR < 1, 1, OR),
+    ) |>
+    select(
+      ID, test, OR
+    ) |>
+    pivot_wider(names_from = test, values_from = OR, values_fill = 0) |>
+    column_to_rownames("ID") |>
+    as.matrix()
+
+  # Scale the size based on -log10(Pval)
+  size_mat <- res |>
+    mutate(
+      fdr_p = Pval |> p.adjust(method = "fdr"),
+      sig_cat = case_when(
+        Pval >= 0.05 ~ "nonsig",
+        Pval < 0.05 & fdr_p >= 0.05 ~ "Nominal p < 0.05",
+        fdr_p < 0.05 ~ "FDR < 0.05"
+      ),
+      size = case_when(
+        sig_cat == "nonsig" ~ 0.5,
+        sig_cat == "Nominal p < 0.05" ~ 1,
+        sig_cat == "FDR < 0.05" ~ 1.5
+      )
+    ) |>
+    select(
+      ID, test, size
+    ) |>
+    pivot_wider(names_from = test, values_from = size, values_fill = 0) |>
+    column_to_rownames("ID") |>
+    as.matrix()
+
+  # Reorder the matrix based on the order of spd_anno_df
+  spd_order <- c(
+    "SpD07-L1",
+    "SpD06-L2/3",
+    "SpD02-L3/4",
+    "SpD05-L5",
+    "SpD03-L6",
+    "SpD01-WMtz",
+    "SpD04-WM"
+  )
+
+  # cell_type_order <- res$ID |> unique()
+
+  mat <- mat[spd_order, ]
+  size_mat <- size_mat[spd_order, ]
+
+  # Change matrix orientation
+  # mat <- t(mat)
+  # size_mat <- t(size_mat)
+
+browser()
+
+  # Define color function for Odds Ratio
+  col_fun <- colorRamp2(
+    # TODO: change the color scale
+    c(min(mat), median(mat), max(mat)),
+    c("grey", "yellow", "blue")
+  )
+
+  # browser()
+
+  # Create the dot plot using ComplexHeatmap
+  ht_list <- Heatmap(
+    mat,
+    name = "Odds Ratio",
+    col = col_fun,
+    cluster_columns = FALSE,
+    cluster_rows = FALSE,
+    # Keep cell boundaries with black lines but hide the heatmap elements
+    rect_gp = gpar(col = "black", lwd = 0.5, fill = NA),
+    cell_fun = function(j, i, x, y, width, height, fill) {
+      grid.circle(
+        x = x, y = y,
+        r = unit(1.5 * size_mat[i, j], "mm"),
+        gp = gpar(fill = col_fun(mat[i, j]), col = NA)
+      )
+    },
+    row_names_gp = gpar(fontsize = 12),
+    column_names_gp = gpar(fontsize = 12, rot = 45, just = "right"),
+    heatmap_legend_param = list(
+      title = "Odds Ratio",
+      title_gp = gpar(fontsize = 14),
+      labels_gp = gpar(fontsize = 12) # ,
+      # at = c(min(mat), max(mat))
+    )
+  )
+
+  # browser()
+  lgd_list <- list(
+    # dot size for p-value
+    Legend(
+      labels = c("Not sig.", "Nominal p < 0.05", "FDR < 0.05"),
+      title = "Significance", type = "points",
+      pch = 16,
+      legend_gp = gpar(fill = "black"),
+      size = unit(1:3, "mm"),
+    )
+  )
+
+  draw(ht_list,
+    annotation_legend_list = lgd_list,
+    column_title = title,
+    column_title_gp = grid::gpar(fontsize = 16)
+  )
+}
+
+
+## Overall enrichment ----
+overall_enrich_res <- spatialLIBD::gene_set_enrichment(
+  gene_list = overall_deg_list |> map(~ .x |> pull(gene_id)),
   modeling_results = list(
     "enrichment" = cell_type_enrich_df
   ),
@@ -56,10 +190,76 @@ res <- spatialLIBD::gene_set_enrichment(
   fdr_cut = 0.05
 )
 
-gene_set_enrichment_plot(
-  res,
-  PThresh = 12,
-  ORcut = 3,
-  enrichOnly = FALSE,
-  cex = 1.5 # control the size of the text
+### Make dot plot ----
+pdf(
+  here(
+    "plots/12_cross_study_enrichment",
+    "layer_restricted_DEG_vs_PEC_cell_type_marker_overall.pdf"
+  ),
+  height = 3
 )
+overall_enrich_res |>
+  enrichment_dot_plot_heatmap(
+    title = "Overall Layer-Specifc SCZ-DEGs"
+  )
+dev.off()
+
+# Up-reg enrichment ----
+up_reg_enrich_res <- spatialLIBD::gene_set_enrichment(
+  gene_list = up_reg_deg_list |> map(~ .x |> pull(gene_id)),
+  modeling_results = list(
+    "enrichment" = cell_type_enrich_df
+  ),
+  model_type = "enrichment",
+  fdr_cut = 0.05
+)
+### Make dot plot ----
+pdf(
+  here(
+    "plots/12_cross_study_enrichment",
+    "layer_restricted_DEG_vs_PEC_cell_type_marker_up_reg.pdf"
+  ),
+  height = 3
+)
+up_reg_enrich_res |>
+  enrichment_dot_plot_heatmap(
+    title = "up-regulated Layer-Specifc SCZ-DEGs"
+  )
+dev.off()
+
+# Down-reg enrichment ----
+down_reg_enrich_res <- spatialLIBD::gene_set_enrichment(
+  gene_list = down_reg_deg_list |> map(~ .x |> pull(gene_id)),
+  modeling_results = list(
+    "enrichment" = cell_type_enrich_df
+  ),
+  model_type = "enrichment",
+  fdr_cut = 0.05
+)
+
+### Make dot plot ----
+pdf(
+  here(
+    "plots/12_cross_study_enrichment",
+    "layer_restricted_DEG_vs_PEC_cell_type_marker_down_reg.pdf"
+  ),
+  height = 3
+)
+down_reg_enrich_res |>
+  enrichment_dot_plot_heatmap(
+    title = "Down-regulated Layer-Specifc SCZ-DEGs"
+  )
+dev.off()
+
+
+## (Deprecated) Heatmap Visualization ----
+# gene_set_enrichment_plot(
+#   res,
+#   PThresh = 12,
+#   ORcut = 3,
+#   enrichOnly = FALSE,
+#   cex = 1.5 # control the size of the text
+# )
+
+# Session Info ----
+session_info()
