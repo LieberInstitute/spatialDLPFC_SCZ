208d9cdfb181d874b863cf92c6d0413730c87479
Boyi Guo <boyi.guo.work@gmail.com>
2025-06-03 15:44:46 -0400

    format

--- code/analysis/11_dx_deg_interaction/012_descriptive_stat.R
+++ code/analysis/11_dx_deg_interaction/012_descriptive_stat.R
@@ -7,10 +7,12 @@ suppressPackageStartupMessages({
 
 # Load data ----
 ## Load Layer-ajusted DEGs ----
-spd_deg_df <- read_csv(here(
-  "processed-data/rds/11_dx_deg_interaction",
-  "layer_restricted_degs_all_spds.csv"
-))
+spd_deg_df <- read_csv(
+  here(
+    "processed-data/rds/11_dx_deg_interaction",
+    "layer_restricted_degs_all_spds.csv"
+  )
+)
 
 
 # Descriptive Stats ----
@@ -18,11 +20,20 @@ spd_deg_df <- read_csv(here(
 
 ## Layer-specific DEGs ----
 ### Total number ----
-spd_deg_df |> filter(layer_specific) |> summarize(n = n())
+spd_deg_df |>
+  filter(layer_specific) |>
+  summarize(n = n())
 
 ### Total number by up-/down-reg ----
-spd_deg_df |> 
+spd_deg_df |>
   filter(layer_specific) |>
   mutate(logFC_sign = if_else(logFC > 0, "up", "down")) |>
   group_by(logFC_sign) |>
+  summarize(n = n())
+
+
+### PRECAST_spd ----
+spd_deg_df |>
+  filter(layer_specific) |>
+  group_by(PRECAST_spd) |>
   summarize(n = n())
