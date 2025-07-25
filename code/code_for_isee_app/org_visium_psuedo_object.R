4c1058492acd305f58887c4c275f4b9129ba1d3d
Boyi Guo <boyi.guo.work@gmail.com>
2025-07-25 17:05:59 -0400

    typo

--- code/code_for_isee_app/org_visium_psuedo_object.R
+++ code/code_for_isee_app/org_visium_psuedo_object.R
@@ -17,10 +17,10 @@ raw_sce <- readRDS(
 )
 
 # Adjust data ----
-ret_sec <- raw_sce
+ret_sce <- raw_sce
 ## Change SpD07-L1 to SpD07-L1/M ----
-levels(ret_sec$spd_label)[levels(ret_sec$spd_label) == "SpD07-L1"] <- "SpD07-L1/M"
-levels(ret_sec$spd_label)
+levels(ret_sce$spd_label)[levels(ret_sce$spd_label) == "SpD07-L1"] <- "SpD07-L1/M"
+levels(ret_sce$spd_label)
 
 ## Keep only relavent columns ----
 col_df <- colData(raw_sce) |> as.data.frame()
@@ -35,37 +35,37 @@ subset_col_df <- col_df |>
     spd_label
   )
 
-colData(ret_sec) <- DataFrame(subset_col_df)
+colData(ret_sce) <- DataFrame(subset_col_df)
 
-ret_sec
+ret_sce
 
 ## Remove outdated reduced dims ----
-reducedDim(ret_sec, type = "10x_pca") <- NULL
-reducedDim(ret_sec, type = "10x_tsne") <- NULL
-reducedDim(ret_sec, type = "10x_umap") <- NULL
-reducedDimNames(ret_sec)
+reducedDim(ret_sce, type = "10x_pca") <- NULL
+reducedDim(ret_sce, type = "10x_tsne") <- NULL
+reducedDim(ret_sce, type = "10x_umap") <- NULL
+reducedDimNames(ret_sce)
 
 # Check reduced dim for the pseudobulked data ----
 set.seed(20250725)
 # Calculate PCA
-ret_sec <- runPCA(ret_sec, exprs_values = "logcounts")
+ret_sce <- runPCA(ret_sce, exprs_values = "logcounts")
 # Plot PCA using scater
-plotPCA(ret_sec, colour_by = "spd_label") +
+plotPCA(ret_sce, colour_by = "spd_label") +
   ggtitle("PCA of Pseudobulked Data")
 
 # Calculate UMAP
 set.seed(20250725)
-ret_sec <- runUMAP(ret_sec, dimred = "PCA")
+ret_sce <- runUMAP(ret_sce, dimred = "PCA")
 
 ggarrange(
   plotReducedDim(
-    ret_sec,
+    ret_sce,
     dimred = "UMAP",
     colour_by = "spd_label"
   ) +
     ggtitle("UMAP of Pseudobulked Data"),
   plotReducedDim(
-    ret_sec,
+    ret_sce,
     dimred = "UMAP",
     colour_by = "sex"
   ) +
@@ -75,17 +75,17 @@ ggarrange(
 
 # Calculate tSNE
 set.seed(20250725)
-ret_sec <- runTSNE(ret_sec)
+ret_sce <- runTSNE(ret_sce)
 
 ggarrange(
   plotReducedDim(
-    ret_sec,
+    ret_sce,
     dimred = "TSNE",
     colour_by = "spd_label"
   ) +
     ggtitle("TSNE of Pseudobulked Data"),
   plotReducedDim(
-    ret_sec,
+    ret_sce,
     dimred = "TSNE",
     colour_by = "sex"
   ) +
@@ -95,7 +95,7 @@ ggarrange(
 
 # Save object ----
 saveRDS(
-  ret_sec,
+  ret_sce,
   here(
     "processed-data/rds/99_pb_data_for_isee",
     "sce_pseudo_PRECAST07_donor_spd_for_isee.rds"
