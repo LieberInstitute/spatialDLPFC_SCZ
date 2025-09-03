d2cff52635a511dc1c5cf497430f0d3a8f6285ef
Boyi Guo <boyi.guo.work@gmail.com>
2025-09-03 17:36:05 -0600

    Adjust sizes and having two plots.

--- code/analysis/11_dx_deg_interaction/03_volcano_plot_layer_restricted_analysis.R
+++ code/analysis/11_dx_deg_interaction/03_volcano_plot_layer_restricted_analysis.R
@@ -27,6 +27,29 @@ spd_deg_list <-
 # Make volcano plot ----
 ## Make a list of volcano plots ----
 
+spd_lookup_tab <- data.frame(
+  spd_labels = c(
+    "SpD07-L1/M",
+    "SpD06-L2/3",
+    "SpD02-L3/4",
+    "SpD05-L5",
+    "SpD03-L6",
+    "SpD01-WMtz",
+    "SpD04-WM"
+  )
+) |> mutate(
+  spd_raw = str_split_i(
+    spd_labels, "-", 1
+  ) |> tolower()
+)
+
+
+names(spd_deg_list) <- spd_lookup_tab$spd_labels[match(
+  names(spd_deg_list), spd_lookup_tab$spd_raw
+)]
+
+
+
 p_volca_list <- spd_deg_list |>
   imap(.f = function(df, spd_it) {
     # Create color categories for the volcano plot
@@ -48,7 +71,7 @@ p_volca_list <- spd_deg_list |>
         linetype = "dashed",
         color = "grey"
       ) +
-      geom_point() +
+      geom_point(size = 0.2) +
       # TODO:
       # Add labels on genes to higlight
       # geom_label_repel(
@@ -61,7 +84,7 @@ p_volca_list <- spd_deg_list |>
       labs(
         title = spd_it,
         y = "-log10(p-value)",
-        x = "log2(FC) in SCZ"
+        x = "log2(FC)"
       ) +
       scale_color_identity(
         name = "Significance",
@@ -71,22 +94,49 @@ p_volca_list <- spd_deg_list |>
           "Down-regulated"
         )
       ) +
-      theme_minimal()
+      theme_classic(base_size = 6) +
+      theme(plot.title = element_text(hjust = 0.5, size = 6, face = "bold"))
   })
 
 ## Save volcano plots ----
-ggpubr::ggarrange(
-  plotlist = p_volca_list,
-  ncol = 5, nrow = 2
-) |>
-  ggsave(
-    filename = here(
-      "plots/11_dx_deg_interaction",
-      "volcano_plot_layer_specific.pdf"
-    ),
-    plot = _,
-    width = 8, height = 6
-  )
+p_spd07_L1 <- p_volca_list[["SpD07-L1/M"]]
+
+ggsave(
+  filename = here(
+    "plots/11_dx_deg_interaction",
+    "volcano_plot_layer_restricted_spd07_L1_M.pdf"
+  ),
+  plot = p_spd07_L1,
+  width = 1.13, height = 1.31,
+  units = "in"
+)
+
+
+p_spd01 <- p_volca_list[["SpD01-WMtz"]]
+
+ggsave(
+  filename = here(
+    "plots/11_dx_deg_interaction",
+    "volcano_plot_layer_restricted_spd01_WMtz.pdf"
+  ),
+  plot = p_spd07_L1,
+  width = 1.13, height = 1.31,
+  units = "in"
+)
+
+
+# ggpubr::ggarrange(
+#   plotlist = p_volca_list,
+#   ncol = 5, nrow = 2
+# ) |>
+#   ggsave(
+#     filename = here(
+#       "plots/11_dx_deg_interaction",
+#       "volcano_plot_layer_restricted.pdf"
+#     ),
+#     plot = _,
+#     width = 8, height = 6
+#   )
 
 
 # Session Info ----
