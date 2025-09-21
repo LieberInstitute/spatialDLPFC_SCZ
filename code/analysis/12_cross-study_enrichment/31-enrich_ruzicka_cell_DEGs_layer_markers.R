7ad9c136cbc8b8b905a5db738e32365b96b275a1
Boyi Guo <boyi.guo.work@gmail.com>
2025-09-21 14:36:25 -0600

    Change Aethestics setting and remake the plots

--- code/analysis/12_cross-study_enrichment/31-enrich_ruzicka_cell_DEGs_layer_markers.R
+++ code/analysis/12_cross-study_enrichment/31-enrich_ruzicka_cell_DEGs_layer_markers.R
@@ -22,6 +22,8 @@ stopifnot(
   length(ruzicka_deg_list) == 25
 )
 
+ruzicka_cell_types <- names(ruzicka_deg_list)
+
 ## Load PRECAST SpD09 layer-markers ----
 raw_layer_df <- read_csv(
   here(
@@ -85,6 +87,8 @@ ruzicka_deg_list_up <- ruzicka_deg_list |>
       pull(ensembl, name = NULL)
   )
 
+length(ruzicka_deg_list_up)
+
 # Number of up-reg DEGs per cell type
 # ruzicka_deg_list_up |>
 #   map_int(~ length(.x))
@@ -118,6 +122,8 @@ ruzicka_deg_list_down <- ruzicka_deg_list |>
       pull(ensembl, name = NULL)
   )
 
+length(ruzicka_deg_list_down)
+
 ruzicka_deg_list_down |>
   map_int(~ length(.x))
 #            Ex-L2           Ex-L23            Ex-L3
@@ -145,8 +151,9 @@ enrichment_dot_plot_heatmap <- function(
     res,
     #  PThresh = 12, ORcut = 3, enrichOnly = FALSE, cex = 0.5
     title) {
+  up_flag <- stringr::str_detect(title, "Up")
+
   # Prepare data for ComplexHeatmap
-  # browser()
   mat <- res |>
     mutate(
       OR = ifelse(OR < 1, 1, OR),
@@ -155,6 +162,10 @@ enrichment_dot_plot_heatmap <- function(
       ID, test, OR
     ) |>
     pivot_wider(names_from = test, values_from = OR, values_fill = 0) |>
+    left_join(
+      x = data.frame(ID = ruzicka_cell_types),
+      by = "ID"
+    ) |>
     column_to_rownames("ID") |>
     as.matrix()
 
@@ -177,8 +188,14 @@ enrichment_dot_plot_heatmap <- function(
       ID, test, size
     ) |>
     pivot_wider(names_from = test, values_from = size, values_fill = 0) |>
+    left_join(
+      x = data.frame(ID = ruzicka_cell_types),
+      by = "ID"
+    ) |>
     column_to_rownames("ID") |>
     as.matrix()
+
+
 
   # Reorder the matrix based on the order of spd_anno_df
   spd_order <- c(
@@ -190,25 +207,73 @@ enrichment_dot_plot_heatmap <- function(
     "SpD01-WMtz",
     "SpD04-WM"
   )
-  # browser()
-  cell_type_order <- res$ID |> unique()
+  cell_type_order <- ruzicka_cell_types
 
   # TODO: manullay group cells to groups
 
   mat <- mat[cell_type_order, spd_order]
   size_mat <- size_mat[cell_type_order, spd_order]
 
+  stopifnot(nrow(mat) == 25 && nrow(size_mat) == 25)
+
+
+
   # Change matrix orientation
-  mat <- t(mat)
-  size_mat <- t(size_mat)
+  # mat <- t(mat)
+  # size_mat <- t(size_mat)
 
   # Define color function for Odds Ratio
   col_fun <- colorRamp2(
-    c(min(mat), 3, max(mat)),
+    c(min(mat, na.rm = TRUE), median(mat, na.rm = TRUE), max(mat, na.rm = TRUE)),
     c("white", "yellow", "blue")
   )
 
-  # browser()
+
+  # Add row annotations to visualize number of DEGs per cell type
+
+  deg_list <- list()
+
+  if (up_flag) {
+    deg_list <- ruzicka_deg_list_up
+  } else {
+    deg_list <- ruzicka_deg_list_down
+  }
+
+  if (length(deg_list) != nrow(mat)) {
+    missing_names <- setdiff(rownames(mat), names(deg_list))
+    if (length(missing_names) > 0) {
+      for (nm in missing_names) {
+        deg_list[[nm]] <- character(0)
+      }
+    }
+    deg_list <- deg_list[rownames(mat)]
+    stopifnot(length(deg_list) == nrow(mat))
+  }
+
+  row_anno <- rowAnnotation(
+    Num_DEGs = anno_barplot(
+      map_int(
+        deg_list,
+        ~ length(.x)
+      )[rownames(mat)],
+      gp = gpar(fill = ifelse(up_flag,
+        "red", "blue"
+      )),
+      border = FALSE,
+      axis = TRUE,
+      axis_param = list(
+        # at = c(0, 100, 200, 300),
+        # labels = c(0, 100, 200, 300),
+        gp = gpar(fontsize = 8),
+        direction = ifelse(up_flag, "normal", "reverse"),
+        labels_rot =  90 #ifelse(up_flag, -90, 90)
+      ),
+      width = unit(1, "cm"),
+    ),
+    show_annotation_name = FALSE # ,
+    # annotation_name_gp = gpar(fontsize = 8),
+    # annotation_name_side = "top"
+  )
 
   # Create the dot plot using ComplexHeatmap
   ht_list <- Heatmap(
@@ -218,16 +283,17 @@ enrichment_dot_plot_heatmap <- function(
     cluster_columns = FALSE,
     cluster_rows = FALSE,
     # Keep cell boundaries with black lines but hide the heatmap elements
-    rect_gp = gpar(col = "black", lwd = 0.5, fill = NA),
+    rect_gp = gpar(col = "lightgrey", lwd = 0.5, fill = NA),
     cell_fun = function(j, i, x, y, width, height, fill) {
       grid.circle(
         x = x, y = y,
-        r = unit(1.5 * size_mat[i, j], "mm"),
+        r = unit(size_mat[i, j], "mm"),
         gp = gpar(fill = col_fun(mat[i, j]), col = NA)
       )
     },
     row_names_gp = gpar(fontsize = 8),
     column_names_gp = gpar(fontsize = 8, rot = 45, just = "right"),
+    left_annotation = row_anno,
     heatmap_legend_param = list(
       title = "Odds Ratio",
       title_gp = gpar(fontsize = 8),
@@ -236,56 +302,24 @@ enrichment_dot_plot_heatmap <- function(
     )
   )
 
-  # browser()
-  lgd_list <- list(
-    # dot size for p-value
-    Legend(
-      labels = c("Not sig.", "Nominal p < 0.05", "FDR < 0.05"),
-      title = "Significance", type = "points",
-      pch = 16,
-      legend_gp = gpar(fill = "black"),
-      size = unit(1:3, "mm"),
-    )
-  )
+  return(ht_list)
+  # lgd_list <- list(
+  #   # dot size for p-value
+  #   Legend(
+  #     labels = c("Not sig.", "Nominal p < 0.05", "FDR < 0.05"),
+  #     title = "Significance", type = "points",
+  #     pch = 16,
+  #     legend_gp = gpar(fill = "black"),
+  #     size = unit(1:3, "mm"),
+  #   )
+  # )
 
-  draw(ht_list,
-    annotation_legend_list = lgd_list,
-    column_title = title,
-    column_title_gp = gpar(fontsize = 8, fontface = "bold")
-  )
+  # draw(ht_list,
+  #   annotation_legend_list = lgd_list,
+  #   column_title = title,
+  #   column_title_gp = gpar(fontsize = 8, fontface = "bold")
+  # )
 }
-
-##  ALL GENS ----
-all_gene_res <- ruzicka_deg_list_sig[
-  # remove cell types with less than 25 DEGs
-  -which(ruzicka_deg_list_sig |> map_int(~ length(.x)) < 25)
-] |>
-  spatialLIBD::gene_set_enrichment(
-    gene_list = _,
-    modeling_results = list("enrichment" = raw_layer_df),
-    model_type = "enrichment",
-    fdr_cut = 0.1
-  ) |>
-  # Annotate the spds
-  inner_join(
-    spd_anno_df,
-    by = c("test" = "spd")
-  ) |>
-  mutate(test = anno_lab) |>
-  select(-label, -anno_lab)
-
-# all_gene_res |> enrichment_dot_plot_ggplot()
-pdf(
-  here(
-    "plots/12_cross_study_enrichment",
-    "Ruzicka_DEGs_vs_PRECAST_layer_markers_all.pdf"
-  ),
-  height = 3.5
-)
-all_gene_res |> enrichment_dot_plot_heatmap(
-  title = "Overall Ruzicka DEGs"
-)
-dev.off()
 
 ## Up-reg gene only enrichment ----
 ruzicka_deg_list_up <- ruzicka_deg_list_up[
@@ -312,11 +346,13 @@ pdf(
     "plots/12_cross_study_enrichment",
     "Ruzicka_DEGs_vs_PRECAST_layer_markers_up-reg.pdf"
   ),
-  height = 3.5
+  height = 4, width = 3
 )
-up_gene_res |> enrichment_dot_plot_heatmap(
-  title = "Up-regulated Ruzicka DEGs"
-)
+up_gene_res |>
+  enrichment_dot_plot_heatmap(
+    title = "Up-regulated Ruzicka DEGs"
+  ) |>
+  draw(show_heatmap_legend = FALSE)
 dev.off()
 
 ## Down-reg gene only enrichment ----
@@ -341,11 +377,13 @@ pdf(
     "plots/12_cross_study_enrichment",
     "Ruzicka_DEGs_vs_PRECAST_layer_markers_down-reg.pdf"
   ),
-  height = 3.5
+  height = 4, width = 3
 )
-down_gene_res |> enrichment_dot_plot_heatmap(
-  title = "Down-regulated Ruzicka DEGs"
-)
+down_gene_res |>
+  enrichment_dot_plot_heatmap(
+    title = "Down-regulated Ruzicka DEGs"
+  ) |>
+  draw(show_heatmap_legend = FALSE)
 dev.off()
 
 # Session info ----
@@ -353,25 +391,57 @@ session_info()
 
 
 # Deprecated functions ----
+##  ALL GENS ----
+# all_gene_res <- ruzicka_deg_list_sig[
+#   # remove cell types with less than 25 DEGs
+#   -which(ruzicka_deg_list_sig |> map_int(~ length(.x)) < 25)
+# ] |>
+#   spatialLIBD::gene_set_enrichment(
+#     gene_list = _,
+#     modeling_results = list("enrichment" = raw_layer_df),
+#     model_type = "enrichment",
+#     fdr_cut = 0.1
+#   ) |>
+#   # Annotate the spds
+#   inner_join(
+#     spd_anno_df,
+#     by = c("test" = "spd")
+#   ) |>
+#   mutate(test = anno_lab) |>
+#   select(-label, -anno_lab)
+
+# # all_gene_res |> enrichment_dot_plot_ggplot()
+# pdf(
+#   here(
+#     "plots/12_cross_study_enrichment",
+#     "Ruzicka_DEGs_vs_PRECAST_layer_markers_all.pdf"
+#   ),
+#   height = 4
+# )
+# all_gene_res |> enrichment_dot_plot_heatmap(
+#   title = "Overall Ruzicka DEGs"
+# )
+# dev.off()
+
 ## (Deprecated) Dot plot wrapper (ggplot) ----
-enrichment_dot_plot_ggplot <- function(
-    res) {
-  res |>
-    ggplot(aes(x = test, y = ID, color = OR, size = -log10(Pval))) +
-    geom_point() +
-    # Colorblind-friendly palette
-    scale_color_viridis_c(option = "C", direction = -1) +
-    scale_size_continuous(range = c(1, 10)) +
-    theme_minimal(base_size = 14) + # Adjust text size for publication
-    theme(
-      axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 12),
-      axis.text.y = element_text(size = 12),
-      axis.title = element_blank(),
-      legend.title = element_text(size = 14),
-      legend.text = element_text(size = 12)
-    ) +
-    labs(
-      color = "Odds Ratio",
-      size = "-log10(p-value)"
-    )
-}
+# enrichment_dot_plot_ggplot <- function(
+#     res) {
+#   res |>
+#     ggplot(aes(x = test, y = ID, color = OR, size = -log10(Pval))) +
+#     geom_point() +
+#     # Colorblind-friendly palette
+#     scale_color_viridis_c(option = "C", direction = -1) +
+#     scale_size_continuous(range = c(1, 10)) +
+#     theme_minimal(base_size = 14) + # Adjust text size for publication
+#     theme(
+#       axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1, size = 12),
+#       axis.text.y = element_text(size = 12),
+#       axis.title = element_blank(),
+#       legend.title = element_text(size = 14),
+#       legend.text = element_text(size = 12)
+#     ) +
+#     labs(
+#       color = "Odds Ratio",
+#       size = "-log10(p-value)"
+#     )
+# }
