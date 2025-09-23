5ac91efca6c0283a9007e6ad039aa83f22c3bbd4
Boyi Guo <boyi.guo.work@gmail.com>
2025-09-23 14:12:10 -0600

    Refine volcano plot aesthetics

--- code/analysis/14_prs_deg/13_volcano_plot.R
+++ code/analysis/14_prs_deg/13_volcano_plot.R
@@ -2,6 +2,7 @@
 suppressPackageStartupMessages({
   library(tidyverse)
   library(sessioninfo)
+  library(ggrepel)
   library(here)
 })
 
@@ -15,6 +16,8 @@ test_res <- read_csv(
   mutate(
     sig_10 = adj.P.Val < 0.10,
     gene_cat = case_when(
+      sig_10 == FALSE & P.Value < 0.05 & logFC >= 0 ~ "#ef8080",
+      sig_10 == FALSE & P.Value < 0.05 & logFC < 0 ~ "#808ef8",
       sig_10 == FALSE ~ "grey",
       sig_10 == TRUE & logFC >= 0 ~ "red",
       sig_10 == TRUE & logFC < 0 ~ "blue"
@@ -23,34 +26,61 @@ test_res <- read_csv(
 
 ## Create a volcano plot ----
 ggplot(
-  test_res,
+  test_res |> arrange(desc(P.Value)),
   aes(x = logFC, y = -log10(P.Value))
 ) +
-  geom_hline(yintercept = -log10(0.05), color = "red") +
+  geom_hline(
+    yintercept = -log10(0.05),
+    linetype = "dashed",
+    color = "grey"
+  ) +
   geom_point(
     aes(color = gene_cat)
   ) +
-  scale_color_manual(
-    values = c(
-      "grey" = "grey",
-      "red" = "red",
-      "blue" = "blue"
-    ),
-    name = "Genes",
-    labels = c(
-      "grey" = "Not sig.",
-      "red" = "Up (FDR < 0.10)",
-      "blue" = "Down (FDR < 0.10)"
-    )
+  geom_text_repel(
+    data = test_res |>
+      filter(gene_name %in% c("XRRA1", "C2orf74", "AC004556.3", "MTRNR2L8")),
+    aes(label = gene_name),
+    color = "black",
+    force = 0.5,
+    # All labels not overlapping
+    max.overlaps = Inf,
+    # Have arrows
+    min.segment.length = 0,
+    size = 2, # 6pt label font ≈ size 2 in ggplot2
+    arrow = arrow(length = unit(0.5, "points"), type = "closed"),
+    segment.size = 0.2,
+    segment.color = "black"
   ) +
-  theme_bw() +
+  # scale_color_manual(
+  #   values = c(
+  #     "grey" = "grey",
+  #     "red" = "red",
+  #     "blue" = "blue"
+  #   ),
+  #   name = "Genes",
+  #   labels = c(
+  #     "grey" = "Not sig.",
+  #     "red" = "Up (FDR < 0.10)",
+  #     "blue" = "Down (FDR < 0.10)"
+  #   )
+  # ) +
+  theme_classic(base_size = 6) +
   theme(
-    axis.title = element_text(size = 14, face = "bold"),
-    axis.text = element_text(size = 12),
-    legend.title = element_text(size = 12),
-    legend.text = element_text(size = 10),
+    axis.title = element_text(size = 6, face = "bold"),
+    axis.text = element_text(size = 6),
+    legend.title = element_text(size = 6),
+    legend.text = element_text(size = 6),
     legend.position = "bottom"
-  )
+  ) + scale_color_identity(
+    # name = "Significance",
+    # labels = c(
+    #   "FDR > 0.10",
+    #   "Up-regulated",
+    #   "Down-regulated"
+    # ),
+    # guide = "legend"
+  ) 
 
 ggsave(
   filename = here(
@@ -58,7 +88,7 @@ ggsave(
     "PRS_DEG_volcano_plot_PRECAST07_donor_spd.pdf"
   ),
   plot = last_plot(),
-  width = 4, height = 4.5
+  width = 2.5, height = 2.5, units = "in"
 )
 
 # Session Info ----
