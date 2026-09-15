7c11b17d7e62ac4a3170a928707010bf0f4cc896
Boyi Guo <boyi.guo.work@gmail.com>
2026-09-14 22:13:24 -0600

    Plot k=2-16

--- code/analysis/03_visium_spatial_clustering/plot_PRECAST_02-16.R
+++ code/analysis/03_visium_spatial_clustering/plot_PRECAST_02-16.R
@@ -4,6 +4,7 @@ suppressPackageStartupMessages({
   library(SpatialExperiment)
   library(tidyverse)
   library(escheR)
+  library(ggpubr)
   library(sessioninfo)
 })
 
@@ -11,8 +12,7 @@ suppressPackageStartupMessages({
 ## SPE data ----
 spe <- readRDS(
   here(
-    "processed-data/rds/spatial_cluster",
-    "PRECAST",
+    "processed-data/rds/03_visium_spatial_clustering",
     "spe_wo_spg_N63_PRECAST.rds"
   )
 )
@@ -24,34 +24,6 @@ stopifnot(all(spe$in_tissue))
 spe$sample_label <- paste0(
   spe$brnum, "_", toupper(spe$dx)
 )
-
-## Load PRECAST df ----
-PRECAST_df <- readRDS(
-  here(
-    "processed-data/rds/spatial_cluster",
-    "PRECAST",
-    "test_clus_label_df_semi_inform_k_2-16.rds"
-  )
-)
-
-## Merge PRECAST df ----
-precast_vars <- grep(
-  "^PRECAST_", colnames(PRECAST_df),
-  value = TRUE
-)
-
-spe <- spe[, spe$key %in% PRECAST_df$key]
-col_data_df <- PRECAST_df |>
-  right_join(
-    colData(spe) |> data.frame(),
-    by = c("key"),
-    relationship = "one-to-one"
-  )
-rownames(col_data_df) <- colnames(spe)
-colData(spe) <- DataFrame(col_data_df)
-
-# error prevention
-stopifnot(is.character(spe$PRECAST_07))
 
 ## Subset to representative samples ----
 rep_sample_id <- c("V13M06-342_D1", "V13M06-343_D1")
@@ -73,6 +45,32 @@ stopifnot(
   ) == 2
 )
 
+## Load and add PRECAST labels ----
+PRECAST_df <- readRDS(
+  here(
+    "processed-data/rds/03_visium_spatial_clustering",
+    "PRECAST_label_df_semi_sup_k_2-16.rds"
+  )
+)
+
+precast_vars <- grep(
+  "^PRECAST_", colnames(PRECAST_df),
+  value = TRUE
+)
+precast_idx <- match(spe$key, PRECAST_df$key)
+
+stopifnot(!anyNA(precast_idx))
+
+colData(spe)[, precast_vars] <- DataFrame(
+  PRECAST_df[precast_idx, precast_vars, drop = FALSE]
+)
+
+# error prevention
+stopifnot(is.character(spe$PRECAST_07))
+
+rm(PRECAST_df)
+gc()
+
 # Make plots ----
 ## Iterat over all possible PRECAST settings
 # More data driven approach
@@ -80,7 +78,7 @@ stopifnot(
 #   grep(pattern = "PRECAST_", x = _, value = TRUE) |>
 
 panel_list <-
-  set_names(2:13, ~ sprintf("PRECAST_%02s", .)) |>
+  set_names(2:16, ~ sprintf("PRECAST_%02s", .)) |>
   map(.f = function(.k) {
     # iterate over samples
     precast_k <- sprintf("PRECAST_%02d", .k)
@@ -99,7 +97,7 @@ panel_list <-
           scale_fill_manual(
             name = "Spatial Domain",
             values = set_names(
-              Polychrome::palette36.colors(13)[seq.int(.k)],
+              Polychrome::palette36.colors(16)[seq.int(.k)],
               unique(sub_spe[[precast_k]]) |> sort()
             )
           ) +
@@ -130,21 +128,21 @@ panel_list <-
 # Make panels ----
 cowplot::plot_grid(
   plotlist = panel_list,
-  nrow = 6, ncol = 2 # ,
-  # labels = sprintf("k=%02d", 2:13)
+  nrow = 8, ncol = 2 # ,
+  # labels = sprintf("k=%02d", 2:16)
   # TODO: use the max precast label
   # common.legend = TRUE,
   # legend = "bottom"
 ) |> ggsave(
   here(
     "plots/03_visium_spatial_clustering",
-    "spot_plot_PRECAST_2-13_rep_samples.pdf"
+    "spot_plot_PRECAST_2-16_rep_samples.pdf"
   ),
   plot = _,
-  height = 11, width = 7
+  height = 14.7, width = 7
 )
 
 
 
 # Session Info ----
-sessioninfo()
+session_info()
